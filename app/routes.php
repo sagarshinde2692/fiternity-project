47e7247b3d61bcb7331760566e747339f3522552
“sanjaysahu1688” <sanjay.id7@gmail.com>
2014-11-26 17:21:43 +0530

    updates for home api

--- app/routes.php
+++ app/routes.php
@@ -40,7 +40,7 @@ Route::get('getallfinders/', array('as' => 'finders.getallfinders','uses' => 'Fi
 
 ##############################################################################
 /******************** BLOGS SECTION START HERE ********************/
-Route::get('/blogs/{limit}/{offset}', 'BlogsController@getBlogs');
+Route::get('/blogs/{limit?}/{offset?}', 'BlogsController@getBlogs');
 Route::get('blogdetail/{slug}', array('as' => 'blogs.blogdetail','uses' => 'BlogsController@blogdetail'));
 Route::get('/blogs/{cat}', 'BlogsController@getCategoryBLogs');
 Route::get('/updateblogdate', 'BlogsController@updateblogdate');
