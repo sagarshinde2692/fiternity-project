e4a825bdea56a4bb4cdcd1415fd2cb585479e274
sanjay1688 <sanjay.id7@gmail.com>
2014-11-28 20:42:23 +0530

    updates article offest

--- app/controllers/BlogsController.php
+++ app/controllers/BlogsController.php
@@ -15,9 +15,9 @@ class BlogsController extends \BaseController {
     }
 
     // Limiting to something
-	public function getBlogs($offset = 0){	
+	public function getBlogs($offset = 0,$limit = 10){	
 		$offset =  	(int) $offset;	
-		$limit 	= 	10;
+		$limit 	= 	(int) $limit;
 		$blogs 	=	Blog::with(array('category'=>function($query){$query->select('_id','name','slug','meta');}))
 						->with('categorytags')
 						->with(array('author'=>function($query){$query->select('_id','name','username','email','avatar');}))
