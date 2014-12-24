6efdb718dc435d81b95f9962eff70f52beb3f5f5
“sanjaysahu1688” <sanjay.id7@gmail.com>
2014-12-24 12:40:59 +0530

    test email queue

--- app/controllers/EmailSmsApiController.php
+++ app/controllers/EmailSmsApiController.php
@@ -61,7 +61,7 @@ class EmailSmsApiController extends \BaseController {
 				$reciver_name = 'sanjay sahu';
 				$reciver_subject = 'subject of test email';
 				//$message->to('sanjay.id@gmail.com')->bcc('sanjay.@example.com');				
-				$message->to($'sanjay.id@gmail.com', 'sanjay sahu')->subject('subject of test email');
+				$message->to($to, $reciver_name)->subject($reciver_subject);
 
 			});
 
