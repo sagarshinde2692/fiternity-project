4839257b2eddd1e176fd3b9087878bd7bcdf43ea
“sanjaysahu1688” <sanjay.id7@gmail.com>
2014-12-26 15:44:02 +0530

    updates for fivefitness email

--- app/controllers/EmailSmsApiController.php
+++ app/controllers/EmailSmsApiController.php
@@ -313,7 +313,7 @@ class EmailSmsApiController extends \BaseController {
 				'date' => date("h:i:sa")        
 				), 
 			'to'				=> 	Config::get('mail.to_neha'), 
-			'bcc_emailds' 		=> 	Config::get('mail.bcc_emailds_book_trial_landing_page'), 
+			'bcc_emailds' 		=> 	Config::get('mail.bcc_emailds_finder_offer_pop'), 
 			'email_subject' 	=> 	'Request for Available Offer - '.Input::json()->get('vendor'),
 			'send_bcc_status' 	=> 	1
 			);
