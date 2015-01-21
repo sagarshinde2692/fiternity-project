a75dac94d46b55971f74d471090502be8203f543
“sanjaysahu1688” <sanjay.id7@gmail.com>
2015-01-21 16:06:01 +0530

    updates

--- app/controllers/EmailSmsApiController.php
+++ app/controllers/EmailSmsApiController.php
@@ -451,7 +451,7 @@ class EmailSmsApiController extends \BaseController {
 				);
 
 		$emaildata = array(
-			'email_template' 		=> 	'emails.finder.not_able_to_find', 
+			'email_template' 		=> 	'emails.finder.customer_not_able_to_find', 
 			'email_template_data' 	=> 	$data, 
 			'to'					=> 	Config::get('mail.to_neha'), 
 			'bcc_emailds' 			=> 	Config::get('mail.bcc_emailds_not_able_to_find'), 
