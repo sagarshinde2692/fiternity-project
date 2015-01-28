f8caedc72808462a69c0383538454ee90cdd55bf
“sanjaysahu1688” <sanjay.id7@gmail.com>
2015-01-28 13:44:37 +0530

    updates for deals of the day - product

--- app/config/deals.php
+++ app/config/deals.php
@@ -11,13 +11,13 @@ return array(
 			'name' => "Raw Pressery - Light Cleanse, One + One FREE",
 			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_9.jpg',
 			'location' => 'Trim, Run, Flush, Shield, Glow, Build',
-			'discount' => '50%','price'=>1500,'special_price'=>750,'sold_out'=>0
+			'discount' => '50%','price'=>3000,'special_price'=>1500,'sold_out'=>0
 			),	
 		array(
 			'name' => "Raw Pressery - Deep Cleanse, One + One FREE",
 			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_10.jpg',
 			'location' => 'Trim, Love, Flush, Shield, Glow, Lean',
-			'discount' => '50%','price'=>1500,'special_price'=>750,'sold_out'=>0
+			'discount' => '50%','price'=>3000,'special_price'=>1500,'sold_out'=>0
 			),	
 		array(
 			'name' => "RiteBite Max Protein - Pack of 6",
@@ -33,16 +33,40 @@ return array(
 			),
 
 		array(
+			'name' => "The Green Snacks - Kale Chips",
+			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_6.jpg',
+			'location' => 'Thai Sweet Chilli, Sea Salt & Vinegar, Cheese Onion',
+			'discount' => '51%','price'=>200,'special_price'=>99,'sold_out'=>0
+			),
+		array(
+			'name' => "Jiwa Atta 1Kg",
+			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_23.jpg',
+			'location' => 'Slimming, Fitness, Diabetic Care, Gluten Free',
+			'discount' => '90%','price'=>99,'special_price'=>9,'sold_out'=>0
+			),
+		array(
 			'name' => "Eat A Whey - Protein Peanut Butter",
 			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_2.jpg',
 			'location' => 'Vanilla, Chocolate',
 			'discount' => '67%','price'=>450,'special_price'=>149,'sold_out'=>0
 			),
 		array(
+			'name' => "Organic Food Hamper (Jiwa Nutra Multi-Grain Atta, Nourish Organic Honey Roasted Almond & Concious Food Masala Tea)",
+			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_22.jpg',
+			'location' => 'Jiwa Nutra Multi-Grain Atta, Nourish Organic Honey Roasted Almond & Concious Food Masala Tea',
+			'discount' => '65%','price'=>500,'special_price'=>325,'sold_out'=>0
+			),
+		array(
 			'name' => "Eat A Whey - Chocolate Protein Brownies",
 			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_3.jpg',
 			'location' => 'Chocolate',
 			'discount' => '79%','price'=>950,'special_price'=>199,'sold_out'=>0
+			),
+		array(
+			'name' => "Fofamlee Green Tea",
+			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_21.jpg',
+			'location' => 'Cranberry, Chamomile, Apple Cinnamon',
+			'discount' => '100%','price'=>195,'special_price'=>0,'sold_out'=>0
 			),
 		array(
 			'name' => "Eat A Whey - Chocolate Peanut Protein Brownies",
@@ -57,22 +81,10 @@ return array(
 			'discount' => '80%','price'=>750,'special_price'=>149,'sold_out'=>0
 			),
 		array(
-			'name' => "The Green Snacks - Kale Chips",
-			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_6.jpg',
-			'location' => 'Thai Sweet Chilli, Sea Salt & Vinegar, Cheese Onion',
-			'discount' => '51%','price'=>200,'special_price'=>99,'sold_out'=>0
-			),
-		array(
 			'name' => "The Green Snacks - Kale Chips (Pack of 3 Flavours)",
 			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_7.jpg',
 			'location' => 'Thai Sweet Chilli, Sea Salt & Vinegar, Cheese Onion',
 			'discount' => '17%','price'=>600,'special_price'=>499,'sold_out'=>0
-			),
-		array(
-			'name' => "Jiwa Atta 1Kg",
-			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_23.jpg',
-			'location' => 'Slimming, Fitness, Diabetic Care, Gluten Free',
-			'discount' => '90%','price'=>99,'special_price'=>9,'sold_out'=>0
 			),
 		array(
 			'name' => "Jiwa Atta 1Kg",
@@ -82,22 +94,10 @@ return array(
 			),
 
 		array(
-			'name' => "Organic Food Hamper (Jiwa Nutra Multi-Grain Atta, Nourish Organic Honey Roasted Almond & Concious Food Masala Tea)",
-			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_22.jpg',
-			'location' => 'Jiwa Nutra Multi-Grain Atta, Nourish Organic Honey Roasted Almond & Concious Food Masala Tea',
-			'discount' => '65%','price'=>500,'special_price'=>325,'sold_out'=>0
-			),
-		array(
 			'name' => "Fofamlee Green Tea",
 			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_21.jpg',
 			'location' => 'Green Tea, Honey Lemon. Mint, Jasmine, Kashmiri, Tulsi',
 			'discount' => '100%','price'=>125,'special_price'=>0,'sold_out'=>0
-			),
-		array(
-			'name' => "Fofamlee Green Tea",
-			'image' => 'http://b.fitn.in/global/fitmania/28/onlineimageresize_com_21.jpg',
-			'location' => 'Cranberry, Chamomile, Apple Cinnamon',
-			'discount' => '100%','price'=>195,'special_price'=>0,'sold_out'=>0
 			),
 
 
