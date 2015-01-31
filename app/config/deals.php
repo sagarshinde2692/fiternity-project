acf54d9f7dc442d705fede91be70448094f032bb
“sanjaysahu1688” <sanjay.id7@gmail.com>
2015-01-31 19:21:45 +0530

    deals

--- app/config/deals.php
+++ app/config/deals.php
@@ -9,8 +9,8 @@ return array(
 	'offers' => array(
 		array(
 			'name' => "Funatic Football Jersery",
-			'image' => 'http://b.fitn.in/global/fitmania/31/onlineimageresize_com_4.png',
-			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1336',
+			'image' => 'http://b.fitn.in/global/fitmania/31/onlineimageresize_com_wazza_6.png',
+			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1335',
 			'discount' => '60%','price'=>599,'special_price'=>249,'sold_out'=>0
 			),
 		array(
@@ -22,19 +22,19 @@ return array(
 		array(
 			'name' => "Funatic Women's Apparel",
 			'image' => 'http://b.fitn.in/global/fitmania/31/onlineimageresize_com_2.png',
-			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1338',
+			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1337',
 			'discount' => '83%','price'=>599,'special_price'=>99,'sold_out'=>0
 			),
 		array(
-			'name' => "Tego Towels",
+			'name' => "Anti-bacterial Towels",
 			'image' => 'http://b.fitn.in/global/fitmania/31/onlineimageresize_com_5.jpg',
-			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1335',
+			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1339',
 			'discount' => '40%','price'=>499,'special_price'=>249,'sold_out'=>0
 			),
 		array(
 			'name' => "Funatic Sippers",
 			'image' => 'http://b.fitn.in/global/fitmania/31/onlineimageresize_com_1.jpg',
-			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1339',
+			'product_url' => 'http://store.fitternity.com/deals-of-the-day?cat=1338',
 			'discount' => '80%','price'=>500,'special_price'=>99,'sold_out'=>0
 			),								
 
