170e82f80d1767764b994624cb2bedc87658e252
“sanjaysahu1688” <sanjay.id7@gmail.com>
2015-02-09 13:22:03 +0530

    updates for deals

--- app/config/deals.php
+++ app/config/deals.php
@@ -8,53 +8,146 @@ return array(
 	
 	'offers' => array(
 		array(
-			'name' => "Top Gear FG 1 month membership",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_1.jpg',
-			'location' => 'Juhu | Bandra',
-			'discount' => '80%','price'=>2500,'special_price'=>499,'sold_out'=>0
+			'name' => "Yog Power - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_1.jpg',
+			'location' => 'Kandivali West, Kandivali East',
+			'discount' => '97%','price'=>3933,'special_price'=>99,'sold_out'=>0
 			),
 		array(
-			'name' => "Be-Fit - 3 sessions of Marathon Training",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_8.jpg',
-			'location' => 'Juhu | Bandra | Shivaji Park | Mahalaxmi Racecourse',
-			'discount' => '90%','price'=>1000,'special_price'=>99,'sold_out'=>0
+			'name' => "Yoga 101 - 1 month, 8 sessions",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_yo1.jpg',
+			'location' => 'Versova',
+			'discount' => '96%','price'=>2800,'special_price'=>99,'sold_out'=>0
 			),	
 		array(
-			'name' => "Run India Run 1 year membership ",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_3.jpg',
-			'location' => 'Ghatkopar (East) | Juhu | Matunga | Powai | Matunga | Powai | Mulund (West) | Ghatkopar (West) | Chembur (West)',
-			'discount' => '33%','price'=>15000,'special_price'=>9999,'sold_out'=>0
+			'name' => "Vandana Full Circle - 1 Session",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_4.jpg',
+			'location' => 'Bandra',
+			'discount' => '15%','price'=>400,'special_price'=>340,'sold_out'=>0
 			),
 		array(
-			'name' => "Dumbelled Marathon Training - 8 Sessions",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_4.jpg',
-			'location' => 'Juhu | Malad (West) | Kandivali (East) | Chembur | Nerul | Andheri (East) | Powai | Matunga | Borivili | Lokhandwala',
-			'discount' => '96%','price'=>2500,'special_price'=>99,'sold_out'=>0
+			'name' => "The Yoga Institute - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_19.jpg',
+			'location' => 'Santacruz East',
+			'discount' => '15%','price'=>1000,'special_price'=>850,'sold_out'=>0
 			),
 		array(
-			'name' => "Star Athletes United 1 month membership",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_2.jpg',
+			'name' => "Sunaina Rekhi - 1 Session",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_12.jpg',
+			'location' => 'Khar, Bandra, Versova',
+			'discount' => '15%','price'=>450,'special_price'=>383,'sold_out'=>0
+			),
+		array(
+			'name' => "Nuage Hot Yoga - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_22.jpg',
+			'location' => 'Andheri West, Lokhandwala',
+			'discount' => '29%','price'=>10000,'special_price'=>7125,'sold_out'=>0
+			),	
+		array(
+			'name' => "Ancient Yoga - 5 Months",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_16.png',
+			'location' => 'Versova',
+			'discount' => '49%','price'=>15000,'special_price'=>7650,'sold_out'=>0
+			),	
+		array(
+			'name' => "Samanta Duggal - 1 Session",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_2.jpg',
+			'location' => 'Kemps Corner',
+			'discount' => '15%','price'=>500,'special_price'=>425,'sold_out'=>0
+			),	
+		array(
+			'name' => "Mandeep Hot Yoga - 1 Session",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_23.jpg',
+			'location' => 'Andheri West, Lokhandwala',
+			'discount' => '15%','price'=>1000,'special_price'=>850,'sold_out'=>0
+			),	
+		array(
+			'name' => "Anshuka Yoga - 1 Session",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_8.jpg',
+			'location' => 'Bandra',
+			'discount' => '30%','price'=>500,'special_price'=>350,'sold_out'=>0
+			),	
+		array(
+			'name' => "Yogvan - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_11.jpg',
+			'location' => 'Malad West',
+			'discount' => '15%','price'=>1100,'special_price'=>935,'sold_out'=>0
+			),	
+		array(
+			'name' => "Rima Thakker - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_9.jpg',
+			'location' => 'Chembur East',
+			'discount' => '15%','price'=>1400,'special_price'=>1190,'sold_out'=>0
+			),	
+		array(
+			'name' => "Yoga in Soul - 12 Sessions",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_21.jpg',
+			'location' => 'Ghatkopar West',
+			'discount' => '15%','price'=>3000,'special_price'=>2550,'sold_out'=>0
+			),	
+		array(
+			'name' => "Cosmic Fusion - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_3.jpg',
+			'location' => 'Santacruz West',
+			'discount' => '15%','price'=>3000,'special_price'=>2550,'sold_out'=>0
+			),	
+		array(
+			'name' => "Ajinkya Natu - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_13.jpg',
+			'location' => 'Bandra East',
+			'discount' => '26%','price'=>4000,'special_price'=>2975,'sold_out'=>0
+			),	
+		array(
+			'name' => "Freedom Holistic Wellness - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_7.jpg',
+			'location' => 'Bandra, Carter Road',
+			'discount' => '15%','price'=>4500,'special_price'=>3825,'sold_out'=>0
+			),	
+		array(
+			'name' => "Yogacara - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_10.jpg',
+			'location' => 'Bandra',
+			'discount' => '10%','price'=>4200,'special_price'=>3780,'sold_out'=>0
+			),	
+		array(
+			'name' => "Samyak Fitness - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_17.png',
+			'location' => 'Bandup, Mahim',
+			'discount' => '33%','price'=>6400,'special_price'=>4250,'sold_out'=>0
+			),	
+
+		array(
+			'name' => "Shiv Holistic - 1 Month (Personal Training)",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_6.jpg',
+			'location' => 'South Mumbai, Bandra',
+			'discount' => '15%','price'=>7000,'special_price'=>5525,'sold_out'=>0
+			),	
+		array(
+			'name' => "Aum Shanti Yoga - 1 Month",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_14.jpg',
 			'location' => 'Juhu',
-			'discount' => '95%','price'=>2000,'special_price'=>99,'sold_out'=>0
-			),
-		array(
-			'name' => "Savio Dsouza - 1 Month",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_5.jpg',
-			'location' => 'Nariman Point | PDP Park',
-			'discount' => '33%','price'=>2500,'special_price'=>1500,'sold_out'=>0
+			'discount' => '21%','price'=>7500,'special_price'=>5950,'sold_out'=>0
 			),	
 		array(
-			'name' => "Top Gear FG 3 month membership",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_6.jpg',
-			'location' => 'Juhu | Bandra',
-			'discount' => '67%','price'=>8000,'special_price'=>1999,'sold_out'=>0
+			'name' => "Mindfulness Yoga - 8 Sessions",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_20.jpg',
+			'location' => 'Juhu, Mahim, Bandra, Andheri',
+			'discount' => '15%','price'=>6400,'special_price'=>5440,'sold_out'=>0
 			),	
 		array(
-			'name' => "Star Athletes United 3 month membership",
-			'image' => 'http://b.fitn.in/global/fitmania/4/onlineimageresize_com_7.jpg',
-			'location' => 'Juhu',
-			'discount' => '67%','price'=>5000,'special_price'=>2999,'sold_out'=>0
+			'name' => "Health of Green (Personal Training) - 16 Sessions",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_18.jpg',
+			'location' => 'Andheri West',
+			'discount' => '10%','price'=>10000,'special_price'=>9000,'sold_out'=>0
 			),	
+		array(
+			'name' => "Anjali Sahu - 1 Session",
+			'image' => 'http://b.fitn.in/global/fitmania/9/onlineimageresize_com_15.jpg',
+			'location' => 'Bandra, Carter Road',
+			'discount' => '15%','price'=>500,'special_price'=>425,'sold_out'=>0
+			),	
+
+
 
 		),
 
