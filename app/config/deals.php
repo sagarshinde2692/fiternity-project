574f673f86dead698ade5982efbe9f8b01d8788a
“sanjaysahu1688” <sanjay.id7@gmail.com>
2015-02-13 17:35:13 +0530

    updates new deals

--- app/config/deals.php
+++ app/config/deals.php
@@ -14,7 +14,13 @@ return array(
 			'discount' => '100%','price'=>1000,'special_price'=>0,'sold_out'=>0
 			),
 		array(
-			'name' => "House of Wow - BollyHop Workshop (Individual)",
+			'name' => "Fitness First Workshop 1 Session",
+			'image' => 'http://b.fitn.in/global/fitmania/12/onlineimageresize_com_unnamed.png',
+			'location' => 'Bandra',
+			'discount' => '100%','price'=>1200,'special_price'=>0,'sold_out'=>0
+			),	
+		array(
+			'name' => "House of Wow - BollyHop Workshop (1 People)",
 			'image' => 'http://b.fitn.in/global/fitmania/12/onlineimageresize_com_HOW.jpg',
 			'location' => 'Bandra',
 			'discount' => '50%','price'=>600,'special_price'=>300,'sold_out'=>0
@@ -26,19 +32,19 @@ return array(
 			'discount' => '50%','price'=>400,'special_price'=>200,'sold_out'=>0
 			),
 		array(
-			'name' => "Powerhouse - 1 Year (Couple)",
+			'name' => "Powerhouse - 1 Year (2 People)",
 			'image' => 'http://b.fitn.in/global/fitmania/12/onlineimageresize_com_1.jpg',
 			'location' => 'Mumbai Central',
 			'discount' => '25%','price'=>15000,'special_price'=>11250,'sold_out'=>0
 			),
 		array(
-			'name' => "Beyond Fitness - 1 Year (Couple)",
+			'name' => "Beyond Fitness - 1 Year (2 People)",
 			'image' => 'http://b.fitn.in/global/fitmania/12/onlineimageresize_com_2.jpg',
 			'location' => 'Walkeshwar',
 			'discount' => '54%','price'=>80000,'special_price'=>37000,'sold_out'=>0
 			),	
 		array(
-			'name' => "Muscle N Mind - 1 Year (Couple)",
+			'name' => "Muscle N Mind - 1 Year (2 People)",
 			'image' => 'http://b.fitn.in/global/fitmania/12/onlineimageresize_com_m1.jpg',
 			'location' => 'Colaba',
 			'discount' => '50%','price'=>44000,'special_price'=>22000,'sold_out'=>0
@@ -50,12 +56,11 @@ return array(
 			'discount' => '61%','price'=>25843,'special_price'=>10000,'sold_out'=>0
 			),	
 		array(
-			'name' => "House of Wow - BollyHop Workshop (Couple)",
+			'name' => "House of Wow - BollyHop Workshop (2 People)",
 			'image' => 'http://b.fitn.in/global/fitmania/12/onlineimageresize_com_HOW2.jpg',
 			'location' => 'Bandra',
 			'discount' => '50%','price'=>900,'special_price'=>450,'sold_out'=>0
 			),	
-
 
 		),
 
