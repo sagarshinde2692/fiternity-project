3f444765b654912758d660e60a611b1481ca967f
Sanjay Sahu <sanjay.id7@gmail.com>
2015-03-04 15:56:02 +0530

    test mail

--- app/views/emails/testemail.blade.php
+++ app/views/emails/testemail.blade.php
@@ -1,29 +1,17 @@
 <html>
 <body>
-	<table style="width:100%">
-		<tr>
-			<td style="display:block !important;
-			max-width:600px !important; 
-			margin:0 auto !important;
-			clear:both !important;">
-			<div style="padding:15px;max-width:100% !important;margin:0 auto; display:block;">
-				<table>
-					<tr>
-						<td>
-							<p style="font-size:16px;color:#464646;text-align:justify;">Hello,<br>This is a test email.</p>
-							<br>
-							
-							<p style="font-size:16px;color:#464646;text-align:justify;">
-								Thank you.								
-							</p>						
-						</td>
-					</tr>
-				</table>
-			</div>
+	
+	<p style="font-size:16px;color:blue;text-align:justify;">
 
-		</td>
-	</tr>
-</table>
+		Hello,
+		<br> <br>
+
+		This is a test email.
+		<br> <br>
+
+		Thank you.		
+	</p>						
+
 
 </body>
 </html>
