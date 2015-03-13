b99c66c2b459044df50a2bbce8de3642ef27b2f6
Sanjay Sahu <sanjay.id7@gmail.com>
2015-03-13 18:36:30 +0530

    updates

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -80,9 +80,9 @@ class SchedulebooktrialsController extends \BaseController {
 	public function getBookTrial($finderid,$date = null){
 		$finderid 	= 	(int) $finderid;
 		$items 		= 	Booktrial::where('finder_id', '=', $finderid)
-		->where('service_name', '=', 'gyms' )
-		->where('schedule_date', '=', new DateTime($date) )
-		->get(array('customer_name','service_name','finder_id','schedule_date','schedule_slot'));
+								->where('service_name', '=', 'gyms' )
+								->where('schedule_date', '=', new DateTime($date) )
+								->get(array('customer_name','service_name','finder_id','schedule_date','schedule_slot'));
 		return $items;
 	}
 
