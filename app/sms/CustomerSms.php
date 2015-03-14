506ec67ad18e58ef76671ebda5755e48c91863b9
Sanjay Sahu <sanjay.id7@gmail.com>
2015-03-14 16:08:05 +0530

    updates for sms

--- app/sms/CustomerSms.php
+++ app/sms/CustomerSms.php
@@ -41,7 +41,8 @@ Class CustomerSms extends VersionNextSms{
 	public function bookTrialReminderBefore1Hour ($data, $delay){
 
 		$to 		=  	array_merge(explode(',', $data['customer_phone']));
-		$message 	=	"Hey ".ucwords($data['customer_name']).". Hope you are ready for your session at ".ucwords($data['finder_name']).", ".ucwords($data['finder_location']).". Please note the address: ".ucwords($data['finder_name']).", ".ucwords($data['finder_address']).", ".ucwords($data['finder_location']).". Contact person: ".ucwords($data['finder_poc_for_customer_name']).". Have a great workout!";
+		//$message 	=	"Hey ".ucwords($data['customer_name']).". Hope you are ready for your session at ".ucwords($data['finder_name']).", ".ucwords($data['finder_location']).". Please note the address: ".ucwords($data['finder_name']).", ".ucwords($data['finder_address']).", ".ucwords($data['finder_location']).". Contact person: ".ucwords($data['finder_poc_for_customer_name']).". Have a great workout!";
+		$message 	=	"Hey ".ucwords($data['customer_name']).". Hope you are ready for your session at ".ucwords($data['finder_name']).", ".ucwords($data['finder_location']).". For address please refer to ".  {{ link_to('http://www.fitternity.com/'.$finder_slug, ucwords($finder_name) ) }} .". Contact person: ".ucwords($data['finder_poc_for_customer_name']).". Have a great workout!";
 		
 		return $this->sendTo($to, $message, $delay);
 	}
