73184af3e5ebfa78fe29c67dccdc54ecae8eb6b6
Sanjay Sahu <sanjay.id7@gmail.com>
2015-03-20 22:34:38 +0530

    updates

--- app/views/emails/customer/bk/autobooktrial.blade.php
+++ app/views/emails/customer/bk/autobooktrial.blade.php
@@ -5,13 +5,13 @@
 		<p style="font-size:16px;color:#464646;text-align:justify;"> Hey, {{ ucwords($customer_name) }}</p> 
 		<p> Thank you for booking a workout session / trial at {{ ucwords($finder_name) }} @if ($show_location_flag) ,  {{ ucwords($finder_location) }} @endif through Fitternity.com. Your session is CONFIRMED. Here are the details. </p>
 		<table border="1" bordercolor="#2c3e50" align="center" cellspacing="0" width="550" style="margin:5px 0px 15px 0px;">
+			<tr><td>Session booked for:		</td><td>{{	ucwords($customer_name)	}}</td></tr>
 			<tr><td>Name of the fitness service provider: </td><td>{{ ucwords($finder_name) }}</td></tr>
 			<tr><td>Date: </td><td>{{ date(' jS F\, Y \(l\) ', strtotime($schedule_date_time) )  }}</td></tr>
 			<tr><td>Time: </td><td>{{ date(' g\.i A', strtotime($schedule_date_time) ) }}</td></tr>
 			<tr><td>Workout form (if any):	</td><td>{{	ucwords($service_name)	}}</td></tr>
 			<tr><td>Workout type: </td><td> Trial </td></tr>
 			<tr><td>Subscription Code: </td><td> {{ $code }}(please flash this code at the service provider location) </td></tr>
-			<tr><td>Session booked for:		</td><td>{{	ucwords($customer_name)	}}</td></tr>
 		</table> 
 
 		<p><b>How to get there?</b></p>
