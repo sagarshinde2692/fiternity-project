1f0be12f96d04954f150e7f0986e07408f6bf2e5
Sanjay Sahu <sanjay.id7@gmail.com>
2015-03-21 04:33:03 +0530

    updates

--- app/views/emails/finder/cancelbooktrial.blade.php
+++ app/views/emails/finder/cancelbooktrial.blade.php
@@ -293,10 +293,7 @@
                      {{ ucwords($service_name)  }}</td>
                       </tr> 
                      
-                     <tr>
-                     <td style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; text-align:left;line-height: 20px; vertical-align:text-top; color: #333; font-weight:500" st-title="3col-title1">
-                     Trial</td>
-                      </tr>
+                     
                        <tr>
                       <td style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; text-align:left;line-height: 20px; vertical-align:text-top; color: #ec8601;" st-title="3col-title1">
                      <p style="font-weight:light">  Subscription Code:  
