49bb50d81b49971ffce55d03b0dc596892c5c611
Sanjay Sahu <sanjay.id7@gmail.com>
2015-03-25 15:09:23 +0530

    updates

--- app/routes.php
+++ app/routes.php
@@ -79,39 +79,48 @@ Route::get('/testpushemail', function() {
 	// var_dump($delaytime);
 	// exit;
 	
-	$messageid1 =  Mail::queue($email_template, $email_template_data, function($message) use ($email_message_data){
-		$message->to($email_message_data['to'], $email_message_data['reciver_name'])
-		->bcc($email_message_data['bcc_emailids'])
-		->subject($email_message_data['email_subject'].' from instant -- '.date( "Y-m-d H:i:s", time()));
-	});
+	for ($i=0; $i < 100; $i++) { 
+		$id =  Mail::queue($email_template, $email_template_data, function($message) use ($email_message_data){
+			$message->to($email_message_data['to'], $email_message_data['reciver_name'])
+			->bcc($email_message_data['bcc_emailids'])
+			->subject($email_message_data['email_subject'].' from instant -- '.date( "Y-m-d H:i:s", time()));
+		});
 
+		echo "<br> $i ------   id -- $id <br>";
+	}
+
+	// $messageid1 =  Mail::queue($email_template, $email_template_data, function($message) use ($email_message_data){
+	// 		$message->to($email_message_data['to'], $email_message_data['reciver_name'])
+	// 		->bcc($email_message_data['bcc_emailids'])
+	// 		->subject($email_message_data['email_subject'].' from instant -- '.date( "Y-m-d H:i:s", time()));
+	// 	});
 	// return var_dump($messageid1);
 
-	$messageid2 = Mail::later(Carbon::now()->addMinutes(5), $email_template, $email_template_data, function($message) use ($email_message_data){
-		$message->to($email_message_data['to'], $email_message_data['reciver_name'])
-		->bcc($email_message_data['bcc_emailids'])
-		->subject($email_message_data['email_subject'].' delay by 5 min -- '.date( "Y-m-d H:i:s", time()));
-	});
+	// $messageid2 = Mail::later(Carbon::now()->addMinutes(5), $email_template, $email_template_data, function($message) use ($email_message_data){
+	// 	$message->to($email_message_data['to'], $email_message_data['reciver_name'])
+	// 	->bcc($email_message_data['bcc_emailids'])
+	// 	->subject($email_message_data['email_subject'].' delay by 5 min -- '.date( "Y-m-d H:i:s", time()));
+	// });
 
-	$messageid3 = Mail::later(Carbon::now()->addMinutes(10), $email_template, $email_template_data, function($message) use ($email_message_data){
-		$message->to($email_message_data['to'], $email_message_data['reciver_name'])
-		->bcc($email_message_data['bcc_emailids'])
-		->subject($email_message_data['email_subject'].' delay by 10 min -- '.date( "Y-m-d H:i:s", time()));
-	});
+	// $messageid3 = Mail::later(Carbon::now()->addMinutes(10), $email_template, $email_template_data, function($message) use ($email_message_data){
+	// 	$message->to($email_message_data['to'], $email_message_data['reciver_name'])
+	// 	->bcc($email_message_data['bcc_emailids'])
+	// 	->subject($email_message_data['email_subject'].' delay by 10 min -- '.date( "Y-m-d H:i:s", time()));
+	// });
 
-	$messageid4 = Mail::later(Carbon::now()->addMinutes(15), $email_template, $email_template_data, function($message) use ($email_message_data){
-		$message->to($email_message_data['to'], $email_message_data['reciver_name'])
-		->bcc($email_message_data['bcc_emailids'])
-		->subject($email_message_data['email_subject'].' delay by 15 min -- '.date( "Y-m-d H:i:s", time()));
-	});
+	// $messageid4 = Mail::later(Carbon::now()->addMinutes(15), $email_template, $email_template_data, function($message) use ($email_message_data){
+	// 	$message->to($email_message_data['to'], $email_message_data['reciver_name'])
+	// 	->bcc($email_message_data['bcc_emailids'])
+	// 	->subject($email_message_data['email_subject'].' delay by 15 min -- '.date( "Y-m-d H:i:s", time()));
+	// });
 
-	$messageid5 = Mail::later(Carbon::now()->addMinutes(25), $email_template, $email_template_data, function($message) use ($email_message_data){
-		$message->to($email_message_data['to'], $email_message_data['reciver_name'])
-		->bcc($email_message_data['bcc_emailids'])
-		->subject($email_message_data['email_subject'].' delay by 25 min -- '.date( "Y-m-d H:i:s", time()));
-	});
+	// $messageid5 = Mail::later(Carbon::now()->addMinutes(25), $email_template, $email_template_data, function($message) use ($email_message_data){
+	// 	$message->to($email_message_data['to'], $email_message_data['reciver_name'])
+	// 	->bcc($email_message_data['bcc_emailids'])
+	// 	->subject($email_message_data['email_subject'].' delay by 25 min -- '.date( "Y-m-d H:i:s", time()));
+	// });
 
-	echo "<br>messageid1 -- $messageid1 <br>messageid2 -- $messageid2 <br>messageid3 -- $messageid3<br>messageid4  -- $messageid4 <br>messageid5 -- $messageid5";
+	// echo "<br>messageid1 -- $messageid1 <br>messageid2 -- $messageid2 <br>messageid3 -- $messageid3<br>messageid4  -- $messageid4 <br>messageid5 -- $messageid5";
 	// // sleep(60 * 5);
 	// echo "<br>messageid1 -- $messageid1 <br>messageid2 -- $messageid2 <br>messageid3 -- $messageid3";
 	// echo $deleteid = Queue::deleteMessage('app',$messageid2);
