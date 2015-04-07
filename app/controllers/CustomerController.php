79af23002a8c8e47fe11522dc98c14787450631e
Sanjay Sahu <sanjay.id7@gmail.com>
2015-04-07 17:56:12 +0530

    updates

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -18,8 +18,7 @@ class CustomerController extends \BaseController {
 	public function getAutoBookTrials($customeremail){
 
 		$selectfields 	=	array('finder', 'finder_id','finder_name','finder_slug','service_name','schedule_date','schedule_slot_start_time','schedule_slot_end_time','code');
-		$trials 		=	Booktrial::with('finder')
-									  with(array('finder'=>function($query){$query->select('_id','lon', 'lat', 'contact.address','finder_poc_for_customer_mobile', 'finder_poc_for_customer_name');}))
+		$trials 		=	Booktrial::with(array('finder'=>function($query){$query->select('_id','lon', 'lat', 'contact.address','finder_poc_for_customer_mobile', 'finder_poc_for_customer_name');}))
 									  ->where('customer_email', '=', $customeremail)->where('going_status', '=', 1)->orderBy('_id', 'desc')->get($selectfields)->toArray();
 
 		if(count($trials) < 1){
