a7353d7498332bc93078f6175704660d908e0c54
Sanjay Sahu <sanjay.id7@gmail.com>
2015-04-07 17:54:42 +0530

    updates

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -19,8 +19,8 @@ class CustomerController extends \BaseController {
 
 		$selectfields 	=	array('finder', 'finder_id','finder_name','finder_slug','service_name','schedule_date','schedule_slot_start_time','schedule_slot_end_time','code');
 		$trials 		=	Booktrial::with('finder')
-									// ->with(array('finder'=>function($query){$query->select('_id','name','slug','related_finder_title','detail_rating');}))
-									->where('customer_email', '=', $customeremail)->where('going_status', '=', 1)->orderBy('_id', 'desc')->get($selectfields)->toArray();
+									  with(array('finder'=>function($query){$query->select('_id','lon', 'lat', 'contact.address','finder_poc_for_customer_mobile', 'finder_poc_for_customer_name');}))
+									  ->where('customer_email', '=', $customeremail)->where('going_status', '=', 1)->orderBy('_id', 'desc')->get($selectfields)->toArray();
 
 		if(count($trials) < 1){
 			$resp 	= 	array('status' => 200,'trials' => $trials,'message' => 'No trials scheduled yet :)');
