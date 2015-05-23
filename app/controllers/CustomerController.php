97b169b69ab27ce06cea944c0c663e9fe5a33a29
Sanjay Sahu <sanjay.id7@gmail.com>
2015-05-23 18:30:14 +0530

    updates

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -26,10 +26,10 @@ class CustomerController extends \BaseController {
 		$selectfields 	=	array('finder', 'finder_id', 'finder_name', 'finder_slug', 'service_name', 'schedule_date', 'schedule_slot_start_time', 'schedule_date_time', 'schedule_slot_end_time', 'code', 'going_status', 'going_status_txt');
 
 		$trials 		=	Booktrial::with(array('finder'=>function($query){$query->select('_id','lon', 'lat', 'contact.address','finder_poc_for_customer_mobile', 'finder_poc_for_customer_name');}))
-		->where('customer_email', '=', $customeremail)
-		->whereIn('booktrial_type', array('auto'))
-		->orderBy('_id', 'desc')
-		->get($selectfields)->toArray();
+										->where('customer_email', '=', $customeremail)
+										->whereIn('booktrial_type', array('auto'))
+										->orderBy('_id', 'desc')
+										->get($selectfields)->toArray();
 
 		if(count($trials) < 1){
 
@@ -63,11 +63,11 @@ class CustomerController extends \BaseController {
 
 		$selectfields 	=	array('finder', 'finder_id', 'finder_name', 'finder_slug', 'service_name', 'schedule_date', 'schedule_slot_start_time', 'schedule_date_time', 'schedule_slot_end_time', 'code', 'going_status', 'going_status_txt');
 		$trials 		=	Booktrial::with(array('finder'=>function($query){$query->select('_id','lon', 'lat', 'contact.address','finder_poc_for_customer_mobile', 'finder_poc_for_customer_name');}))
-		->where('customer_email', '=', $customeremail)
-		->where('fitcard_user', 1)
-		->whereIn('booktrial_type', array('auto'))
-		->orderBy('_id', 'desc')
-		->get($selectfields)->toArray();
+							->where('customer_email', '=', $customeremail)
+							->where('fitcard_user', 1)
+							->whereIn('booktrial_type', array('auto'))
+							->orderBy('_id', 'desc')
+							->get($selectfields)->toArray();
 
 		if(count($trials) < 1){
 
@@ -222,6 +222,9 @@ class CustomerController extends \BaseController {
 			);
 
 		$order 			= 	new Order($data);
+		
+		$order->_id 		= 	$orderid;
+
 		$orderstatus   		= 	$order->save();
 
 		$resp 	= 	array('status' => 200, 'order' => $order, 'message' => "Transaction details for tmp fitcard buy :)");
