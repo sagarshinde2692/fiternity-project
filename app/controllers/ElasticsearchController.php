435a48c97d18fa3d2ce354f0c99b17243846f280
Sanjay Sahu <sanjay.id7@gmail.com>
2015-06-17 12:38:17 +0530

    updates

--- app/controllers/ElasticsearchController.php
+++ app/controllers/ElasticsearchController.php
@@ -287,7 +287,7 @@ class ElasticsearchController extends \BaseController {
 							->with('facilities')
 							->active()
 							->orderBy('_id')
-				            ->take(2)
+				            // ->take(2)
 							->get();
 			break;
 
