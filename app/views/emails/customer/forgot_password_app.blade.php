b56dccd7a5ae897a93c274c474c151b941c18489
Mahesh Jadhav <mjmjadhav@gmail.com>
2015-06-30 13:35:34 +0530

    debug otp email

--- app/views/emails/customer/forgot_password_app.blade.php
+++ app/views/emails/customer/forgot_password_app.blade.php
@@ -1,2 +1,17 @@
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><META http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>hello got email</body></html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><META http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>
+
+<p style="padding-top:30px;font-weight:bold;color:#5f5e5e;font-size:20px"><span style="color:#5f5e5e;font-weight:400">
+Hello {{$name}},</span></p>      
+                
+             
+              <p style="padding-top:15px;line-height:20px;font-weight:lighter;color:#5f5e5e">The authorisation code required for resetting your password is {{otp}}.</p>
+
+
+<p style="padding-top:15px;line-height:20px;font-weight:lighter;color:#5f5e5e">If you didn&#39;t request this, you can safely ignore this email.</p>
+
+<p style="padding-top:15px;line-height:20px;font-weight:lighter;color:#5f5e5e">Your password won&#39;t change until you use the authorisation code.</p>
+ 
+ <p style="padding-top:25px;padding-bottom:25px;line-height:20px;font-weight:lighter;color:#5f5e5e">Regards, <br>Team Fitternity</p>
+
+ </body></html>
