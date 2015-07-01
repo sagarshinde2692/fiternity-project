9df6ceb75814a4663fda50b6c5d043f1f11ef221
Sanjay Sahu <sanjay.id7@gmail.com>
2015-07-01 14:57:56 +0530

    updates for query

--- app/models/Basemodel.php
+++ app/models/Basemodel.php
@@ -5,12 +5,6 @@ class Basemodel extends \Moloquent {
 
 	protected $guarded = array();
 	
-
-	public function setIdAttribute($value){
-		
-		$this->attributes['_id'] = intval($value);
-	}
-	
 	public function scopeActive ($query){
 
 		return $query->where('status','=','1');
