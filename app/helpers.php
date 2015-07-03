e7d2eeeb599ed277d28984b2c7451a7799a45f39
Sanjay Sahu <sanjay.id7@gmail.com>
2015-07-03 12:33:35 +0530

    updates for search

--- app/helpers.php
+++ app/helpers.php
@@ -83,6 +83,7 @@ if (!function_exists('get_elastic_finder_document')) {
                 'contact'                       =>      (isset($data['contact'])) ? $data['contact'] : '',
                 'coverimage'                    =>      (isset($data['coverimage'])) ? $data['coverimage'] : '',
                 'finder_type'                   =>      (isset($data['finder_type'])) ? $data['finder_type'] : '',
+                'commercial_type'               =>      (isset($data['commercial_type'])) ? $data['commercial_type'] : '',
                 'fitternityno'                  =>      (isset($data['fitternityno'])) ? $data['fitternityno'] : '',
                 'facilities'                    =>      (isset($data['facilities']) && !empty($data['facilities'])) ? array_map('strtolower',array_pluck($data['facilities'],'name')) : "",
                 'logo'                          =>      (isset($data['logo'])) ? $data['logo'] : '',
