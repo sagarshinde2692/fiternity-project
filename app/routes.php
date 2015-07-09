32dc225a2137f3caa5c821518e568cba350e3995
Mahesh Jadhav <mjmjadhav@gmail.com>
2015-07-09 16:33:07 +0530

    changes

--- app/routes.php
+++ app/routes.php
@@ -545,7 +545,7 @@ Route::get('/fitmania', 'FitmaniaController@getMockData');
 ##############################################################################
 /******************** ORDERS SECTION START HERE ***********************/
 
-Route::get('/callvendor',  array('as' => 'ozonetel.callvendor','uses' => 'OzonetelController@callVendor'));
+Route::post('/callvendor',  array('as' => 'ozonetel.callvendor','uses' => 'OzonetelController@callVendor'));
 
 
 /******************** ORDERS SECTION END HERE ********************/
