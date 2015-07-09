12c83a672f8c5df2cb87aa4342ee374c4217cad3
Mahesh Jadhav <mjmjadhav@gmail.com>
2015-07-09 19:17:13 +0530

    changes

--- app/controllers/OzonetelController .php
+++ app/controllers/OzonetelController .php
@@ -24,11 +24,11 @@ class OzonetelController extends \BaseController {
 	public function callVendor(){
 
 		//echo"<pre>";print_r($_REQUEST);exit;
-		Log::info('ozone',$_REQUEST);
+		Log::info('ozone','asdfasdfasdfasf');
 
 		if (isset($_REQUEST['event']) && $_REQUEST['event'] == 'NewCall') {
 		    $this->ozonetel->addPlayText("Please wail while we connecting");
-		    $this->ozonetel->addDial("09920864894"); //phone number to dial
+		    $this->ozonetel->addDial("09920864894","true"); //phone number to dial
 		} elseif (isset($_REQUEST['event']) && $_REQUEST['event'] == 'Dial') {
 		    if ($_REQUEST['status'] == 'answered') {
 		    	$this->ozonetel->addRecord("recordFileName");
@@ -37,9 +37,6 @@ class OzonetelController extends \BaseController {
 		        $this->ozonetel->addPlayText("dialled number is not answered");
 		    }
 		    $this->ozonetel->addHangup();
-		} elseif (isset($_REQUEST['event']) && $_REQUEST['event'] == 'Record') {
-        	$this->ozonetel->addPlayText("your recorded message is ");
-        	$this->ozonetel->addPlayAudio($_REQUEST['data']);
 		}else {
 		    $this->ozonetel->addHangup();
 		}
