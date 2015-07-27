86d11456a64d3b4f35f01144d5019ce7da86a071
Sanjay Sahu <sanjay.id7@gmail.com>
2015-07-27 13:47:53 +0530

    updates

--- app/views/emails/order/fitmanianew_offer_template.blade.php
+++ app/views/emails/order/fitmanianew_offer_template.blade.php
@@ -156,16 +156,26 @@ Hi {{ $customer_name }}!</span></p>
 
  <p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;">Subscription number:</span> {{ $_id }}
 
- <p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;" href="mailto:info@fitternity.com">Membership details:</span> {{ ucwords($service_name) }} {{ ucwords($service_duration) }}  at {{ ucwords($finder_name) }} 
+ <p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;" href="mailto:info@fitternity.com">Membership details:</span> ucwords($service_name) }} {{ ucwords($service_duration) }}  at {{ ucwords($finder_name) }} 
 
  <p style="padding-top:25px; line-height:20px; font-weight:lighter; color:#5f5e5e"><span style="color:#ec8601; font-weight:600;">2. Start the membership</span>
 
- <p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#222;">On your membership start date - visit <span style="color:#ec8601; font-weight:450;"> {{ ucwords($finder_name) }}  {{ ucwords($location) }}</span> and flash the subscription number.
+ <p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#222;">Call the gym / studio where you have purchased the membership on the number below and decide your starting date with them (they have been informed about your purchase) </span> and flash the subscription number.
+
+ <p style="padding-top:10px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;">Location:</span> {{ ucwords($location) }}.
  
  <p style="padding-top:10px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;">Address:</span> {{ ucfirst($finder_address) }}
  
- <p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;">Person to contact:</span> {{ ucwords($finder_poc_for_customer_name) }} 
+ @if ($finder_poc_for_customer_name) 
+ <p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;">Contact Person:</span> {{ ucwords($finder_poc_for_customer_name) }}  
 </p>
+ @endif
+
+ @if ($finder_poc_for_customer_mobile) 
+<p style="padding-top:5px; line-height:20px; font-weight:lighter; color:#ec8601"><span style="color:#222222; font-weight:450;">Contact Number:</span> {{ ucwords($finder_poc_for_customer_mobile) }}  
+</p>
+ @endif
+
 
 <p style="padding-top:25px; line-height:20px; font-weight:lighter; color:#5f5e5e"><span style="color:#ec8601; font-weight:600;">3. Call us for help</span>
 
