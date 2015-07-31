3ef890a2ee725c13bb363cada51a73e9c0f3b271
Sanjay Sahu <sanjay.id7@gmail.com>
2015-08-01 01:27:48 +0530

    updates

--- app/controllers/FitmaniaController.php
+++ app/controllers/FitmaniaController.php
@@ -55,7 +55,7 @@ class FitmaniaController extends \BaseController {
 		}
 
 		if($city == 'mumbai'){
-			$location_cluster	=	['all','central-mumbai','south-mumbai','western-mumbai','navi-mumbai','thane'];
+			$location_cluster	=	['all','central-mumbai','south-mumbai','western-mumbai'];
 		}else{
 			$location_cluster	=	['all','pune-city', 'pimpri-chinchwad' ];
 		}	
