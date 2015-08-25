5a1baa3f4a82341feec83980cf8e75c7fcceb8e5
Sanjay Sahu <sanjay.id7@gmail.com>
2015-08-25 19:51:25 +0530

    updates

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -307,8 +307,8 @@ class FindersController extends \BaseController {
 	public function sendbooktrialdaliysummary(){
 
 		$tommorowDateTime 	=	date('d-m-Y', strtotime(Carbon::now()->addDays(1)));
-		// $finders 			=	Booktrial::where('going_status', 1)->where('schedule_date', '=', new DateTime($tommorowDateTime))->get()->groupBy('finder_id')->toArray();
-		$finders 			=	Booktrial::where('going_status', 1)->where('schedule_date', '=', new DateTime($tommorowDateTime))->where('finder_id', '=',3305)->get()->groupBy('finder_id')->toArray();
+		$finders 			=	Booktrial::where('going_status', 1)->where('schedule_date', '=', new DateTime($tommorowDateTime))->get()->groupBy('finder_id')->toArray();
+		// $finders 			=	Booktrial::where('going_status', 1)->where('schedule_date', '=', new DateTime($tommorowDateTime))->where('finder_id', '=',3305)->get()->groupBy('finder_id')->toArray();
 
 		// echo $todayDateTime 		=	date('d-m-Y', strtotime(Carbon::now()) );
 		// return $todaytrialarr 		=	Booktrial::where('going_status', 1)
@@ -343,6 +343,7 @@ class FindersController extends \BaseController {
 				$trialdata = array();
 				foreach ($trials as $key => $value) {
 					$trial = array('customer_name' => $value->customer_name, 
+						'customer_phone' => ($finderarr['share_customer_no'] == '1') ? $value->customer_phone : '',
 						'schedule_date' => date('d-m-Y', strtotime($value->schedule_date) ), 
 						'schedule_slot' => $value->schedule_slot, 
 						'code' => $value->code, 
@@ -358,6 +359,7 @@ class FindersController extends \BaseController {
 				if($todaytrialarr){
 					foreach ($todaytrialarr as $key => $value) {
 						$trial = array('customer_name' => $value->customer_name, 
+							'customer_phone' => ($finderarr['share_customer_no'] == '1') ? $value->customer_phone : '',
 							'schedule_date' => date('d-m-Y', strtotime($value->schedule_date) ), 
 							'schedule_slot' => $value->schedule_slot, 
 							'code' => $value->code, 
@@ -378,7 +380,7 @@ class FindersController extends \BaseController {
 					'scheduletrials' 				=> $trialdata,
 					'todaytrials' 					=> $todaytrialdata
 					);
-					// echo "<pre>";print_r($scheduledata); 
+				// echo "<pre>";print_r($scheduledata); 
 				$this->findermailer->sendBookTrialDaliySummary($scheduledata);
 			}	  
 		}
