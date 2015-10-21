34b29f1716b79922e64c81b0d60c6422db24f1df
fitmahesh <maheshjadhav@fitternity.com>
2015-10-21 15:38:35 +0530

    fixed ozonetel api bugs

--- app/controllers/OzonetelsController.php
+++ app/controllers/OzonetelsController.php
@@ -129,11 +129,12 @@ class OzonetelsController extends \BaseController {
 
 		if (isset($_REQUEST['event']) && $_REQUEST['event'] == 'NewCall') {
 
-		    $this->ozonetelResponse->addPlayText("This call is recorderd for quality purpose");
-
 		    $finderDetails = $this->getFinderDetails($_REQUEST['called_number']);
    
 	    	if($finderDetails){
+
+	    		$this->ozonetelResponse->addPlayText("This call is recorderd for quality purpose");
+
 	    		$phone = $finderDetails->finder->contact['phone'];
 	    		$phone = explode(',', $phone);
 	    		$contact_no = (string)trim($phone[0]);
@@ -212,9 +213,9 @@ class OzonetelsController extends \BaseController {
 		try {
 
 			if(!$extension){
-				$ozonetelno = Ozonetelno::with('finder')->where('phone_number','=',(string) $phone_number)->first();
+				$ozonetelno = Ozonetelno::with('finder')->active()->where('phone_number','=',(string) $phone_number)->where('finder_id', 'exists', true)->first();
 			}else{
-				$ozonetelno = Ozonetelno::with('finder')->where('phone_number','=',(string) $phone_number)->where('extension','=',(string) $extension)->first();
+				$ozonetelno = Ozonetelno::with('finder')->active()->where('phone_number','=',(string) $phone_number)->where('finder_id', 'exists', true)->where('extension','=',(string) $extension)->first();
 			}
 
 			if(!empty($ozonetelno)){
@@ -261,48 +262,55 @@ class OzonetelsController extends \BaseController {
 
 		$ozonetel_capture = Ozonetelcapture::where('ozonetel_unique_id','=',$data['sid'])->first();
 
-		if($finder_id){
-			$ozonetel_capture->finder_id = (int) $finder_id;
-			$ozonetel_capture->count = 0;
-		}
+		if($ozonetel_capture){
 
-		if($extension){
-			$ozonetel_capture->extension = $extension;
-		}
-
-		if($add_count){
-			$ozonetel_capture->count += 1;
-		}
-
-
-		if(isset($data['status']) && $data['status'] != ''){
-			if($data['status'] != 'answered'){
-
-				$ozonetel_capture->call_status = "not_answered";
-				$ozonetel_capture->message = $data['message'];
-				$ozonetel_capture->telco_code = $data['telco_code'];
-				$ozonetel_capture->rec_md5_checksum = $data['rec_md5_checksum'];
-				$ozonetel_capture->pickduration = $data['pickduration'];
-				
-			}else{
-				
-				$ozonetel_capture->call_status = "answered";
-				$ozonetel_capture->call_duration = $data['callduration'];
-				$ozonetel_capture->ozone_url_record = $data['data'];
-				$ozonetel_capture->message = $data['message'];
-				$ozonetel_capture->telco_code = $data['telco_code'];
-				$ozonetel_capture->rec_md5_checksum = $data['rec_md5_checksum'];
-				$ozonetel_capture->pickduration = $data['pickduration'];
-				
-				/*$aws_filename = time().$data['sid'].'.wav';
-				$ozonetel_capture->aws_file_name = $aws_filename;
-				$this->addToAws($data['data'],$aws_filename);*/
+			if($finder_id){
+				$ozonetel_capture->finder_id = (int) $finder_id;
+				$ozonetel_capture->count = 0;
 			}
+
+			if($extension){
+				$ozonetel_capture->extension = $extension;
+			}
+
+			if($add_count){
+				$ozonetel_capture->count += 1;
+			}
+
+
+			if(isset($data['status']) && $data['status'] != ''){
+				if($data['status'] != 'answered'){
+
+					$ozonetel_capture->call_status = "not_answered";
+					$ozonetel_capture->message = $data['message'];
+					$ozonetel_capture->telco_code = $data['telco_code'];
+					$ozonetel_capture->rec_md5_checksum = $data['rec_md5_checksum'];
+					$ozonetel_capture->pickduration = $data['pickduration'];
+					
+				}else{
+					
+					$ozonetel_capture->call_status = "answered";
+					$ozonetel_capture->call_duration = $data['callduration'];
+					$ozonetel_capture->ozone_url_record = $data['data'];
+					$ozonetel_capture->message = $data['message'];
+					$ozonetel_capture->telco_code = $data['telco_code'];
+					$ozonetel_capture->rec_md5_checksum = $data['rec_md5_checksum'];
+					$ozonetel_capture->pickduration = $data['pickduration'];
+					
+					/*$aws_filename = time().$data['sid'].'.wav';
+					$ozonetel_capture->aws_file_name = $aws_filename;
+					$this->addToAws($data['data'],$aws_filename);*/
+				}
+			}
+
+			$ozonetel_capture->update();
+
+			return $ozonetel_capture;
+
+		}else{
+
+			return false;
 		}
-
-		$ozonetel_capture->update();
-
-		return $ozonetel_capture;
 	}
 
 	public function getCapture($sid){
