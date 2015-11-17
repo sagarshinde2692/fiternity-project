7527affcd736ac2624a9558aa790c33feccd6a35
fitajay <ajaysharma@fitternity.com>
2015-11-17 18:40:17 +0530

    bulk reload api globalsearch

--- app/routes.php
+++ app/routes.php
@@ -424,6 +424,9 @@ Route::get('cron/monitor/{days}',  array('as' => 'cron.monitor','uses' => 'CronC
 
 Route::get('pushcategorylocations', 'GlobalPushController@pushcategorylocations');
 Route::get('pushfinders', 'GlobalPushController@pushFinders');
-
+Route::get('pushcategorywithfacilities', 'GlobalPushController@pushcategorywithfacilities');
+Route::get('pushcategoryoffering', 'GlobalPushController@pushcategoryoffering');
+Route::get('pushcategoryofferinglocation', 'GlobalPushController@pushcategoryofferinglocation');
+Route::get('pushcategoryfacilitieslocation', 'GlobalPushController@pushcategoryfacilitieslocation');
 /******************  GLOBALSEARCH BULK PUSH END HERE************************************************/
 #####################################################################################################
