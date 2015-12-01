ec5d0a4d76e25a80a4d9847400f190129fd9bf7e
fitmahesh <maheshjadhav@fitternity.com>
2015-12-01 15:42:31 +0530

    changes

--- app/services/OzontelOutboundCall.php
+++ app/services/OzontelOutboundCall.php
@@ -28,10 +28,10 @@ Class OzontelOutboundCall {
         $api_key = 'KK6cb3903e3d2c428bb60c0cfaa212009e';
         $phone_no = '9920864894';
         $outbound_version = '2';
-        $extra_data = '<response><playtext>Welcome To Fitternity</playtext></response>';
+        //$extra_data = '<response><playtext>Welcome To Fitternity</playtext></response>';
         $url = 'http://apistg.fitn.in/ozonetel/outboundcallrecive/13333';
 
-        $url_pass = 'api_key='.$api_key.'&phone_no='.$phone_no.'&outbound_version='.$outbound_version.'&extra_data='.$extra_data.'&url='.$url;
+        $url_pass = 'api_key='.$api_key.'&phone_no='.$phone_no.'&outbound_version='.$outbound_version.'&url='.$url;
 
         try {
             $response = $this->client->get($url_pass)->getBody()->getContents();
