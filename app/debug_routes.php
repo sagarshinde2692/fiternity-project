f33a8f54f99fe0c0e87831037af8d8bc971f5528
sanjay sahu <sanjay.id7@gmail.com>
2015-12-14 13:45:32 +0530

    updates

--- app/debug_routes.php
+++ app/debug_routes.php
@@ -79,7 +79,7 @@ Route::get('exportcustomer', function() {
 
 		if(isset($value['finder_id']) && $value['finder_id'] != '5000'){
 			$finder = Finder::with('city')->with('location')->find(intval($value['finder_id']));
-			$finder_city = $finder->city->name;
+			$finder_city = (isset($finder->city->name) && $finder->city->name !="") ? $finder->city->name : "-"; 
 		}else{
 			$city = City::find(intval($value['city_id']));
 			$finder_city = $city->name;
