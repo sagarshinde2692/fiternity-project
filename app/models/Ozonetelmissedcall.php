2e984d7491cce1680fa241f91e9681064c5bff92
fitmahesh <maheshjadhav@fitternity.com>
2015-12-25 18:55:33 +0530

    fitmania missed call sms

---
+++ app/models/Ozonetelmissedcall.php
@@ -0,0 +1,14 @@
+<?php
+
+/** 
+ * ModelName : Customer.
+ * Maintains a list of functions used for Customer.
+ *
+ * @author Mahesh Jadhav <mjmjadhav@gmail.com>
+ */
+
+class Ozonetelmissedcall extends  \Basemodel {
+
+	protected $collection = "ozonetelmissedcall";
+
+}
