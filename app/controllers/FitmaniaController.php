a83179a329cb5c24e2c824eaacc919008009f47b
sanjay sahu <sanjay.id7@gmail.com>
2016-01-10 00:43:51 +0530

    updates

--- app/controllers/FitmaniaController.php
+++ app/controllers/FitmaniaController.php
@@ -31,7 +31,7 @@ class FitmaniaController extends \BaseController {
 
 	public function categoryId($category){
 
-		$categorydays_arr     =  array('surprise' => 'all', 'mix bag' => 'all', 'mix bag' => 'all', 'zumba' => '19', 'gym' => '65', 'crossfit' => '111,5','mma' => '3', 'dance' => '2', 'yoga' => '1,4');
+		$categorydays_arr     =  array('surprise' => '19,65,111,5,3,2,1,4', 'personal trainers' => 'all', 'mix bag' => '19,65,111,5,3,2,1,4', 'zumba' => '19', 'gym' => '65', 'crossfit' => '111,5','mma' => '3', 'dance' => '2', 'yoga' => '1,4');
 		return $categorydays_arr[$category];
 	}
 
