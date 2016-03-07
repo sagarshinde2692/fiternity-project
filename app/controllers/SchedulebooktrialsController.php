28bf4cffd0d81ba4e9b9eb8522c19728ed3f943e
fitmahesh <maheshjadhav@fitternity.com>
2016-03-07 20:47:19 +0530

    cahgnes

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -1314,7 +1314,7 @@ class SchedulebooktrialsController extends \BaseController {
 			$finer_sms_messageids['instant'] 		= 	$sndInstantSmsFinder;
 
 			//ozonetel outbound calls
-			$customer_ozonetel_outbound = $this->ozonetelOutbound($booktrialdata,$schedule_date_starttime);
+			$customer_ozonetel_outbound = 'asdfasdf'; //$this->ozonetelOutbound($booktrialdata,$schedule_date_starttime);
 
 			//Send Reminder Notiication (Email, Sms) Before 12 Hour To Customer
 			if($twelveHourDiffInMin >= (12 * 60)){
