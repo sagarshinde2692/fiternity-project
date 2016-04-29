b403267db3861d787df58a37b3beb64d003c5a4e
fitmahesh <maheshjadhav@fitternity.com>
2016-04-29 21:18:43 +0530

    changes

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -113,6 +113,19 @@ class OrderController extends \BaseController {
 				$after3days = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $preferred_starting_date)->addMinutes(60 * 24 * 3);
 				$after10days = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $preferred_starting_date)->addMinutes(60 * 24 * 10);
 
+				$category_slug = "no_category";
+
+				if(isset($order->finder_category_id) && $order->finder_category_id != ""){
+
+					$finder_category_id = $order->finder_category_id;
+
+					$category = Findercategory::find((int)$finder_category_id);
+
+					if($category){
+						$category_slug = $category->slug;
+					}
+				}
+
 				if(isset($order->ratecard_id) || isset($order->duration_day)){
 
 					$validity = 0;
@@ -185,12 +198,18 @@ class OrderController extends \BaseController {
 						$order_data['missedcall2'] = $alreadyextended;
 						$order_data['missedcall3'] = $explore;
 
+						$order_data['category_array'] = $this->getCategoryImage($category_slug);
+
 						$order->customer_email_renewal = $this->customermailer->orderRenewalMissedcall($order_data,$renewal_date);
 						$order->customer_sms_renewal = $this->customersms->orderRenewalMissedcall($order_data,$renewal_date);
 
 					}
 
 				}
+
+				$order_data = $order->toArray();
+
+				$order_data['category_array'] = $this->getCategoryImage($category_slug);
 
 				$order->customer_sms_after3days = $this->customersms->orderAfter3Days($order_data,$after3days);
 				$order->customer_email_after10days = $this->customermailer->orderAfter10Days($order_data,$after10days);
@@ -892,6 +911,27 @@ class OrderController extends \BaseController {
 
 		return "email send";
 
+
+	}
+
+	public function getCategoryImage($category = "no_category"){
+
+		$category_array['gyms'] = array('personal-trainers'=>'http://email.fitternity.com/229/personal.jpg','sport-nutrition-supliment-stores'=>'http://email.fitternity.com/229/nutrition.jpg','yoga'=>'http://email.fitternity.com/229/yoga.jpg');
+	    $category_array['zumba'] = array('gyms'=>'http://email.fitternity.com/229/gym.jpg','dance'=>'http://email.fitternity.com/229/dance.jpg','healthy-tiffins'=>'http://email.fitternity.com/229/healthy-tiffin.jpg');
+	    $category_array['yoga'] = array('pilates'=>'http://email.fitternity.com/229/pilates.jpg','personal-trainers'=>'http://email.fitternity.com/229/personal.jpg','marathon-training'=>'http://email.fitternity.com/229/marathon.jpg');
+	    $category_array['pilates'] = array('yoga'=>'http://email.fitternity.com/229/yoga.jpg','healthy-tiffins'=>'http://email.fitternity.com/229/healthy-tiffin.jpg','marathon-training'=>'http://email.fitternity.com/229/marathon.jpg');
+	    $category_array['cross-functional-training'] = array('sport-nutrition-supliment-stores'=>'http://email.fitternity.com/229/nutrition.jpg','personal-trainers'=>'http://email.fitternity.com/229/personal.jpg','healthy-tiffins'=>'http://email.fitternity.com/229/healthy-tiffin.jpg');
+	    $category_array['crossfit'] = array('yoga'=>'http://email.fitternity.com/229/yoga.jpg','healthy-tiffins'=>'http://email.fitternity.com/229/healthy-tiffin.jpg','sport-nutrition-supliment-stores'=>'http://email.fitternity.com/229/nutrition.jpg');
+	    $category_array['dance'] = array('zumba'=>'http://email.fitternity.com/229/zumba.jpg','mma-and-kick-boxing'=>'http://email.fitternity.com/229/mma&kickboxing.jpg','spinning-and-indoor-cycling'=>'http://email.fitternity.com/229/spinning.jpg');
+	    $category_array['mma-and-kick-boxing'] = array('personal-trainers'=>'http://email.fitternity.com/229/personal.jpg','healthy-tiffins'=>'http://email.fitternity.com/229/healthy-tiffin.jpg','cross-functional-training'=>'http://email.fitternity.com/229/cross-functional.jpg');
+	    $category_array['spinning-and-indoor-cycling'] = array('gyms'=>'http://email.fitternity.com/229/gym.jpg','dietitians-and-nutritionists'=>'http://email.fitternity.com/229/dietitians.jpg','yoga'=>'http://email.fitternity.com/229/yoga.jpg');
+	    $category_array['marathon-training'] = array('dietitians-and-nutritionists'=>'http://email.fitternity.com/229/dietitians.jpg','yoga'=>'http://email.fitternity.com/229/yoga.jpg','cross-functional-training'=>'http://email.fitternity.com/229/cross-functional.jpg');
+
+	    if(array_key_exists($category,$category_array)){
+	    	return $category_array[$category];
+	    }else{
+	    	return array('gyms'=>'http://email.fitternity.com/229/gym.jpg','dance'=>'http://email.fitternity.com/229/dance.jpg','yoga'=>'http://email.fitternity.com/229/yoga.jpg');
+	    }	
 
 	}
 
