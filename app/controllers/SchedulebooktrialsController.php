116834d64cfcad98682f5803c290f638cf46280f
fitmahesh <maheshjadhav@fitternity.com>
2016-05-12 18:59:56 +0530

    get only active service

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -118,7 +118,7 @@ class SchedulebooktrialsController extends \BaseController {
         $timestamp 				= 	strtotime($date);
         $weekday 				= 	strtolower(date( "l", $timestamp));
 
-        $items = Service::where('finder_id', '=', $finderid)->get(array('_id','name','finder_id', 'trialschedules', 'workoutsessionschedules'))->toArray();
+        $items = Service::active()->where('finder_id', '=', $finderid)->get(array('_id','name','finder_id', 'trialschedules', 'workoutsessionschedules'))->toArray();
         if(!$items){
             return $this->responseNotFound('TrialSchedule does not exist');
         }
