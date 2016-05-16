da91807d4836248072599e49e493fa0bf2243670
Utkarsh <utkarshmehrotra@fitternity.com>
2016-05-16 17:53:48 +0530

    changes in schedule

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -118,7 +118,7 @@ class SchedulebooktrialsController extends \BaseController {
         $timestamp 				= 	strtotime($date);
         $weekday 				= 	strtolower(date( "l", $timestamp));
 
-        $items = Service::where('finder_id', '=', $finderid)->get(array('_id','name','finder_id', 'trialschedules', 'workoutsessionschedules'))->toArray();
+        $items = Service::where('status', "1")->where('finder_id', '=', $finderid)->get(array('_id','name','finder_id', 'trialschedules', 'workoutsessionschedules'))->toArray();
         if(!$items){
             return $this->responseNotFound('TrialSchedule does not exist');
         }
