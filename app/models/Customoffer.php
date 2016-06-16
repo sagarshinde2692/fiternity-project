c2be11cffb7eb96a7a9c5d112c39218ef98d7e4f
Renuka Aggarwal <renukaaggarwal@fitternity.com>
2016-06-16 17:22:35 +0530

    Models updated

--- app/models/Customoffer.php
+++ app/models/Customoffer.php
@@ -4,4 +4,21 @@ class Customoffer extends \Basemodel {
 
     protected $collection = "customoffers";
 
+    // Add your validation rules here
+    public static $rules = [
+        'title'=> 'required',
+        'description'=> 'required',
+        'quantity_type'=> 'required',
+        'quantity'=> 'required|integer',
+        'price'=> 'required|integer',
+        'validity'=> 'required|integer',
+        'status'=> 'required'
+    ];
+
+    public function campaigns(){
+        return $this->belongsToMany('Campaign', null, 'customoffers', 'campaigns');
+    }
+    // Don't forget to fill this array
+//	protected $fillable = [];
+
 }
