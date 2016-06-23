adc6b622e0241352866fccb501912364374ec3e8
Sanjay Sahu <sanjay@Sanjays-MacBook-Pro.local>
2016-06-23 12:50:47 +0530

    check customer_source

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -1404,7 +1404,7 @@ class SchedulebooktrialsController extends \BaseController {
 
             
             if(isset($data['customofferorder_id']) && $data['customofferorder_id'] != ""){
-                if($data['source'] == 'cleartrip'){
+                if($data['customer_source'] == 'cleartrip'){
                     $booktrialdata['cleartrip_customofferorder_id'] = $data['customofferorder_id'];
                 }else{
                     $booktrialdata['customofferorder_id'] = intval($data['customofferorder_id']);
@@ -1930,10 +1930,10 @@ class SchedulebooktrialsController extends \BaseController {
 
 
             if(isset($data['customofferorder_id']) && $data['customofferorder_id'] != ""){
-                if($data['source'] == 'cleartrip'){
+                if($data['customer_source'] == 'cleartrip'){
                     $booktrialdata['cleartrip_customofferorder_id'] = $data['customofferorder_id'];
                 }else{
-                    $booktrialdata['customofferorder_id'] = intval($data['customofferorder_id']);
+                    $booktrialdata['customofferordanjay@123er_id'] = intval($data['customofferorder_id']);
                 }
             }
 
