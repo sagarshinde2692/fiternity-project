d01875d21e2f20838a44b6d307c47fa5855dcefe
Sanjay Sahu <sanjay@Sanjays-MacBook-Pro.local>
2016-08-17 14:49:33 +0530

    changes for mou templates

--- app/models/Vendormou.php
+++ app/models/Vendormou.php
@@ -12,7 +12,13 @@ class Vendormou extends  \Basemodel {
 	protected $connection = 'mongodb2';
 	
 	protected $collection = "vendormous";
+
+	protected $dates = array('collection_date','execution_start_date','contract_start_date','contract_end_date','created_at_after_15_days','generated_link_date');
+
 	
 
+	public function finder(){
+		return $this->belongsTo('Finder','vendor_id');
+	}
 
 }
