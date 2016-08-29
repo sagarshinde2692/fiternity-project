ffdf24db01652f81a1d678f12d2b6b7689b9b93e
fitmahesh <maheshjadhav@fitternity.com>
2016-08-29 18:41:36 +0530

    reward changes

--- app/controllers/MyrewardController.php
+++ app/controllers/MyrewardController.php
@@ -14,22 +14,22 @@ class MyrewardController extends BaseController {
     }
 
 
-	public function createMyReward(){
+    public function createMyReward(){
 
-		try {
-			
-			$data = Input::json()->all();
+        try {
+            
+            $data = Input::json()->all();
             $data = $this->utilities->removeEmptyKeys($data);
-			Log::info('Add reward to customer myrewards',$data);
+            Log::info('Add reward to customer myrewards',$data);
 
-			
-			return Response::json(array("status" => 200,"message" => "success"),200);
+            
+            return Response::json(array("status" => 200,"message" => "success"),200);
 
-		} catch (Exception $e) {
+        } catch (Exception $e) {
 
-			 return Response::json(array('status' => 404,'message' => $e->getMessage()),404);
-		}							
-	}
+             return Response::json(array('status' => 404,'message' => $e->getMessage()),404);
+        }                           
+    }
 
     public function listMyRewards($offset = 0, $limit = 10){
     
@@ -37,10 +37,23 @@ class MyrewardController extends BaseController {
         $decoded = $this->utilities->customerTokenDecode($jwt_token);
         $customer_id = (int)$decoded->customer->_id;
 
-        $myrewards      =   Myreward::where('customer_id',$customer_id)
-                            ->skip($offset)
-                            ->take($limit)
-                            ->orderBy('_id', 'desc')->get()->toArray();
+        $query      =   Myreward::where('customer_id',$customer_id);
+
+        if(isset($_GET['device_type']) && (strtolower($_GET['device_type']) == "android") && isset($_GET['app_version']) && ((float)$_GET['app_version'] < 2.6)){
+            $query->where("payload.booktrial_type","vip_booktrials");
+        }
+
+        if(isset($_GET['device_type']) && (strtolower($_GET['device_type']) == "ios")){
+            $query->where("payload.booktrial_type","vip_booktrials");
+        }
+
+        $myrewards = array();
+        $myrewards = $query->skip($offset)->take($limit)->orderBy('_id', 'desc')->get();
+
+        if(count($myrewards) > 0){
+            $myrewards->toArray();
+        }
+
         return Response::json(array('status' => 200,'data' => $myrewards), 200);
     }
 
@@ -53,7 +66,7 @@ class MyrewardController extends BaseController {
         $reward = Myreward::find($myreward_id);
 
         if(empty($reward)){
-            $resp 	= 	array('status' => 400,'message' => "Invalid MyReward ID");
+            $resp   =   array('status' => 400,'message' => "Invalid MyReward ID");
             return  Response::json($resp, 400);
         }
 
@@ -68,5 +81,5 @@ class MyrewardController extends BaseController {
                 break;
         }
     }
-	
-}																																																																																																																																																																																																																																																																										
+    
+}
