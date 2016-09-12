4d849f12fc02d261abff74970758219c9aa2ac10
fitmahesh <maheshjadhav@fitternity.com>
2016-09-12 19:43:05 +0530

    added location

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -221,7 +221,7 @@ class FindersController extends \BaseController {
 
                     $brand = Brand::find((int)$finderarr['brand_id']);
 
-                    $brandFinder = Finder::select('_id','title','slug','brand_id')->where("brand_id",(int)$finderarr['brand_id'])->where("_id","!=",(int)$finderarr['_id'])->where('city_id',(int)$finderarr['city_id'])->get();
+                    $brandFinder = Finder::select('_id','title','slug','brand_id','location_id','city_id')->with(array('location'=>function($query){$query->select('_id','name','slug');}))->where("brand_id",(int)$finderarr['brand_id'])->where("_id","!=",(int)$finderarr['_id'])->where('city_id',(int)$finderarr['city_id'])->get();
 
                     $finderarr['brand']['brand_detail'] = $brand;
                     $finderarr['brand']['finder_detail'] = $brandFinder;
