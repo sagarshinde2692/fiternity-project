87c32f147f654dd1277abfff91e217878396661f
Sanjay Sahu <sanjaysahu@Sanjays-MacBook-Pro-2.local>
2016-10-27 02:43:40 +0530

    comment personal trainers customer home

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -2258,7 +2258,7 @@ class CustomerController extends \BaseController {
 
 		if(isset($_GET['device_type']) && (strtolower($_GET['device_type']) == "android") && isset($_GET['app_version']) && ((float)$_GET['app_version'] >= 2.5)){
 
-			$category_slug = array("gyms","yoga","zumba","fitness-studios","crossfit","marathon-training","dance","cross-functional-training","mma-and-kick-boxing","swimming","pilates","personal-trainers","luxury-hotels","healthy-snacks-and-beverages","spinning-and-indoor-cycling","healthy-tiffins","dietitians-and-nutritionists","sport-nutrition-supliment-stores","aerobics");
+			$category_slug = array("gyms","yoga","zumba","fitness-studios","crossfit","marathon-training","dance","cross-functional-training","mma-and-kick-boxing","swimming","pilates",/*"personal-trainers",*/"luxury-hotels","healthy-snacks-and-beverages","spinning-and-indoor-cycling","healthy-tiffins","dietitians-and-nutritionists","sport-nutrition-supliment-stores","aerobics");
 
 			$cache_tag = 'customer_home_by_city_2_5';
 
