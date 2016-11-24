b49dc3aeb93a517fa892015e294a98a7e426238a
Sanjay Sahu <sanjaysahu@Sanjays-MacBook-Pro-2.local>
2016-11-24 12:47:56 +0530

    updates delete

--- app/debug_routes.php
+++ app/debug_routes.php
@@ -45,6 +45,15 @@ Route::get('latlonswapserviceapi', 'DebugController@latLonSwapServiceApi');
 Route::get('addexpirydate', 'DebugController@addExpiryDate');
 
 
+Route::get('deletetrans/{email}', function ($email){
+
+    DB::connection('mongodb')->table('booktrials')->where('customer_email', trim($email))->delete();
+    DB::connection('mongodb')->table('orders')->where('customer_email', trim($email))->delete();
+    DB::connection('mongodb')->table('captures')->where('email', trim($email))->delete();
+
+});
+
+
 Route::get('checkfileons3', function (){
 
     $booktrial          = Booktrial::find(43208);
