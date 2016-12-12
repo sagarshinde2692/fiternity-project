a48453f27a47d985423480bfba5a2511c75ee829
Sanjay Sahu <sanjaysahu@Sanjays-MacBook-Pro-2.local>
2016-12-12 20:42:59 +0530

    added new category offerings _getOfferingRegex

--- app/controllers/RankingSearchController.php
+++ app/controllers/RankingSearchController.php
@@ -2048,7 +2048,7 @@ class RankingSearchController extends \BaseController
                 $regex = 'Post Workout Supplements|pre workout Supplements|Accessories|Nutrition Bar|Protein Supplements|Energy & Endurance Supplements|Lean Muscle Gainer|Lean Mass Gainer|Weight Gainer|Fat Burners|Health & Wellness Supplements|Muscle Gainer|Soy Protein|Whey Protein|Fish Oil|Shakers|Mass Gainers|Food Supplements|';
                 break;
 
-            case 'pre natal classes':
+            case 'pre-natal classes':
                 $regex = 'Lamaze Class|Prenatal Yoga|';
                 break;
 
