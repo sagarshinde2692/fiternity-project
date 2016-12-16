8bd8a218bd3a4c059787c3f42f4a073502694467
fitmahesh <maheshjadhav@fitternity.com>
2016-12-16 13:06:46 +0530

    chagnes

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -2394,7 +2394,7 @@ class CustomerController extends \BaseController {
 		);
 
 		if(isset($_REQUEST['device_type']) && $_REQUEST['device_type'] == "ios" ){
-			unset($result['campaign']['link']);
+			$result['campaign']['link'] = "";
 		}
 
 		return Response::json($result);
