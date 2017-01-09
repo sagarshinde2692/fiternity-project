2abfc6e35e4a262af6387f2109fc46c15aa41079
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-01-09 15:58:06 +0530

    commented send sms on call to customer

--- app/controllers/OzonetelsController.php
+++ app/controllers/OzonetelsController.php
@@ -617,11 +617,12 @@ class OzonetelsController extends \BaseController {
 
 			$ozonetel_capture->update();
 
-			if(!$call_jump){
+			//send sms on call to customer
+			/*if(!$call_jump){
 				if($ozonetel_capture->call_status == 'answered' || $ozonetel_capture->call_status == 'not_answered' || $data['event'] == 'Disconnect' && isset($ozonetel_capture->finder_commercial_type) && $ozonetel_capture->finder_commercial_type != 0){
 					$this->customersms->ozonetelCapture($ozonetel_capture->toArray());
 				}
-			}
+			}*/
 
 			return $ozonetel_capture;
 
