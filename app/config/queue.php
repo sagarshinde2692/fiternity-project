4394ab2c0ff90aa548aa85a6515d11765d9052e1
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-01-16 15:44:41 +0530

    stagev3 merged and ready for a2.fitternity.com

--- app/config/queue.php
+++ app/config/queue.php
@@ -85,7 +85,7 @@ return array(
         //
         'redis' => array(
             'driver' => 'redis',
-            'queue'  => 'booktrial',
+            'queue'  => 'booktrialv2',
         ),
 
 	),
