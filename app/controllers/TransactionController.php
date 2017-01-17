4d671ce48f22c9d25b6aae5c938f6d5a36dc6fb0
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-01-17 12:31:50 +0530

    transaction chnages

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -1,10 +1,10 @@
 <?PHP
 
 /**
- * ControllerName : OrderController.
- * Maintains a list of functions used for OrderController.
+ * ControllerName : TransactionController.
+ * Maintains a list of functions used for TransactionController
  *
- * @author Sanjay Sahu <sanjay.id7@gmail.com>
+ * @author Mahesh Jadhav <maheshjadhav@fitternity.com>
  */
 
 use App\Mailers\CustomerMailer as CustomerMailer;
@@ -240,23 +240,18 @@ class TransactionController extends \BaseController {
 
     public function getCashbackRewardWallet($data){
 
-        $amount = $data['amount'];
-
-        $cashback_detail = $data['cashback_detail'] = $this->customerreward->purchaseGame($data['amount_finder'],$data['finder_id'],'paymentgateway',$data['offer_id'],$data['customer_id']);
-
-        if(isset($data['cashback']) && $data['cashback'] == true){
-            $amount = $data['amount'] - $cashback_detail['amount_discounted'];
-        }
+        $data['cashback_detail'] = $this->customerreward->purchaseGame($data['amount_finder'],$data['finder_id'],'paymentgateway',$data['offer_id'],$data['customer_id']);
 
         if(isset($data['wallet']) && $data['wallet'] == true){
+            $data['wallet_amount'] = $data['cashback_detail']['amount_deducted_from_wallet'];
+            $data['amount'] = $data['amount'] - $data['wallet_amount'];
+        }
 
-            $wallet_amount = $data['wallet_amount'] = $cashback_detail['only_wallet']['fitcash'] + $cashback_detail['only_wallet']['fitcash_plus'];
+        if(isset($data['cashback']) && $data['cashback'] == true){
+            $data['amount'] = $data['amount'] - $data['cashback_detail']['amount_discounted'];
+        }
 
-            if(isset($data['cashback']) && $data['cashback'] == true){
-                $wallet_amount = $data['wallet_amount'] = $cashback_detail['discount_and_wallet']['fitcash'] + $cashback_detail['discount_and_wallet']['fitcash_plus'];
-            }
-
-            $amount = $data['amount'] - $wallet_amount;
+        if(isset($data['wallet_amount']) && $data['wallet_amount'] > 0){
 
             $req = array(
                 'customer_id'=>$data['customer_id'],
@@ -265,7 +260,7 @@ class TransactionController extends \BaseController {
                 'type'=>'DEBIT',
                 'description'=>'Paid for Order ID: '.$data['order_id'],
             );
-            $walletTransactionResponse = $this->utilities->walletTransaction($req,$data)->getData();
+            $walletTransactionResponse = $this->utilities->walletTransaction($req)->getData();
             $walletTransactionResponse = (array) $walletTransactionResponse;
 
             if($walletTransactionResponse['status'] != 200){
@@ -276,10 +271,9 @@ class TransactionController extends \BaseController {
             $url = Config::get('app.url').'/orderfailureaction/'.$data['order_id'];
             $delay = \Carbon\Carbon::createFromFormat('d-m-Y g:i A', date('d-m-Y g:i A'))->addHours(4);
             $data['wallet_refund_sidekiq'] = $this->hitURLAfterDelay($url, $delay);
+
         }
 
-        $data['amount'] = $amount;
-        
         if(isset($data['reward_ids'])&& count($data['reward_ids']) > 0) {
             $data['reward_ids']   =  array_map('intval', $data['reward_ids']);
         }
