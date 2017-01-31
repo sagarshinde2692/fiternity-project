2f9ed7aea163cd97d9be01f6d1f6871b19636f48
utkarsh <ut.mehrotra@gmail.com>
2017-01-31 19:12:40 +0530

    sort_by_order

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -268,7 +268,7 @@ class FindersController extends \BaseController {
 
 				if(count($finder['photos']) > 0 ){
 					$photoArr        =   [];
-					usort($finder['photos'], "order");
+					usort($finder['photos'], "sort_by_order");
 					foreach ($finder['photos'] as $photo) {
 						$servicetags                =   (isset($photo['servicetags']) && count($photo['servicetags']) > 0) ? Service::whereIn('_id',$photo['servicetags'])->lists('name') : [];
 						$photoObj                   =   array_except($photo,['servicetags']);
