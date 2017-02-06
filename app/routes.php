8b5e9e3163b01504e48b725b9dcbf1120fd472a5
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-02-06 19:55:19 +0530

    changes

--- app/routes.php
+++ app/routes.php
@@ -654,7 +654,7 @@ Route::get('stats/smsbalance/{day}', 'StatsController@smsBalance');
 ##############################################################################
 /******************** OZONETELS SECTION START HERE ***********************/
 
-Route::get('ozonetel/freevendor',  array('as' => 'ozonetel.freevendor','uses' => 'OzonetelsController@freeVendorV1'));
+Route::get('ozonetel/freevendor',  array('as' => 'ozonetel.freevendor','uses' => 'OzonetelsController@freeVendor'));
 Route::get('ozonetel/paidvendor',  array('as' => 'ozonetel.paidvendor','uses' => 'OzonetelsController@paidVendorV1'));
 Route::get('ozonetel/outboundcallsend/{phone_number}',  array('as' => 'ozonetel.outboundCallSend','uses' => 'OzonetelsController@outboundCallSend'));
 Route::get('ozonetel/outboundcallrecive/{id}',  array('as' => 'ozonetel.outboundCallRecive','uses' => 'OzonetelsController@outboundCallRecive'));
