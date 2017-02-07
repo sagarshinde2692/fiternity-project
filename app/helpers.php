bff4beb0194ba4e417b6148de937c9cc9c237e5e
utkarsh <ut.mehrotra@gmail.com>
2017-02-07 16:36:32 +0530

    substr

--- app/helpers.php
+++ app/helpers.php
@@ -2263,7 +2263,7 @@ if (!function_exists(('getReversehash'))){
         $txnid = $data['txnid'];
         $amount = $data['amount'].".00";
         $productinfo = $data['productinfo'] = $service_name." - ".$finder_name;
-        $productinfo = substr($productinfo,100);
+        $productinfo = substr($productinfo,0,100);
         $firstname = $data['customer_name'];
         $email = $data['customer_email'];
         $udf1 = "";
