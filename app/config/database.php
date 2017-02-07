9aec13571556b1251be59244222dd3eede057e67
utkarsh <ut.mehrotra@gmail.com>
2017-02-07 15:07:32 +0530

    db creds

--- app/config/database.php
+++ app/config/database.php
@@ -52,8 +52,8 @@ return array(
 			'host'     => 'localhost',
 			// 'host'     => '52.77.247.239',
 			'port'     => 27017,
-		    // 'username' => 'fitadmin',
-		    // 'password' => 'fit1234',
+		    'username' => 'fitadmin',
+		    'password' => 'fit1234',
 			'database' => 'fitadmin'
 		),
 
