9a4886349ee8c4a91716cf0fe1442607427e91f9
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-02-16 17:07:32 +0530

    redundant bug fix

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -470,9 +470,8 @@ Class Utilities {
                         ->whereIn('type',['memberships','healthytiffinmembership'])
                         ->where('service_id',(int)$order->service_id)
                         ->where('finder_id',(int)$order->finder_id)
-                        ->where('customer_email',(int)$order->customer_email)
-                        ->where('created_at', '>=', new DateTime( date("d-m-Y 00:00:00", strtotime("-44 days"))))
-                        ->where('created_at', '<=', new DateTime( date("d-m-Y 23:59:59", strtotime("-44 days"))))
+                        ->where('customer_email',$order->customer_email)
+                        ->where('created_at', '>=', new \DateTime( date("d-m-Y 00:00:00", strtotime("-44 days"))))
                         ->where('paymentLinkEmailCustomerTiggerCount','exists',true)
                         ->where('paymentLinkEmailCustomerTiggerCount','>',0)
                         ->where('redundant_order','exists',false)
@@ -483,7 +482,8 @@ Class Utilities {
 
             foreach ($allOrders as $orderData) {
 
-                $orderData->update(array('redundant_order'=>'1'));
+                $orderData->redundant_order = "1";
+                $orderData->update();
             }
         }
 
