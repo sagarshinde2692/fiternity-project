bdc7f386b963ad743b498473ee0cbe737c64e2e0
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-04-06 19:38:57 +0530

    resolve conflict

--- app/mailers/Mailer.php
+++ app/mailers/Mailer.php
@@ -180,8 +180,6 @@ abstract Class Mailer {
 
 	public function sendDbToWorker($to = '',$email_template, $message_data = [], $label = 'label', $delay = 0){
 
-		return true;
-
 		$sidekiq = new Sidekiq();
 
 		if(is_array($delay))
