8ffb29885a0b79058804f4092f0320121efbb239
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-04-14 13:01:41 +0530

    changes

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -2168,10 +2168,8 @@ class SchedulebooktrialsController extends \BaseController {
 
             $currentDateTime 			       =	\Carbon\Carbon::now();
             $scheduleDateTime 			       =	\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s',strtotime($booktrial->schedule_date_time)));
-
-            $currentScheduleDateDiffMin = $currentDateTime->diffInHours($scheduleDateTime, false);
-
-            Log:info("----------------------------currentScheduleDateDiffMin : ".$currentScheduleDateDiffMin."-----------------------");
+            
+            $currentScheduleDateDiffMin = $currentDateTime->diffInMinutes($scheduleDateTime, false);
 
             $customer_email_messageids 	=  $finder_email_messageids  =	$customer_sms_messageids  =  $finder_sms_messageids  =  $customer_notification_messageids  =  array();
 
