93878bf8f9b09df33eebd25189d76a65784285a4
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-05-09 12:53:07 +0530

    chagnes

--- app/routes.php
+++ app/routes.php
@@ -916,3 +916,4 @@ Route::get('addwallet',array('as' => 'transaction.addwallet','uses' => 'Transact
 Route::get('customerorderdata','DebugController@customer_data');
 Route::get('zumbadata','DebugController@zumba_data');
 Route::get('syncsharecustomerno','DebugController@syncsharecustomerno');
+Route::get('reverse/migration/{colllection}/{id}','MigrationReverseController@byId');
