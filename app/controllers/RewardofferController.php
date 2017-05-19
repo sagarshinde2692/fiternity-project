ac699938ddf25256f871c3ed66599649330b91e2
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-05-19 22:19:51 +0530

    changes

--- app/controllers/RewardofferController.php
+++ app/controllers/RewardofferController.php
@@ -130,11 +130,11 @@ class RewardofferController extends BaseController {
                 'percentage'    =>  $calculation['algo']['cashback'].'%',
                 'commision'     =>  $calculation['algo']['cashback'],
                 'calculation'   =>  $calculation,
-                'info'          =>  "You can only pay upto 10% of the booking amount through FitCash. \nIt is calculated basis the amount, type and duration of the purchase.  \nYour total FitCash balance is Rs. ".$calculation['current_wallet_balance_only_fitcash']." FitCash applicable for this transaction is Rs. ".$calculation['amount_deducted_from_wallet']
+                'info'          =>  ""//"You can only pay upto 10% of the booking amount through FitCash. \nIt is calculated basis the amount, type and duration of the purchase.  \nYour total FitCash balance is Rs. ".$calculation['current_wallet_balance_only_fitcash']." FitCash applicable for this transaction is Rs. ".$calculation['amount_deducted_from_wallet']
             ];
-            if($calculation["current_wallet_balance_only_fitcash_plus"] > 0){
+            /*if($calculation["current_wallet_balance_only_fitcash_plus"] > 0){
                 $cashback["info"] = "You can only pay upto 10% of the booking amount through FitCash. \n\nIt is calculated basis the amount, type and duration of the purchase.  \n\nYour total FitCash balance is Rs. ".$calculation['current_wallet_balance_only_fitcash_plus']."\n\nYour total FitCash Plus balance is Rs. ".$calculation['current_wallet_balance_only_fitcash']." FitCash applicable for this transaction is Rs. ".$calculation['amount_deducted_from_wallet'];
-            }
+            }*/
 
             $renewal_cashback   =   ['title'=>'Discount on Renewal'];
             $rewards            =   [];
@@ -363,9 +363,9 @@ class RewardofferController extends BaseController {
             'info'          =>  "",//"You can only pay upto 10% of the booking amount through FitCash. \n\nIt is calculated basis the amount, type and duration of the purchase.  \n\nYour total FitCash balance is Rs. ".$calculation['current_wallet_balance_only_fitcash']." FitCash applicable for this transaction is Rs. ".$calculation['amount_deducted_from_wallet'],
             'description'=>$calculation['description']
         );
-        if($calculation["current_wallet_balance_only_fitcash_plus"] > 0){
-            $cashback["info"] = "";//"You can only pay upto 10% of the booking amount through FitCash. \n\nIt is calculated basis the amount, type and duration of the purchase.  \n\nYour total FitCash balance is Rs. ".$calculation['current_wallet_balance_only_fitcash_plus']."\n\nYour total FitCash Plus balance is Rs. ".$calculation['current_wallet_balance_only_fitcash']." FitCash applicable for this transaction is Rs. ".$calculation['amount_deducted_from_wallet'];
-        }
+        /*if($calculation["current_wallet_balance_only_fitcash_plus"] > 0){
+            $cashback["info"] = "You can only pay upto 10% of the booking amount through FitCash. \n\nIt is calculated basis the amount, type and duration of the purchase.  \n\nYour total FitCash balance is Rs. ".$calculation['current_wallet_balance_only_fitcash_plus']."\n\nYour total FitCash Plus balance is Rs. ".$calculation['current_wallet_balance_only_fitcash']." FitCash applicable for this transaction is Rs. ".$calculation['amount_deducted_from_wallet'];
+        }*/
 
         unset($cashback['calculation']['description']);
 
