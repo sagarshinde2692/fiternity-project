416c1d3ff2a5b2169e1a7e124be86dc8b5516f3d
utkarsh <ut.mehrotra@gmail.com>
2017-05-22 21:33:56 +0530

    diet check on email

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -315,7 +315,7 @@ class TransactionController extends \BaseController {
                 return Response::json($resp,$resp["status"]);
             }
 
-            if($order->status == "1" && isset($data['preferred_starting_date']) && $data['preferred_starting_date']  != '' && $data['preferred_starting_date']  != '-'){
+            if($order->status == "1" && isset($data['preferred_starting_date']) && $data['preferred_starting_date']  != '' && $data['preferred_starting_date']  != '-' && $data['type']  != 'diet_plan'){
 
                 $preferred_starting_date = date('Y-m-d 00:00:00', strtotime($data['preferred_starting_date']));
 
