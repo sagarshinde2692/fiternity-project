b030875eda9be1beb2087d44bda746ba6e420c3b
fitdhruv <dhruvsarawagi@fitternity.com>
2017-06-22 20:53:12 +0530

    order subscription code

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -171,7 +171,7 @@ class TransactionController extends \BaseController {
             $order_id = $data['_id'] = $data['order_id'] = Order::max('_id') + 1;
         }
 
-        $data['code'] = $data['order_id'].str_random(8);
+        $data['code'] = $data['order_id'];
 
         if(isset($data['referal_trial_id'])){
 
