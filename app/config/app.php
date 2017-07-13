9372f8c1629495be0b509d1c9b48318b00291b72
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-07-13 15:41:16 +0530

    config changes

--- app/config/app.php
+++ app/config/app.php
@@ -15,7 +15,7 @@ return array(
 	*/
 
 
-	'debug' => TRUE,
+	'debug' => FAlSE,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -28,17 +28,17 @@ return array(
 	|
 	*/
 
-	// 'url' => 'https://a1.fitternity.com', //live
-	'url' => 'http://apistage.fitn.in', //stage
+	'url' => 'https://a1.fitternity.com', //live
+	// 'url' => 'http://apistage.fitn.in', //stage
 	// 'url' => 'http://fitapi.com', //local
 	// 'url' => 'http://beta.fitn.in', //beta
 
-	// 'admin_url' => 'https://fitn.in', //live
-	'admin_url' => 'http://adminstage.fitn.in', //stage
+	'admin_url' => 'https://fitn.in', //live
+	// 'admin_url' => 'http://adminstage.fitn.in', //stage
 	// 'admin_url' => 'http://fitadmin.com', //local
 
-    // 'website' => 'https://www.fitternity.com', //live
-    'website' => 'http://apistage.fitn.in:2233', //stage
+    'website' => 'https://www.fitternity.com', //live
+    // 'website' => 'http://apistage.fitn.in:2233', //stage
     // 'website' => 'http://localhost:3000', //stage
     // 'website' => 'http://apistage.fitn.in:8020', //beta
 
@@ -46,8 +46,8 @@ return array(
 
     'business' => 'http://business.fitternity.com',
 
-    // 'sidekiq_url' => 'http://nw.fitn.in/', //live
-   	'sidekiq_url' => 'http://kick.fitn.in/', //stage
+    'sidekiq_url' => 'http://nw.fitn.in/', //live
+   	// 'sidekiq_url' => 'http://kick.fitn.in/', //stage
 
     // 'queue' => 'beta', //beta
     'queue' => 'booktrial', //live
@@ -60,8 +60,8 @@ return array(
 	// 'vendor_communication' => true,//live
 	'vendor_communication' => false,//stage
 
-	'env' => 'stage',//stage
-	// 'env' => 'production',//live
+	// 'env' => 'stage',//stage
+	'env' => 'production',//live
 
 	/*
 	|--------------------------------------------------------------------------
@@ -266,21 +266,21 @@ return array(
 	//will be implemented everywhere in future when other api will be changed
 	/*************************************************************************/
 	//Production
-	// 'es' =>array(
-	// 	'url'		=> 			'ESAdmin:fitternity2020@54.169.120.141:8050',
-	// 	'host'		=> 			'ESAdmin:fitternity2020@54.169.120.141',
-	// 	'port'		=>			8050,
-	// 	'default_index' => 	'fitternity',
-	// 	'default_type' 	=> 	'finder',
-	// ),
-	//stage
 	'es' =>array(
-	 	'url'		=> 			'139.59.16.74:1243',
-	 	'host'		=> 			'139.59.16.74',
-	 	'port'		=>			1243,
-	 	'default_index' => 	'fitternity',
-	 	'default_type' 	=> 	'finder',
+		'url'		=> 			'ESAdmin:fitternity2020@54.169.120.141:8050',
+		'host'		=> 			'ESAdmin:fitternity2020@54.169.120.141',
+		'port'		=>			8050,
+		'default_index' => 	'fitternity',
+		'default_type' 	=> 	'finder',
 	),
+	//stage
+	// 'es' =>array(
+	//  	'url'		=> 			'139.59.16.74:1243',
+	//  	'host'		=> 			'139.59.16.74',
+	//  	'port'		=>			1243,
+	//  	'default_index' => 	'fitternity',
+	//  	'default_type' 	=> 	'finder',
+	// ),
 	//local
 	// 'es' =>array(
 	// 	'url'		=> 			'localhost:9200',
