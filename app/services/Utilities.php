7fdc43fa647d8aa56915f40ba1a9052fdf5b296b
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-07-26 16:54:24 +0530

    resolve conflict

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -1274,7 +1274,7 @@ Class Utilities {
                 
                 if($allDeviceCount == 0 && isset($data['customer_id']) && $data['customer_id'] != ''){
 
-                    $booktrial = \Booktrial::where('created_at','>',new DateTime(date("d-m-Y 00:00:00",strtotime("20-4-2017 00:00:00"))))->where("customer_id",(int)$data['customer_id'])->where('type','booktrials')->count();
+                    $booktrial = \Booktrial::where('created_at','>',new \DateTime(date("d-m-Y 00:00:00",strtotime("20-4-2017 00:00:00"))))->where("customer_id",(int)$data['customer_id'])->where('type','booktrials')->count();
 
                     $description = "app download";
 
