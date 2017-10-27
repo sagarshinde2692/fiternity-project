23dab1d0a612595e319ff76fb0311ba62e293fc6
utkarsh <ut.mehrotra@gmail.com>
2017-10-27 14:17:06 +0530

    vendor inclusion done

--- app/services/CustomerReward.php
+++ app/services/CustomerReward.php
@@ -1130,7 +1130,8 @@ Class CustomerReward {
                 return $resp;
             }
             $included_vendors = isset($coupon["finders"]) ? $coupon["finders"] : [];
-            Log::info($included_vendors);
+            // Log::info($included_vendors);
+            // Log::info("ratecard['finder_id']".$ratecard['finder_id']);
             if(isset($ratecard['finder_id']) && count($included_vendors) > 0 && !in_array($ratecard['finder_id'], $included_vendors)){
                 $resp = array("data"=>array("discount" => 0, "final_amount" => $price, "wallet_balance" => $wallet_balance, "only_discount" => $price), "coupon_applied" => false, "vendor_coupon"=>$vendor_coupon, "error_message"=>"Coupon not valid for this transaction");
                 return $resp;
