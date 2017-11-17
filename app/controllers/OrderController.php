418ebc4c83b97a773dc4ded4e49158f8639ddd11
fitdhruv <dhruvsarawagi@fitternity.com>
2017-11-17 16:10:39 +0530

    cod cashback

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -2287,9 +2287,13 @@ class OrderController extends \BaseController {
                     $resp   =   array("status" => 401,"message" => "We have already received your request");
                     return Response::json($resp,$resp["status"]);
                 }
+               
+                if($data['payment_mode'] != 'cod'){
 
-                $order->unset('cashback');
-                $order->unset('reward_info');
+                    $order->unset('cashback');
+                    $order->unset('reward_info');
+
+                }
             }
 
 
