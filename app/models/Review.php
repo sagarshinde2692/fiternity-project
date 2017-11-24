20d58851e16205a8eb8e60c2c1e617620020586a
Mahesh Jadhav <maheshjadhav@fitternity.com>
2017-11-24 17:51:01 +0530

    removed email and contact_no

--- app/models/Review.php
+++ app/models/Review.php
@@ -23,7 +23,7 @@ class Review extends  \Basemodel {
 	protected $appends = array('customer');
 
 	public function getCustomerAttribute(){
-		$customer = Customer::where('_id',$this->customer_id)->first(array('name','picture','email','contact_no' ));
+		$customer = Customer::where('_id',$this->customer_id)->first(array('name','picture'));
 		return $customer;
 	}
 
