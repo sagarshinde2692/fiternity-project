32a15ffdbfe44b79e7700986b21fb8db3f9b2bc8
utkarsh <ut.mehrotra@gmail.com>
2017-12-18 13:20:51 +0530

    removed personal trainer

--- app/controllers/GlobalPushController.php
+++ app/controllers/GlobalPushController.php
@@ -398,8 +398,11 @@ class GlobalPushController extends \BaseController
     ini_set('max_execution_time', 30000);
     // ini_set('memory_limit', '512M');
     $city_id = (int) $city_id;
-    $indexdocs = Finder::active()->where('city_id', $city_id)
+    $indexdocs = Finder::active()
+        ->where('status', '=', '1')
+        ->where('city_id', $city_id)
         ->where('flags.state', '!=', 'closed')
+        ->whereNotIn('categorytags',[37])
         ->with(array('country'=>function($query){$query->select('name');}))
         ->with(array('city'=>function($query){$query->select('name');}))
         ->with(array('category'=>function($query){$query->select('name','meta');}))
