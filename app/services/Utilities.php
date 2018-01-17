a7335cda8da4099c598068071cc56401e302eb42
Mahesh Jadhav <maheshjadhav@fitternity.com>
2018-01-17 18:29:23 +0530

    changes

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -2628,7 +2628,7 @@ Class Utilities {
         $response['features'] = [];
 
         $response['features'][] = [
-            'image'=>'https://b.fitn.in/gamification/reward/cashback.jpg',
+            'image'=>'https://b.fitn.in/global/Tab-app-success-page/tab-membership-success-1.png',
             'title1'=>strtoupper('fitternity profile'),
             'title2'=>strtoupper('on app & website'),   
             'description'=>"&#9679; <b>Track</b> your FitCash wallet balance<br/>&#9679; <b>Renew</b> membership with best discount & offers<br/>&#9679; <b>Upgrade</b> membership by extending the duration at initial price",
@@ -2636,7 +2636,7 @@ Class Utilities {
         ];
 
         $response['features'][] = [
-            'image'=>'https://b.fitn.in/gamification/reward/cashback.jpg',
+            'image'=>'https://b.fitn.in/global/Tab-app-success-page/tab-membership-success-1.png',
             'title1'=>strtoupper('<b>Onlie diet</b>'),
             'title2'=>strtoupper('<b>consultation</b>'),
             'description'=>'Make the most of your membership, with <b>Fitternity’s Online Diet Consultation</b> to improve your workout performance',
@@ -2644,7 +2644,7 @@ Class Utilities {
         ];
 
         $response['features'][] = [
-            'image'=>'https://b.fitn.in/gamification/reward/cashback.jpg',
+            'image'=>'https://b.fitn.in/global/Tab-app-success-page/tab-membership-success-1.png',
             'title1'=>strtoupper('beat monotony'),
             'title2'=>strtoupper('<b>pay-per-session</b>'),
             'description'=>'<b>Don’t let your workout be monotonous.</b> Try different workouts around you by only paying per session!',
