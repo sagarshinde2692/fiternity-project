e8219c9f570163bf43d4c0b786fdd419f77b55a1
fitdhruv <dhruvsarawagi@fitternity.com>
2018-03-12 14:10:13 +0530

    unset direct_payment_enable

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -2802,8 +2802,10 @@ class FindersController extends \BaseController {
 					}
 
 					if(isset($item['trial']) && $item['trial']=='manual'){
-						if(isset($_GET['app_version']) && isset($_GET['device_type']) && (($_GET['device_type'] == 'android' && $_GET['app_version'] > '4.42') || ($_GET['device_type'] == 'ios' && $_GET['app_version'] > '4.3'))){
+						if(isset($_GET['app_version']) && isset($_GET['device_type']) && (($_GET['device_type'] == 'android' && $_GET['app_version'] > 4.42) || ($_GET['device_type'] == 'ios' && $_GET['app_version'] > '4.3'))){
 							$rateval['manual_trial_enable'] = "1";
+							unset($rateval['direct_payment_enable']);
+							
 						}else{
 							$rateval['direct_payment_enable'] = "0";
 						}
