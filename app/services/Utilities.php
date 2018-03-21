98c31df6ad35b30499dbef92b5ef5836713c370e
Mahesh Jadhav <maheshjadhav@fitternity.com>
2018-03-21 16:55:19 +0530

    cashback 25%

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -1594,6 +1594,19 @@ Class Utilities {
 
             }
 
+            if(isset($request['finder_id']) && $request['finder_id'] != ""){
+
+                $finder_id = (int)$request['finder_id'];
+
+                $power_world_gym_vendor_ids = Config::get('app.power_world_gym_vendor_ids_vendor_ids');
+
+                if(in_array($finder_id,$power_world_gym_vendor_ids)){
+
+                    $wallet_limit = 100000;
+                }
+                
+            }
+
             /*if(!isset($customer->current_wallet_balance) && $current_wallet_balance >= $wallet_limit){
                 return ['status' => 400,'message' => 'Wallet is overflowing Rs '.$wallet_limit];
             }*/
