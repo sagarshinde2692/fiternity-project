40f8f224318b88df6431c046366187676a8506e1
Mahesh Jadhav <maheshjadhav@fitternity.com>
2018-03-26 14:18:12 +0530

    improvements

--- app/services/CustomerReward.php
+++ app/services/CustomerReward.php
@@ -344,32 +344,22 @@ Class CustomerReward {
                 $walletData = array(
                     "order_id"=>$order['_id'],
                     "customer_id"=> intval($order['customer_id']),
-                    "amount"=> intval($order['amount_customer'] * 20 / 100),
+                    "amount"=> intval($order['amount_customer']),
                     "amount_fitcash" => 0,
-                    "amount_fitcash_plus" => intval($order['amount_customer'] * 20 / 100),
+                    "amount_fitcash_plus" => intval($order['amount_customer']),
                     "type"=>'CASHBACK',
                     'entry'=>'credit',
-                    "description"=> "20% Cashback for paid trial purchase at ".ucwords($order['finder_name'])." (Order ID. ".$order['_id']."), Expires On : ".date('d-m-Y',time()+(86400*60)),
-                    "validity"=>time()+(86400*60)
+                    "description"=> "100% Cashback on trial booking at ".ucwords($order['finder_name'])." Applicable for buying a membership at ".ucwords($order['finder_name']).", Expires On : ".date('d-m-Y',time()+(86400*7)),
+                    "validity"=>time()+(86400*7),
+                    "valid_finder_id"=>intval($order['finder_id']),
+                    "finder_id"=>intval($order['finder_id']),
+                    "valid_service_id"=>intval($order['service_id']),
+                    "service_id"=>intval($order['service_id']),
                 );
 
-                if($order['type'] == 'booktrials'){
+                $walletTransaction =  $utilities->walletTransaction($walletData,$order->toArray());
 
-                    $walletData = array(
-                        "order_id"=>$order['_id'],
-                        "customer_id"=> intval($order['customer_id']),
-                        "amount"=> intval($order['amount_customer']),
-                        "amount_fitcash" => 0,
-                        "amount_fitcash_plus" => intval($order['amount_customer']),
-                        "type"=>'CASHBACK',
-                        'entry'=>'credit',
-                        "description"=> "100% Cashback on trial booking at ".ucwords($order['finder_name'])." Applicable for buying a membership at ".ucwords($order['finder_name']).", Expires On : ".date('d-m-Y',time()+(86400*7)),
-                        "validity"=>time()+(86400*7),
-                        "valid_finder_id"=>intval($order['finder_id']),
-                        "finder_id"=>intval($order['finder_id']),
-                        "valid_service_id"=>intval($order['service_id']),
-                        "service_id"=>intval($order['service_id']),
-                    );
+                if(isset($walletTransaction['status']) && $walletTransaction['status'] == 200){
 
                     $customersms = new CustomerSms();
 
@@ -380,10 +370,7 @@ Class CustomerReward {
                     $sms_data['message'] = "Hi ".ucwords($order['customer_name']).", Rs. ".$order['amount_customer']." Fitcash has been added in your Fitternity wallet. Use this Fitcash to buy ".ucwords($order['finder_name'])."'s membership at lowest price and earn complimentary rewards. Valid for 7 days post your trial session. For quick assistance call ".Config::get('app.contact_us_customer_number');
 
                     $customersms->custom($sms_data);
-
                 }
-
-                $utilities->walletTransaction($walletData,$order->toArray());
 
             }elseif(isset($order['type']) && $order['type'] == 'events' && isset($order['customer_id']) && isset($order['amount']) && isset($order['ticket_id']) ){
                 
