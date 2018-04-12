8a59451adce2dfef467b39688e433bfa8dd36bf9
fitdhruv <dhruvsarawagi@fitternity.com>
2018-04-12 23:57:47 +0530

    time in upcoming bookings

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -3209,12 +3209,14 @@ class CustomerController extends \BaseController {
 									if(time() < (strtotime($data['schedule_date_time'])+3*60*60) && !(isset($data['kiosk_block_shown']) && $data['kiosk_block_shown'])){
 										$data['block_screen'] = [
 											'type'=>'activate_session',
-											'url'=>Config::get('app.url').'/notificationdatabytrialid/'.$data['_id'].'/activate_session'
+											'url'=>Config::get('app.url').'/notificationdatabytrialid/'.$data['_id'].'/activate_session',
+											'time'=>'n-10m'
 										];
 									}else if(time() < (strtotime($data['schedule_date_time'])+3*24*60*60)){
 										$data['block_screen'] = [
 											'type'=>'let_us_know',
-											'url'=>Config::get('app.url').'/notificationdatabytrialid/'.$data['_id'].'/let_us_know'
+											'url'=>Config::get('app.url').'/notificationdatabytrialid/'.$data['_id'].'/let_us_know',
+											'time'=>'n+2'
 										];
 									}
 								
