826560e1713c2e9648c898aeeef2036a98886801
utkarsh <ut.mehrotra@gmail.com>
2018-05-29 00:30:58 +0530

    cult

--- app/controllers/RewardofferController.php
+++ app/controllers/RewardofferController.php
@@ -340,10 +340,11 @@ class RewardofferController extends BaseController {
                     }
                     break;
                 case 13765 : 
-                    if(time() >= strtotime(date('2018-06-14 00:00:00')) && time() <= strtotime(date('2018-06-14 23:59:59'))){
-                        $min_date = strtotime(date('2018-06-14 00:00:00'));
-                        $max_date = strtotime(date('2018-06-14 23:59:59'));
-                    }
+                    $min_date = strtotime(date('2018-06-14 00:00:00'));
+                    $max_date = strtotime(date('2018-06-14 23:59:59'));
+                    // if(time() >= strtotime(date('2018-06-14 00:00:00')) && time() <= strtotime(date('2018-06-14 23:59:59'))){
+                        
+                    // }
                     break;
                 default: break;
             }
