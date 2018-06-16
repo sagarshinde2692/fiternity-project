038447d8cb7ddb6e80aca52cf38d307fbbef54e5
Mahesh Jadhav <maheshjadhav@fitternity.com>
2018-06-16 17:32:26 +0530

    changes

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -1047,24 +1047,29 @@ Class Utilities {
 
                     $customerCoupn->update();
 
-                    $myrewards = \Myrewards::find($customerCoupn['myreward_id']);
+                    $myreward = \Myreward::find($customerCoupn['myreward_id']);
 
-                    $myrewardsData = $myrewards->toArray();
+                    $myrewardData = $myreward->toArray();
 
-                    $coupon_detail = $myrewardsData['coupon_detail'];
+                    $coupon_detail = $myrewardData['coupon_detail'];
 
                     foreach ($coupon_detail as $key => &$value) {
 
-                        if(!isset($value['claimed'])){
-                            $value['claimed'] = 0;
+                        if($value['code'] == strtolower($order['coupon_code'])){
+
+                            if(!isset($value['claimed'])){
+                                $value['claimed'] = 0;
+                            }
+
+                            $value['claimed'] += 1;
+
+                            $myreward->coupon_detail = $coupon_detail;
+                            $myreward->update();
+
+                            break;
                         }
 
-                        $value['claimed'] += 1;
-
                     }
-
-                    $myrewards->coupon_detail = $coupon_detail;
-                    $myrewards->update();
 
                 }      
             }
