d9a5ac8e36010ec0a847e9c510968ac4ed7c630b
fitdhruv <dhruvsarawagi@fitternity.com>
2018-06-18 17:58:38 +0530

    pps_stripe app version

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -4246,7 +4246,10 @@ class FindersController extends \BaseController {
 						$finderData['call_for_action_button'] = "";
 						$finderData['finder']['pay_per_session'] = false;
 					}
-					$finderData['finder']['services'] = $this->addPPSStripe($finderData['finder']['services']);
+
+					if($_GET['device_type']=='android' && $_GET['app_version'] > 4.8){
+						$finderData['finder']['services'] = $this->addPPSStripe($finderData['finder']['services']);
+					}
 				}
 				if($finderData['finder']['commercial_type'] == 0){
 					$finderData['finder']['trial'] = "disable";
