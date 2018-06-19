05c57359d056bf30e1d79f3061f708ec270af4a1
fitdhruv <dhruvsarawagi@fitternity.com>
2018-06-19 17:57:18 +0530

    finder-banners-daywise

--- app/config/database.php
+++ app/config/database.php
@@ -6,15 +6,15 @@
 // $password = "";
 
 //stage
-// $host = "apistage.fitn.in";
-// $username = "admin";
-// $password = "fit123";
-// $options = ['db' => 'admin','authMechanism' => 'MONGODB-CR']; // sets the authentication database required by mongo 3]
+$host = "apistage.fitn.in";
+$username = "admin";
+$password = "fit123";
+$options = ['db' => 'admin','authMechanism' => 'MONGODB-CR']; // sets the authentication database required by mongo 3]
 
 //production
-$host = "54.179.134.14";
-$username = "";
-$password = "";
+// $host = "54.179.134.14";
+// $username = "";
+// $password = "";
 
 return array(
 
@@ -69,8 +69,8 @@ return array(
 			'port'     => 27017,
 			'database' => 'fitadmin',
 		    'username' => $username,
-		    'password' => $password
-			// 'options' => $options
+		    'password' => $password,
+			'options' => $options
 			// 'options' => [
 			// 		'db' => 'admin' // sets the authentication database required by mongo 3
 			// 	]
@@ -82,8 +82,8 @@ return array(
 			'port'     => 27017,
 			'database' => 'fitapi',
             'username' => $username,
-            'password' => $password
-			// 'options' => $options
+            'password' => $password,
+			'options' => $options
 			// 'options' => [
 			// 	'db' => 'admin' // sets the authentication database required by mongo 3
 			// ]
@@ -96,7 +96,7 @@ return array(
 		// 	'port'     => 27017,
 		// 	'database' => 'fitadminnew',
         //     'username' => $username,
-        //     'password' => $password
+        //     'password' => $password,
 		// ),
 
 
