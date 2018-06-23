93835c4a42e0d49ad04675597f27da9908391bf4
utkarsh <ut.mehrotra@gmail.com>
2018-06-23 16:05:38 +0530

    app banner changed

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -3461,7 +3461,7 @@ class CustomerController extends \BaseController {
 
 			$result['campaigns'] =  [];
 			$result['campaigns'][] = [
-				'image'=>'https://b.fitn.in/global/Homepage-branding-2018/app-banner/monsoon-app.png',
+				'image'=>'https://b.fitn.in/global/Homepage-branding-2018/app-banner/monsoon-with-code.jpg',
 				'link'=>'ftrnty://ftrnty.com/search/all',
 				'title'=>'Group Membership',
 				'height'=>100,
