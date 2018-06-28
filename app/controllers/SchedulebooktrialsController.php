9b8085e5222ea5b409609d2cb1fa8d2e1efa7a7f
fitdhruv <dhruvsarawagi@fitternity.com>
2018-06-28 12:34:27 +0530

    lost fitcaode message

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -7231,7 +7231,7 @@ class SchedulebooktrialsController extends \BaseController {
 
                 if($booktrial['type'] == 'booktrials'){
                     unset($response['sub_header_1']);
-                    $response['sub_header_2'] = "Surprise discount will be given to you in form of fitcash post we verify your attendance with ".ucwords($booktrial['finder_name']);
+                    $response['sub_header_2'] = $result->message;
                 }
 
                 Log::info("removing n+2 communication");
