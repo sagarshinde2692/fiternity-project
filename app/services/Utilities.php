3f07f04fa37e2d98e41a1b76709725f455dbea62
fitshahaan <shahaansyed@fitternity.com>
2018-07-18 15:50:18 +0530

    sdsd

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -4824,7 +4824,8 @@ Class Utilities {
 			if(!empty($cart))
 			{
 				$cart=$cart->toArray();
-				$tmp_data=array_values(array_filter($cart,function ($e) use ($data) {return (!empty($data['ratecard_id'])&&!empty($e['ratecard_id'])&&$data['ratecard_id']== $e['ratecard_id']);}));
+				if(!empty($cart['products']))
+				$tmp_data=array_values(array_filter($cart['products'],function ($e) use ($data) {return (!empty($data['ratecard_id'])&&!empty($e['ratecard_id'])&&$data['ratecard_id']== $e['ratecard_id']);}));
 				if(!empty($tmp_data))
 				{
 					$tmp_data=$tmp_data[0];
