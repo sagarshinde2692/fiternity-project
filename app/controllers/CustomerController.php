b44a1cc3f3dc9195f459b910f59116ef6245b047
utkarsh <ut.mehrotra@gmail.com>
2018-07-25 00:14:53 +0530

    http

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -3475,7 +3475,7 @@ class CustomerController extends \BaseController {
 			$result['campaigns'] =  [];
 
 			$result['campaigns'][] = [
-				'image'=>'b.fitn.in/global/Homepage-branding-2018/app-banner/monsoon-with-code.jpg',
+				'image'=>'https://b.fitn.in/global/Homepage-branding-2018/app-banner/monsoon-with-code.jpg',
 				'link'=>'ftrnty://ftrnty.com/search/all',
 				'title'=>'Group Membership',
 				'height'=>100,
