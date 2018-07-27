022234608945879c3e0fa4ab8177f4a30715f966
Mahesh Jadhav <maheshjadhav@fitternity.com>
2018-07-27 17:09:29 +0530

    changes

--- app/controllers/RewardofferController.php
+++ app/controllers/RewardofferController.php
@@ -1073,7 +1073,7 @@ class RewardofferController extends BaseController {
                 ];
                 $rewardObjData['new_amount'] = 6000;
                 $rewardObjData['payload']['amount'] = 600;
-                $rewardObjData['description'] = 'We have curated the perfect partner to kickstart your membership. Strike off this workout essential from your list & get going.'.implode('<br>- ',$rewards_snapfitness_contents);
+                $rewardObjData['description'] = 'We have curated the perfect partner to kickstart your membership. Strike off this workout essential from your list & get going.<br>- '.implode('<br>- ',$rewards_snapfitness_contents);
 
                 $rewards[] = $rewardObjData;
             }
