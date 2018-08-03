5cb334a1b8f5ed1710891b3117f6378a9249ccb0
fitshahaan <shahaansyed@fitternity.com>
2018-08-03 18:38:38 +0530

    sdsd

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -1,4 +1,4 @@
-<?PHP
+\<?PHP
 
 /** 
  * ControllerName : CustomerController.
@@ -7448,13 +7448,13 @@ class CustomerController extends \BaseController {
 								if(!empty($add_chck)&&$add_chck['status']==200)
 								{
 									$total_fitcash=$total_fitcash+$fitcash;
-									$resp1=$this->utilities->getAttendedResponse('attended',$booktrial,$customer_level_data,$pending_payment,$payment_done,null,null);	
+									$resp1=$this->utilities->getAttendedResponse('attended',$booktrial,$customer_level_data,$pending_payment,$payment_done,$fitcash,$add_chck);	
 									array_push($attended,$resp1);
 								}
 								else array_push($un_updated,$value['_id']);
 							}
 							else  {
-								$resp1=$this->utilities->getAttendedResponse('didnotattended',$booktrial,$customer_level_data,$pending_payment,$payment_done,$fitcash,$add_chck);
+								$resp1=$this->utilities->getAttendedResponse('didnotattended',$booktrial,$customer_level_data,$pending_payment,$payment_done,null,null);
 								array_push($not_attended,$resp1);
 								
 							   }
