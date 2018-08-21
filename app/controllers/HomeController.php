68187cd71bc2c0a922db557e8d0850a03cf4d721
fitdhruv <dhruvsarawagi@fitternity.com>
2018-08-21 15:58:36 +0530

    gift success

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2026,6 +2026,13 @@ class HomeController extends BaseController {
                 'why_buy'=>$why_buy
             ];
 
+            if(isset($itemData['coupon_id'])){
+                $resp['coupon'] = Giftcoupon::find($itemData['coupon_id']);
+            }
+            if(isset($itemData['fitcash_coupon_code'])){
+                $resp['fitcash_coupon_code'] = $itemData['fitcash_coupon_code'];
+            }
+
             Log::info(" RX USER ".print_r($itemData,true));
             if(isset($itemData['rx_user'])&&$itemData['rx_user']&&isSet($itemData['rx_success_url'])&&$itemData['rx_success_url']!="")
             	$resp['rx_success_url'] =	$itemData['rx_success_url'];
