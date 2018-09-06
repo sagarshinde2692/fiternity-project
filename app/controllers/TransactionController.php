85fba8710381ed234a0de3ee5027229ff754e035
fitdhruv <dhruvsarawagi@fitternity.com>
2018-09-06 18:13:51 +0530

    success_msg

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -5137,7 +5137,7 @@ class TransactionController extends \BaseController {
         $result['payment_related_details_for_mobile_sdk_hash'] = $mobilehash;
         $result['finder_name'] = strtolower($data['finder_name']);
         $result['fitcash_amount'] = $data['fitcash_amount'];
-        $result['success_message'] = $data['fitcash_amount']." fitcash has been added into your wallet";
+        $result['success_msg'] = $data['fitcash_amount']." fitcash has been added into your wallet";
         
         
         $resp   =   array(
