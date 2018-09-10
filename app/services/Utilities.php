06d2f500792489590b34c37c33843ad70aaa033b
fitdhruv <dhruvsarawagi@fitternity.com>
2018-09-10 16:44:25 +0530

    lastname optional

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -5399,7 +5399,7 @@ Class Utilities {
 
         $rules = [
             'firstname'=>'required | string',
-            'lastname'=>'required | string',
+            // 'lastname'=>'required | string',
             'customer_email'=>'required | email',
             'customer_phone'=>'required|regex:/[0-9]{10}/',
         ];
@@ -5410,7 +5410,11 @@ Class Utilities {
             return array('status' => 404,'message' => error_message($validator->errors()));
         }
 
-        $data['customer_name'] = $customer['firstname'].''.$customer['lastname'];
+        $data['customer_name'] = $customer['firstname'];
+
+        if(!empty($customer['lastname'])){
+            $data['customer_name'] = $data['customer_name'].' '.$customer['lastname'];
+        }
 
         $data = array_merge($data, array_only($customer, ['customer_email', 'customer_phone']));
 
