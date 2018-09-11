d65191e055d3281874085a0f9bd94f6f8cc87332
utkarsh <ut.mehrotra@gmail.com>
2018-09-11 12:07:42 +0530

    luxury in pune and golds pcmc

--- app/helpers.php
+++ app/helpers.php
@@ -152,7 +152,7 @@ if(!function_exists('citywise_category')){
                 ["name" => "Healthy Tiffins","slug" => "healthy-tiffins"],
                 // ["name" => "Personal Trainers","slug" => "personal-trainers"],
                 // ["name" => "Sport Nutrition Supplement Stores","slug" => "sport-nutrition-supplement-stores"],
-                // ["name" => "Luxury Hotels","slug" => "luxury-hotels"],
+                ["name" => "Luxury Hotels","slug" => "luxury-hotels"],
             	["name" => "Swimming","slug" => "swimming-pools"],
                 ["name" => "Aerial Fitness","slug" => "aerial-fitness"],
                 ["name" => "Pre-natal Classes","slug" => "pre-natal-classes"],
