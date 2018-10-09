1e2444ba515785a67ee19a99fe5286e81b2534d2
fitdhruv <dhruvsarawagi@fitternity.com>
2018-10-09 11:12:08 +0530

    pay_at_vendor convinience

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -2969,7 +2969,8 @@ Class Utilities {
            ($this->vendor_token)||
            (in_array($data['finder_id'],Config::get('app.vendors_without_convenience_fee')))||
            (isset($flags) && isset($flags["pay_at_vendor"]) && $flags["pay_at_vendor"] === True)||
-           (!empty($data['type']) && in_array($data['type'], ["workout session", "workout-session", "trial", "booktrials","events"])))
+           (!empty($data['type']) && in_array($data['type'], ["workout session", "workout-session", "trial", "booktrials","events"]))||
+           (!empty($data['ratecard_pay_at_vendor'])))
         {
             return false;
         }
