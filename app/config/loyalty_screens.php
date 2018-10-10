e229c71f31ded9755e5182898173aad654872e80
fitdhruv <dhruvsarawagi@fitternity.com>
2018-10-10 12:38:10 +0530

    register_url

--- app/config/loyalty_screens.php
+++ app/config/loyalty_screens.php
@@ -50,7 +50,7 @@ return array(
         'footer' => [
             'image' => 'https://b.fitn.in/loyalty/Footer11.png',
             'ratio' => 1.38,
-            'url'=> Config::get('app.website', 'https://www.fitternity.com').'/fitsquad?app=true'
+            'url'=> Config::get('loyalty_constants.register_url')
         ],
     ],
     'post_register' => [
