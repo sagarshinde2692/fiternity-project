b5257d86e1a7de862c54d5f10cb287dcca36d80b
fitdhruv <dhruvsarawagi@fitternity.com>
2018-10-12 19:14:45 +0530

    claim message

--- app/config/loyalty_screens.php
+++ app/config/loyalty_screens.php
@@ -120,7 +120,7 @@ return array(
         'button_title' => 'Claim',
         'terms' => 'terms',
         'coupon_description' => 'description',
-        'claim_message' => "Are you sure you want to claim this reward as you won't be able to claim other rewards for this milestone?",
+        'claim_message' => "Are you sure you want to claim this reward? After claiming other rewards will get blocked.",
     ],
     'success_loyalty_header' => [
         'logo' => 'https://b.fitn.in/loyalty/SUCCESS%20PAGE%20LOGO.png',
