2e16286bd36d4162e684d452375ae36183101e5d
fitdhruv <dhruvsarawagi@fitternity.com>
2018-10-17 20:05:27 +0530

    coupon

--- app/services/CustomerReward.php
+++ app/services/CustomerReward.php
@@ -1860,10 +1860,10 @@ Class CustomerReward {
                    
                 }
                 if(empty($finder)){
-                    $finder = Finder::where('_id', $ratecard['finder_id'])->first(['flags']);
+                    $finder = Finder::where('_id', $ratecard['finder_id'])->first();
                 }
                 if(empty($service)){
-                    $service = Service::where('_id', $ratecard['service_id'])->first(['flags','servicecategory_id']);
+                    $service = Service::where('_id', $ratecard['service_id'])->first();
                 }
 
                 if(!empty($coupon_data['service_category_ids']) && !in_array($service['servicecategory_id'],$coupon_data['service_category_ids'])){
