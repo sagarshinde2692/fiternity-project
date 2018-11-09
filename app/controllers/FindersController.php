1e82203a978acfb91c8752e1cfc4f1b85ec2741d
utkarsh <ut.mehrotra@gmail.com>
2018-11-09 21:00:08 +0530

    done

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -675,9 +675,9 @@ class FindersController extends \BaseController {
 										$service['serviceratecard'][$ratekey]['direct_payment_enable'] = "0";
 									}
 									
-									if(in_array($rateval['type'], ['workout session']) && in_array($service["trial"],["manual","diable", "manualauto"]) || in_array($finder["trial"],["manual","diable", "manualauto"]) || in_array($finder["category_id"],[47])){
-										$service['serviceratecard'][$ratekey]['special_price'] = $rateval['peak_price'];
-									}
+									// if(in_array($rateval['type'], ['workout session']) && in_array($service["trial"],["manual","diable", "manualauto"]) || in_array($finder["trial"],["manual","diable", "manualauto"]) || in_array($finder["category_id"],[47])){
+									// 	$service['serviceratecard'][$ratekey]['special_price'] = $rateval['peak_price'];
+									// }
 
 									$customerDiscount = 0;
 									// $customerDiscount = $this->utilities->getCustomerDiscount();
