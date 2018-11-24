8183adf7666728ee57e8e07a17962703be708695
utkarsh <ut.mehrotra@gmail.com>
2018-11-24 15:27:43 +0530

    config

--- app/config/amazonpay.php
+++ app/config/amazonpay.php
@@ -4,7 +4,7 @@ return array(
 				'merchant_id'   => 'AS97SQY1JSHGO',
 				'access_key'    => 'AKIAJ4JOUIDD4735OIFA',
 				'secret_key'    => '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',
-				'base_url'      => 'amazonpay.amazon.in'
+				'base_url'      => 'https://amazonpay.amazon.in'
 		),
 		'config_seamless' => array(
 				'merchant_id'   => 'AS97SQY1JSHGO',
