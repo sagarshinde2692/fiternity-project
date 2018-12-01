2b4b0c76c8cfecd69d23c0a551a41f60cf6b4ee1
fitdhruv <dhruvsarawagi@fitternity.com>
2018-12-01 19:34:50 +0530

    fix

--- app/models/Service.php
+++ app/models/Service.php
@@ -180,10 +180,10 @@ class Service extends \Basemodel{
         $finder = $this->finder;
 		if(!empty($this->_id) && isset($this->_id)){
 
-            if(!empty($finder->brand_id) && $finder->brand_id != 130){
-			    $ratecardsarr 	= 	Ratecard::active()->where('service_id', intval($this->_id))->where('type', '!=', 'trial')->orderBy('order', 'asc')->get()->toArray();
-            }else{
+            if(!empty($finder->brand_id) && $finder->brand_id == 130){
 			    $ratecardsarr 	= 	Ratecard::active()->where('service_id', intval($this->_id))->orderBy('order', 'asc')->get()->toArray();
+            }else{
+			    $ratecardsarr 	= 	Ratecard::active()->where('service_id', intval($this->_id))->where('type', '!=', 'trial')->orderBy('order', 'asc')->get()->toArray();
             }
 		}
 
