453046918fa4ff0b4a8ff350ffc8d6e5a71fc319
fitdhruv <dhruvsarawagi@fitternity.com>
2018-12-07 16:36:46 +0530

    ordertoppsdiva

--- app/controllers/DebugController.php
+++ app/controllers/DebugController.php
@@ -7999,6 +7999,89 @@ public function yes($msg){
         return $orders;
     }
 
+    public function convertOrdersToPPSDiva(){
+        $data = Input::all();
+	    $order_ids = Input::all()['order_ids'];
+
+        // $already_added_order_ids = Wallet::whereIn('membership_order_id', $order_ids)->lists('membership_order_id');
+        $already_added_order_ids = [];
+
+        $order_ids = array_values(array_diff($order_ids, $already_added_order_ids));
+
+        $orders = Order::whereIn('_id', $order_ids)->get();
+
+        foreach ($orders as $order){
+
+            $finder_id = $order->finder_id;
+
+            $finder_name = $order->finder_name;
+
+            if(!empty($data['finder_id'])){
+            
+                $finder = Finder::find($data['finder_id']);
+
+                $finder_name = $finder['title'];
+                $finder_id = $finder['_id'];
+            
+            }
+
+            Log::info($order->_id);
+
+            $amount = $order->amount_customer;
+
+            if(!empty($data['amount'])){
+                $amount = $data['amount'];
+            }
+            
+
+            $amount1 = $amount;
+
+            // $amount2 = $amount - $amount1;
+
+    		$utilities = new Utilities();
+
+    		$walletData = array(
+                "customer_id"=> $order->customer_id,
+                "amount"=> $amount,
+                "amount_fitcash" => 0,
+                "amount_fitcash_plus" => $amount,
+                "type"=>'FITCASHPLUS',
+                'description'=>"Added FitCash+ for converting 1 month membership to pay-per-session only applicable on ".$finder_name,
+                'entry'=>'credit',
+                'valid_finder_id'=>$finder_id,
+                'remove_wallet_limit'=>true,
+                'validity'=>strtotime($order['start_date'])+(86400*90),
+                'order_type'=>['workout-session', 'workout session'],
+                'membership_order_id'=>$order->_id
+            );
+
+            return $walletData;
+            
+            $walletTransaction = $utilities->walletTransactionNew($walletData);
+
+            // $walletData = array(
+            //     "customer_id"=> $order->customer_id,
+            //     "amount"=> $amount2,
+            //     "amount_fitcash" => 0,
+            //     "amount_fitcash_plus" => $amount2,
+            //     "type"=>'FITCASHPLUS',
+            //     'description'=>"Added FitCash+ for converting 1 month membership to pay-per-session applicable across Fitternity",
+            //     'entry'=>'credit',
+            //     'remove_wallet_limit'=>true,
+            //     'validity'=>0,
+            //     'order_type'=>['workout-session', 'workout session'],
+            //     'membership_order_id'=>$order->_id
+            // );
+
+            // $walletTransaction = $utilities->walletTransactionNew($walletData);
+
+            $order->update(["converting_membership_to_pps"=>true, 'converting_membership_to_pps_date'=>time()]);
+        }
+
+        Log::info("done");
+        return $orders;
+    }
+
 	public function addPicturesToRatingParams(){
 		$filenames = array_merge(glob("*.png"), glob("*.PNG"));
 
