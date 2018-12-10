b60535a08111fbc35505409cebe92f08a0881ec2
fitdhruv <dhruvsarawagi@fitternity.com>
2018-12-10 17:41:01 +0530

    pay_per_session false

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -4522,24 +4522,24 @@ class FindersController extends \BaseController {
 					// 	$finderData['finder']['pay_per_session'] = true;
 					// }
 
-					if(isset($finderData['finder']['pay_per_session']) && $finderData['finder']['pay_per_session']){
+					// if(isset($finderData['finder']['pay_per_session']) && $finderData['finder']['pay_per_session']){
 
-						$cheapest_price = $this->getCheapestWorkoutSession($finderData['finder']['services_workout'], 'app');
+					// 	$cheapest_price = $this->getCheapestWorkoutSession($finderData['finder']['services_workout'], 'app');
 						
-						if($cheapest_price>0){
+					// 	if($cheapest_price>0){
 
-							$finderData['finder']['pps_content'] = [
-								'header1'=>	'PAY - PER - SESSION',
-								'header2'=>	'Available here',
-								'header3'=>	"Why pay for 30 days when you use for 6 days?\nPay Per Session at ".$finderData['finder']['title']." by just paying Rs. ".$cheapest_price,
-								'image'=>''
-							];
+					// 		$finderData['finder']['pps_content'] = [
+					// 			'header1'=>	'PAY - PER - SESSION',
+					// 			'header2'=>	'Available here',
+					// 			'header3'=>	"Why pay for 30 days when you use for 6 days?\nPay Per Session at ".$finderData['finder']['title']." by just paying Rs. ".$cheapest_price,
+					// 			'image'=>''
+					// 		];
 
-						}else{
+					// 	}else{
 
 							$finderData['finder']['pay_per_session'] = false;
-						}
-					}
+					// 	}
+					// }
 
 					if(!in_array("false", $disable_button)){
 						$finderData['call_for_action_button'] = "";
