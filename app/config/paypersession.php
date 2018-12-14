09dcc674fa4ba772ca35fe02d49daa780af17565
fitdhruv <dhruvsarawagi@fitternity.com>
2018-12-14 18:47:27 +0530

    pps home feedback from config

---
+++ app/config/paypersession.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+
+    'home_feedback' => [['text' => "I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name' => "Rohit", "location" => "Mumbai"], ['text' => "I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name' => "Rohit", "location" => "Mumbai"], ['text' => "I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name' => "Rohit", "location" => "Mumbai"], ['text' => "I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name' => "Rohit", "location" => "Mumbai"]],
+
+);
--- app/controllers/ServiceController.php
+++ app/controllers/ServiceController.php
@@ -2175,7 +2175,7 @@ class ServiceController extends \BaseController {
          try{
 
 			if(empty($this->app_version) ){
-               $data['feedback'] = [['text'=>"I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name'=>"Rohit", "location"=>"Mumbai"],['text'=>"I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name'=>"Rohit", "location"=>"Mumbai"],['text'=>"I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name'=>"Rohit", "location"=>"Mumbai"],['text'=>"I've been using Fitternity services, the experience has always been pleasant. The offers are always fascinating, also the customer success team has always been helpful. The online facility has made it so easier for me to try out different options instantly &amp; also continue with my regular fitness always! ", 'name'=>"Rohit", "location"=>"Mumbai"]];
+               $data['feedback'] = Config::get('paypersession.home_feedback');
 			}			
 
 
