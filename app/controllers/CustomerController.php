9e5a3a5256d5979755f6ab3adc87429273f02e5c
fitdhruv <dhruvsarawagi@fitternity.com>
2018-12-31 13:10:06 +0530

    home fix

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -3591,9 +3591,10 @@ class CustomerController extends \BaseController {
 		}
         // if($this->app_version > '')
         try{
-            // if((!empty($_GET['device_type']) && !empty($_GET['app_version'])) && ((in_array($_GET['device_type'], ['android']) && $_GET['app_version'] >= '5.18') || ($_GET['device_type'] == 'ios' && $_GET['app_version'] >= '5.1.5'))){
+            $active_session_packs = [];
+            if((!empty($_GET['device_type']) && !empty($_GET['app_version'])) && ((in_array($_GET['device_type'], ['android']) && $_GET['app_version'] >= '5.18') || ($_GET['device_type'] == 'ios' && $_GET['app_version'] >= '5.1.5'))){
                 $active_session_packs = $this->getSessionPacks(null, null, true, $customer_id)['data'];
-            // }
+            }
 
         }catch(Exception $e){
 
