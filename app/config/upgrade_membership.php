b0a2ee537ecc3b477586217aadd980b763c91c27
fitdhruv <dhruvsarawagi@fitternity.com>
2019-01-15 22:15:11 +0530

    how it works

--- app/config/upgrade_membership.php
+++ app/config/upgrade_membership.php
@@ -4,18 +4,18 @@ return array(
 
     'ratecard_popup'=>[
         'header'=>'GET EXCLUSIVE ACCESS TO UPGRADE YOUR MEMBERSHIP',
-        'title'=>'Here is how it works:',
+        'title'=>'How it works:',
         'data'=>[
             'Get 100% instant cashback in your Fitternity wallet to upgrade your 1 month membership to 1 year.',
-            'The Fitcash will get automatically applied on upgrading to a 1 year membership at __finder_name, __finder_location: __service_name hassle free.',
-            'The Fitcash is valid for 20 days from the start date of your membership.'
+            'Fitcash will get automatically applied on upgrading to a 1 year membership for __service_name at __finder_name, __finder_location .',
+            'This instant Fitcash is valid for 20 days from the start date of your membership.'
         ]
     ],
 
     'success_page'=>[
         'head'=>'HURRAY!',
         'header'=>'100% cashback has been added to your Fitternity wallet! You have an exclusive privilege to upgrade your 1 month membership!',
-        'title'=>'Here is how it works:',
+        'title'=>'How it works:',
         'data'=>[
             '100% Cashback has been added in your Fitternity wallet to upgrade your 1 month membership to 1 year.',
             'The Fitcash bonus will get automatically applied on upgrading to a 1 year membership at __finder_name, __finder_location: __service_name hassle free.',
