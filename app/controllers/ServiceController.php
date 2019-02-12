17597e191ca2fc199a6707660c236b31b6328c37
fitdhruv <dhruvsarawagi@fitternity.com>
2019-02-12 20:24:16 +0530

    nearby_options count

--- app/controllers/ServiceController.php
+++ app/controllers/ServiceController.php
@@ -2162,10 +2162,13 @@ class ServiceController extends \BaseController {
                 $search_resp = $this->utilities->getPPSSearchResult(['city'=>$city
                 ,'keys'=>['name', 'slug', 'location', 'coverimage', 'vendor_name','vendor_slug','average_rating', 'total_rating_count']
                 ]);
-				$data['nearby_options']['header'] = "There are ".$search_resp['metadata']['total_records']." sessions happening near you in the next 4 hours";
-                $data['nearby_options']['view_all_link'] = Config::get('app.website')."/pay-per-session/".$city."/fitness?day=within-4-hours";
-                $data['nearby_options']['results'] = $search_resp['results'];
-			}			
+                
+                if(isset($search_resp['metadata']['ppsData']) && isset($search_resp['metadata']['ppsData']['count'])){
+                    $data['nearby_options']['header'] = "There are ".$search_resp['metadata']['ppsData']['count']." sessions happening near you in the next 4 hours";
+                    $data['nearby_options']['view_all_link'] = Config::get('app.website')."/pay-per-session/".$city."/fitness?day=within-4-hours";
+                    $data['nearby_options']['results'] = $search_resp['results'];
+                }
+			}					
 
 
 		}catch(Exception $e){
