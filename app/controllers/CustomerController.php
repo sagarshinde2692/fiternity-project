5a981bc7d4b7f4591a267da17e50b96cb9df590b
Akhil Kulkarni <akhil16.4.93@gmail.com>
2019-03-18 16:02:23 +0530

    fix

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -9344,7 +9344,7 @@ class CustomerController extends \BaseController {
 		$order = null;
 		try{
 			if((empty($data) || empty($data['order_id'])) && !empty($data['type']) && $data['type']=='profile'){
-				$order = Order::active()->where('customer_id', $data['customer_id'])->where('type','memberships')->orderBy('_id', 'desc')->first();
+				$order = Order::active()->where('customer_id', intval($data['customer_id']))->where('type','memberships')->orderBy('_id', 'desc')->first();
 				$data['order_id'] = $order['_id'];
 			}
 			if(!empty($data)){
