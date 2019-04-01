566d3d375be285b6b396db0f250e17931d405dfc
fitdhruv <dhruvsarawagi@fitternity.com>
2019-04-01 16:18:10 +0530

    vendor_stripe_data removed

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -1315,56 +1315,46 @@ class FindersController extends \BaseController {
                 $this->removeEmptyServices($response, 'web');
 				                
                 if(empty($response['vendor_stripe_data']['text'])){
-                    if(empty($finder['flags']['state']) || !in_array($finder['flags']['state'], ['closed', 'temporarily_shut'] )){
+                    // if(empty($finder['flags']['state']) || !in_array($finder['flags']['state'], ['closed', 'temporarily_shut'] )){
                         
-                        if(!empty($finder['flags']['cashback_type'])){
+                    //     if(!empty($finder['flags']['cashback_type'])){
                             
-                            $text1 = $this->getVendorStripeCashbackText($finder);
+                    //         $text1 = $this->getVendorStripeCashbackText($finder);
                             
-                            $response['vendor_stripe_data']	=	[
-                                'text1'=> $text1,
-                                'text3'=>"",
-                                'background-color'=> "",
-                                'text_color'=> '$fff',
-                                'background'=> '-webkit-linear-gradient(left, #f26c46 0%, #eea948 100%)'
-                            ];
+                    //         $response['vendor_stripe_data']	=	[
+                    //             'text1'=> $text1,
+                    //             'text3'=>"",
+                    //             'background-color'=> "",
+                    //             'text_color'=> '$fff',
+                    //             'background'=> '-webkit-linear-gradient(left, #f26c46 0%, #eea948 100%)'
+                    //         ];
                             
-                        }
-                        if(empty($finder['flags']['no_disc_mar'])){
+                    //     }
+                    //     if(empty($finder['flags']['no_disc_mar'])){
 
-                            if(!empty($finder['flags']['fit500'])){
-                                $response['vendor_stripe_data']	=	[
+                    //         if(!empty($finder['flags']['fit500'])){
+                    //             $response['vendor_stripe_data']	=	[
                                     
-                                    'text1'=> "Lowest Prices + Get flat Rs 500 off | Code: FIT500",
-                                    'text3'=>"",
-                                    'background-color'=> "",
-                                    'text_color'=> '$fff',
-                                    'background'=> '-webkit-linear-gradient(left, #425f72 0%, #425f72 100%)'
-                                ];
+                    //                 'text1'=> "Lowest Prices + Get flat Rs 500 off | Code: FIT500",
+                    //                 'text3'=>"",
+                    //                 'background-color'=> "",
+                    //                 'text_color'=> '$fff',
+                    //                 'background'=> '-webkit-linear-gradient(left, #425f72 0%, #425f72 100%)'
+                    //             ];
                                 
-                            }else{
-                                $response['vendor_stripe_data']	=	[
-                                    'text1'=> "Lowest Prices + Get addnl flat 10% off | Code: FIT10 - *T&C Applicable",
-                                    'text3'=>"",
-                                    'background-color'=> "",
-                                    'text_color'=> '$fff',
-                                    'background'=> '-webkit-linear-gradient(left, #425f72 0%, #425f72 100%)'
-                                ];
-                            }
-                        }
-						// if(empty($finder['flags']['end_sale_0'])){
-						// 	if(!empty($finder['flags']['end_sale_10'])){
-						// 		$response['vendor_stripe_data']['text3'] = $response['vendor_stripe_data']['text3'].' | Addnl Flat 10% off. Code: JFIT *T&C';
-						// 	}else{
-						// 		$response['vendor_stripe_data']['text3'] = $response['vendor_stripe_data']['text3'].' | Addnl Flat 5% off. Code: JFIT *T&C';
-						// 	}
-						// }
-
-                        // if(!in_array($response['finder']['_id'], Config::get('app.eoys_excluded_vendor_ids'))){
-                        //     $response['vendor_stripe_data']['text3'] = $response['vendor_stripe_data']['text3']." | Addn Flat 10% off. Code: EOYS *T&C";
-                        // }
+                    //         }else{
+                    //             $response['vendor_stripe_data']	=	[
+                    //                 'text1'=> "Lowest Prices + Get addnl flat 10% off | Code: FIT10 - *T&C Applicable",
+                    //                 'text3'=>"",
+                    //                 'background-color'=> "",
+                    //                 'text_color'=> '$fff',
+                    //                 'background'=> '-webkit-linear-gradient(left, #425f72 0%, #425f72 100%)'
+                    //             ];
+                    //         }
+                    //     }
+					
                     
-                    }
+                    // }
                 }else if(!empty($response['vendor_stripe_data']['text'])){
                     $response['vendor_stripe_data']['text1'] = $response['vendor_stripe_data']['text'];
                 }
