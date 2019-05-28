b6361a74fb2200014f884a327ce11ec6203e9a68
fitdhruv <dhruvsarawagi@fitternity.com>
2019-05-28 12:00:49 +0530

    orderRatecards

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -7459,7 +7459,7 @@ class FindersController extends \BaseController {
             });
             
             $membership_ratecards = array_filter($service['serviceratecard'], function($rc){
-                return $rc['type'] != 'extended validity';
+                return $rc['type'] == 'membership';
             });
 
             $session_ratecards = array_filter($service['serviceratecard'], function($rc){
