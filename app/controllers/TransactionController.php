aab34e62c58ee5601792cdcdc63bb9e18afd64b3
Akhil Kulkarni <akhil16.4.93@gmail.com>
2019-05-30 13:41:08 +0530

    ff purchase price change

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -9033,7 +9033,7 @@ class TransactionController extends \BaseController {
                 $post_data['activationdate'] = date('Y-m-d',strtotime($data['data']['preferred_starting_date'])); // '2019-05-29';
             }
             $post_data['total'] = $order['amount_customer'];
-            $post_data['productprice'] = $order['amount_customer'];
+            $post_data['productprice'] = 0.82 * $order['amount_customer'];
             $post_data['paymentmode'] = 'gymtrekker';
             $post_data['amountpaid'] = $order['amount_customer'];
             $post_data['addpaymentids'] = '13731'; // Ganesh Dhumal said they will be making this non-mandatory, keep it for now...
