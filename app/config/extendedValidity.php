9ec6ad111290924c7e8984c1f1e07740eca62b44
kailashbajya <kailashbajya@fitternity.com>
2019-05-30 18:09:35 +0530

    updated extended  validity tags

--- app/config/extendedValidity.php
+++ app/config/extendedValidity.php
@@ -2,82 +2,62 @@
 
 return array(
     "finder_banner" =>[
-            "image"=> "http://b.fitn.in/global/fexbutton.png",
-            "header"=> "GET EXCLUSIVE ACCESS TO EXTEND YOUR MEMBERSHIP",
-            "text"=> "Get exclusive access to extending your membership to complete all your sessions. So don't worry if you miss any of the class, we have got you covered!",
-            "description"=> "",
-            "how_it_works"=> [
-                "header"=> "How it works",
-                "text"=> "",
-                "data"=> [
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "This membership offers you extended validity which gives you the flexibility of attending all your workout sessions as per your convenience."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "All the sessions should be schdeuled basis the schedule/batch of the service that you have purchased."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "Flexi membership is a app only feature. Please download the app post buying to track your sessions."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "Not able to attend? Cancel on the go, 60 minutes prior to your session time through your user profile on the Fitternity App / Reminder notification prior to your session."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "Cancelled sessions can be rebooked later as per your convinience."
-                            ]
-
-                ]
-            ],
-        "terms"=> ""
+        "header"=> "Flexi Membership",
+        "text"=> "
+        <b>GET EXCLUSIVE ACCESS TO EXTEND YOUR MEMBERSHIP</b>
+        <br/>
+            Get exclusive access to extending your membership to complete all your sessions. So don't worry if you miss any of the class, we have got you covered!
+        <br/>
+        <br/>
+        <b>How it works?
+        </b>
+        <br/>
+        <ul>
+            <li>
+                This membership offers you extended validity which gives you the flexibility of attending all your workout sessions as per your convenience.
+            </li>
+            <li>
+                All the sessions should be schdeuled basis the schedule/batch of the service that you have purchased.
+            </li>
+            <li>
+                Flexi membership is a app only feature. Please download the app post buying to track your sessions.
+            </li>
+            <li>
+                Not able to attend? Cancel on the go, 60 minutes prior to your session time through your user profile on the Fitternity App / Reminder notification prior to your session.
+            </li>
+            <li>
+                Cancelled sessions can be rebooked later as per your convinience.
+            </li>
+        </ul>"
     ], 
     "finder_banner_app" =>[ 
-            "image"=> "http://b.fitn.in/global/fexbutton.png",
-            "header"=> "GET EXCLUSIVE ACCESS TO EXTEND YOUR MEMBERSHIP",
-            "text"=> "Get exclusive access to extending your membership to complete all your sessions. So don't worry if you miss any of the class, we have got you covered!",
-            "description"=> "",
-            "how_it_works"=> [
-                "header"=> "How it works",
-                "text"=> "",
-                "data"=> [
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "This membership offers you extended validity which gives you the flexibility of attending all your workout sessions as per your convenience."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "All the sessions should be schdeuled basis the schedule/batch of the service that you have purchased."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "Flexi membership is a app only feature. Please download the app post buying to track your sessions."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "Not able to attend? Cancel on the go, 60 minutes prior to your session time through your user profile on the Fitternity App / Reminder notification prior to your session."
-                            ],
-                            [
-                                "image"=> "",
-                                "header"=> "",
-                                "text"=> "Cancelled sessions can be rebooked later as per your convinience."
-                            ]
-
-                ]
-            ],
-        "terms"=> ""
+        "header"=> "Flexi Membership",
+        "text"=> "
+        <b>GET EXCLUSIVE ACCESS TO EXTEND YOUR MEMBERSHIP</b>
+        <br/>
+            Get exclusive access to extending your membership to complete all your sessions. So don't worry if you miss any of the class, we have got you covered!
+        <br/>
+        <br/>
+        <b>How it works?
+        </b>
+        <br/>
+        <ul>
+            <li>
+                This membership offers you extended validity which gives you the flexibility of attending all your workout sessions as per your convenience.
+            </li>
+            <li>
+                All the sessions should be schdeuled basis the schedule/batch of the service that you have purchased.
+            </li>
+            <li>
+                Flexi membership is a app only feature. Please download the app post buying to track your sessions.
+            </li>
+            <li>
+                Not able to attend? Cancel on the go, 60 minutes prior to your session time through your user profile on the Fitternity App / Reminder notification prior to your session.
+            </li>
+            <li>
+                Cancelled sessions can be rebooked later as per your convinience.
+            </li>
+        </ul>"
     ]
         
 );
