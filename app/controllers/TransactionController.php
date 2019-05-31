4154614c6d19a440af3f8e9954ddc08c7138fb5c
Akhil Kulkarni <akhil16.4.93@gmail.com>
2019-05-31 13:31:33 +0530

    fix

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -9044,7 +9044,7 @@ class TransactionController extends \BaseController {
             $fitnessForceData = ['form_params' => []];
             $fitnessForceData['form_params']['firstname'] = $post_data['first_name'];
             $fitnessForceData['form_params']['lastname'] = $post_data['last_name'];
-            $fitnessForceData['form_params']['mobileno'] = $post_data['customer_contact'];
+            $fitnessForceData['form_params']['mobileno'] = $post_data['customer_phone'];
             $fitnessForceData['form_params']['emailid'] = $post_data['customer_email'];
             $fitnessForceData['form_params']['productprice'] = $post_data['productprice'];
             $fitnessForceData['form_params']['paymentmode'] = 'gymtrekker';
