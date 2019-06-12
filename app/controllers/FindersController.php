7da52d699261fc48d20f652697ccee36feedf761
fitdhruv <dhruvsarawagi@fitternity.com>
2019-06-13 00:50:45 +0530

    pps section

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -5024,14 +5024,14 @@ class FindersController extends \BaseController {
                                     'image'=>'https://b.fitn.in/global/pps%20-%20web/Path%2027%403x.png'
                                 ],
                                 [
-                                    'title'=>"Super Easy",
-                                    'subtitle'=>"Book, Reschedule, Cancel on the go",
-                                    'image'=>'https://b.fitn.in/global/pps%20-%20web/Group%20325%403x.png'
-                                ],
-                                [
                                     'title'=>"Unlimited Access",
                                     'subtitle'=>"Book multiple sessions.",
                                     'image'=>'https://b.fitn.in/global/pps%20-%20web/Group%20323%403x.png'
+                                ],
+                                [
+                                    'title'=>"Super Easy",
+                                    'subtitle'=>"Book, Reschedule, Cancel on the go",
+                                    'image'=>'https://b.fitn.in/global/pps%20-%20web/Group%20325%403x.png'
                                 ],
                                 [
                                     'title'=>"Get Addicted",
