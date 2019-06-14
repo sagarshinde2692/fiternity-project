f47212daab1d498819f681f2da4775199f5e0795
Akhil Kulkarni <akhilkulkarni@fitternity.com>
2019-06-14 18:02:34 +0530

    slash added findercategory

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -2500,7 +2500,11 @@ class FindersController extends \BaseController {
 		$this->cacheapi->flushTagKey('finder_detail',$finder->slug);
 		$this->cacheapi->flushTagKey('review_by_finder_list',$finder->slug);
 		$this->cacheapi->flushTagKey('finder_detail_android',$finder->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android',$finder->slug.'-'.$category->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
 		$this->cacheapi->flushTagKey('finder_detail_android_3_2',$finder->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_3_2',$finder->slug.'-'.$category->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_3_2',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
 		$this->cacheapi->flushTagKey('finder_detail_ios',$finder->slug);
 		$this->cacheapi->flushTagKey('finder_detail_ios',$finder->slug.'-'.$category->slug);
 		$this->cacheapi->flushTagKey('finder_detail_ios',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
@@ -2511,8 +2515,14 @@ class FindersController extends \BaseController {
 		$this->cacheapi->flushTagKey('finder_detail_ios_4_4_3',$finder->slug.'-'.$category->slug);
 		$this->cacheapi->flushTagKey('finder_detail_ios_4_4_3',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
 		$this->cacheapi->flushTagKey('finder_detail_android_4_4_3',$finder->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_4_4_3',$finder->slug.'-'.$category->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_4_4_3',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
 		$this->cacheapi->flushTagKey('finder_detail_android_5_1_8',$finder->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_5_1_8',$finder->slug.'-'.$category->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_5_1_8',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
 		$this->cacheapi->flushTagKey('finder_detail_android_5_1_9',$finder->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_5_1_9',$finder->slug.'-'.$category->slug);
+		$this->cacheapi->flushTagKey('finder_detail_android_5_1_9',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
 		$this->cacheapi->flushTagKey('finder_detail_ios_5_1_5',$finder->slug);
 		$this->cacheapi->flushTagKey('finder_detail_ios_5_1_5',$finder->slug.'-'.$category->slug);
 		$this->cacheapi->flushTagKey('finder_detail_ios_5_1_5',$finder->slug.'-'.$category->slug.'-'.$finder->location_id);
