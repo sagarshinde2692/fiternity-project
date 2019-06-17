19cb274467b9fd8cd5bc52f99b8d1d4e73afa7d4
kailashbajya <kailashbajya@fitternity.com>
2019-06-18 00:21:21 +0530

    removed comments

--- app/controllers/ServiceController.php
+++ app/controllers/ServiceController.php
@@ -1244,10 +1244,8 @@ class ServiceController extends \BaseController {
                     }
 
                 }
-            }
-
-			Log::info('pps price offer checking::::::::::', [$item['_id']]);
-
+			}
+			
 			$data['slots'] = $this->ppsPriceOffer($data['slots'], $item['_id'], 'pps_offer');
 
 	        return Response::json($data,200);
@@ -2346,11 +2344,11 @@ class ServiceController extends \BaseController {
 
 	public function ppsPriceOffer($data, $service_id, $type){
 		$offers = Offer :: where('vendorservice_id', $service_id)->where('type', $type)->orderBy('_id', 'desc')->select('price')->first();
-		Log::info('offers:::::', [ $offers]);
+
 		if(count($offers))
 		{
 			foreach($data as $key=>$value)
-			{	Log::info('value::::::', [$offers]);
+			{
 				$data[$key]['price'] = "₹ ".$offers['price'];
 				foreach($value['data'] as $key1 => $value1)
 				{
