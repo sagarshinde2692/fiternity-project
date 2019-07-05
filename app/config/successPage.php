54f4398ea92aa664a354d4a8f2ca5f81588ddf48
kailashbajya <kailashbajya@fitternity.com>
2019-07-05 20:56:31 +0530

    added succes page config

---
+++ app/config/successPage.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    "conclusion" =>[
+        "title"=>"Any Queries? Contact Us",
+        "description"=>"Any Queries? Contact Us",
+        "email"=>"support@fitternity.com",
+        "phone"=>"+912261094444"
+    ],
+    "feedback" =>[
+        'reason'=>[
+            'Choice of fitness options available',
+            'Information provided about the gym/studio',
+            'Transaction & Booking process',
+            'Payment/Cashback/Offer related',
+            'Any other issue'
+        ],
+        'threshold_value'=>6
+    ]
+];
