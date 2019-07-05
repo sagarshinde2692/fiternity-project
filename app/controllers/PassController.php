6e71a07ea0a57efd9f2e7d3e6f9c4d94426e5419
fitdhruv <dhruvsarawagi@fitternity.com>
2019-07-05 20:40:41 +0530

    passSuccess fix for payu

--- app/controllers/PassController.php
+++ app/controllers/PassController.php
@@ -63,9 +63,6 @@ class PassController extends \BaseController {
 
         $rules = [
             'order_id'=>'required | integer',
-            'razorpay.razorpay_subscription_id'=>'required | string',
-            'razorpay.razorpay_signature'=>'required | string',
-            'razorpay.razorpay_payment_id'=>'required | string'
         ];
         
         $validator = Validator::make($data,$rules);
@@ -74,9 +71,26 @@ class PassController extends \BaseController {
             return Response::json(array('status' => 404,'message' => error_message($validator->errors())), 400);
         }
 
-        $data['razorpay']['key'] = Config::get('app.razorPaySecret');
-        $data['razorpay']['rp_body'] = $data['razorpay']['razorpay_payment_id'].'|'.$data['razorpay']['razorpay_subscription_id'];
-        $data['payment_id'] = $data['razorpay']['razorpay_payment_id'];
+        if(!empty($data['razorpay'])){
+            
+            $rules = [
+                'order_id'=>'required | integer',
+                'razorpay.razorpay_subscription_id'=>'required | string',
+                'razorpay.razorpay_signature'=>'required | string',
+                'razorpay.razorpay_payment_id'=>'required | string'
+            ];
+            
+            $validator = Validator::make($data,$rules);
+    
+            if ($validator->fails()) {
+                return Response::json(array('status' => 404,'message' => error_message($validator->errors())), 400);
+            }
+            
+            $data['razorpay']['key'] = Config::get('app.razorPaySecret');
+            $data['razorpay']['rp_body'] = $data['razorpay']['razorpay_payment_id'].'|'.$data['razorpay']['razorpay_subscription_id'];
+            $data['payment_id'] = $data['razorpay']['razorpay_payment_id'];
+        
+        }
 
         return $this->passService->passSuccess($data);
 
