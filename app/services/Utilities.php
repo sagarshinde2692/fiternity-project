eb5715d23bb90ba5b748237de4844b112b415c75
kailashbajya <kailashbajya@fitternity.com>
2019-07-12 19:06:54 +0530

    added log for wallet query

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -1990,7 +1990,7 @@ Class Utilities {
             //Log::info("query ::            ", [$query]);
 
             $allWallets  = $query->OrderBy('restricted','desc')->OrderBy('_id','asc')->get();
-
+            Log::info('allwaters::::::::::', [$allWallets]);
             Log::info('wallet ::             ',[count($allWallets)]);
 
             if(count($allWallets) > 0){
