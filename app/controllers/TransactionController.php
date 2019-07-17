5f519be4b9c669436c9311bb86064106b0f4e3ae
fitdhruv <dhruvsarawagi@fitternity.com>
2019-07-17 19:16:20 +0530

    app transaction summary for cashback coupon

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -6121,13 +6121,13 @@ class TransactionController extends \BaseController {
                 
             }
 
-            if(isset($data['coupon_discount_amount']) && $data['coupon_discount_amount'] > 0){
+            if((isset($data['coupon_discount_amount']) && $data['coupon_discount_amount'] > 0) || (!empty($data['coupon_flags']['cashback_100_per']))){
 
                 if($payment_mode_type != 'pay_later'){
 
                     $amount_summary[] = array(
                         'field' => 'Coupon Discount',
-                        'value' => '-Rs. '.$data['coupon_discount_amount']
+                        'value' => !empty($data['coupon_discount_amount']) ? '-Rs. '.$data['coupon_discount_amount'] : "100% Cashback"
                     );
                     $you_save += $data['coupon_discount_amount'];
                 }else{
