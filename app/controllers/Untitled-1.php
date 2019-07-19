ae09362c1724d5e8140143469663bc937521edea
kailashbajya <kailashbajya@fitternity.com>
2019-07-19 22:00:39 +0530

    removed untitled file

--- app/controllers/Untitled-1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$message = "Hi ".ucwords($customer_name).",Congratulations your All Access $type is now activated. Enjoy the unlimited access to book sessions in all gyms and studios across India. Download Fitternity App and start booking ".Config::get('app.download_app_link').". Valid for $duration. In case of quick assistance call ".Config::get('app.contact_us_customer_number').". Book Now";
-
-echo $message;
-
-
-?>
