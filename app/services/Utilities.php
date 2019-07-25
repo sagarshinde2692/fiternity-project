0cf89d1f2fbab369399f962f554d6c1dfeea919b
fitdhruv <dhruvsarawagi@fitternity.com>
2019-07-25 18:04:19 +0530

    checkin:commented different fitsquad block

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -6523,11 +6523,11 @@ Class Utilities {
 				return ['status'=>400, 'message'=>'Customer not registered'];
             }
             
-            if(!empty($customer->loyalty['brand_loyalty']) || (!empty($customer->loyalty['reward_type']) && $customer->loyalty['reward_type'] > 2 )){
-                if(!empty($data['finder_id']) && !empty($customer->loyalty['finder_id']) && $customer->loyalty['finder_id'] != $data['finder_id'] ){
-                    return ['status'=>400, 'message'=>'Since you are registered with Fitsquad program of another Gym / Studio, this check-in is not valid'];
-                }
-            }
+            // if(!empty($customer->loyalty['brand_loyalty']) || (!empty($customer->loyalty['reward_type']) && $customer->loyalty['reward_type'] > 2 )){
+            //     if(!empty($data['finder_id']) && !empty($customer->loyalty['finder_id']) && $customer->loyalty['finder_id'] != $data['finder_id'] ){
+            //         return ['status'=>400, 'message'=>'Since you are registered with Fitsquad program of another Gym / Studio, this check-in is not valid'];
+            //     }
+            // }
             
 			$checkin = new \Checkin();
 			$checkin->finder_id = $data['finder_id'];
