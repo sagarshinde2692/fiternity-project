59548d273b318da79d217f3485013dc98b54f82c
Akhil Kulkarni <akhilkulkarni@fitternity.com>
2019-07-31 22:30:37 +0530

    non reliance test id change

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -3977,7 +3977,7 @@ class CustomerController extends \BaseController {
 					$result['health'] = $this->relianceService->buildHealthObject($customer_id, $corporate_id, $this->device_type, $city);
 					$result['is_health_rewad_shown'] = true;
 				}
-				else if(!empty($customer_id) && !empty($customeremail) && $customeremail=='sailismart@fitternity.com'){
+				else if(!empty($customer_id) && !empty($customeremail) && $customeremail=='ss@test.com'){
 					$customerRec = Customer::active()->where('email', $customeremail)->first();
 					$result['non_reliance'] = Config::get('health_config.non_reliance');
 					$result['health'] = $this->relianceService->buildHealthObject($customer_id, $corporate_id, $this->device_type, $city);
