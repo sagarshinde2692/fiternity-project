ab3f33e7a09bfa0354e8657d39c4afab8eaacf2f
kailashbajya <kailashbajya@fitternity.com>
2019-08-07 22:08:37 +0530

    updated footer_txt

--- app/config/mfp_success.php
+++ app/config/mfp_success.php
@@ -3,7 +3,7 @@
 return [
     'cover_image' => 'https://b.fitn.in/global/mfp/slider_01.jpg',
     'top_txt' =>'Congratulations your <b>JOCKEY MORNING FITNESS PARTY</b> passes have been booked.',
-    'footer_txt' =>'Please remember to carry your yoga mat and water bottle along with you.</br>Wear your workout outfit to enjoy the most.'
+    'footer_txt' =>'Please remember to carry your yoga mat and water bottle along with you. Wear your workout outfit to enjoy the most.'
 ];
 
 ?>
