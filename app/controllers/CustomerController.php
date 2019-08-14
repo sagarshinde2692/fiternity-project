ba898cf510b2ab55264800cc76c741c677dee3a5
Akhil Kulkarni <akhilkulkarni@fitternity.com>
2019-08-14 18:40:06 +0530

    reliance hardcoded id removed

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -8476,7 +8476,6 @@ class CustomerController extends \BaseController {
 		$isReward = !empty($input['isReward']) && ($input['isReward']!=false && $input['isReward']!="false");
 
 		$customer = $this->utilities->getCustomerFromTokenAsObject();
-		$customer->_id = 99901;
         if(!empty($customer->_id)){
 			if(isset($customer->corporate_id)){
 				$customer = Customer::active()->where('_id', $customer->_id)->where('corporate_id', 1)->first();	
