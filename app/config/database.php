04b8d493d9d2c2d071f9c812aee5cd8ed6eeaeb5
ankitamamnia <ankitamamnia@fitternity.com>
2019-08-19 17:40:03 +0530

    live config

--- app/config/database.php
+++ app/config/database.php
@@ -6,16 +6,16 @@
 // $password = "";
 
 //stage
-// $host = "apistage.fitn.in";
-// $username = "admin";
-// $password = "fit123";
-// $options = ['db' => 'admin','authMechanism' => 'MONGODB-CR']; // sets the authentication database required by mongo 3]
+$host = "apistage.fitn.in";
+$username = "admin";
+$password = "fit123";
+$options = ['db' => 'admin','authMechanism' => 'MONGODB-CR']; // sets the authentication database required by mongo 3]
 
 //production
 
-$host = "54.179.134.14"; 
-$username = ""; 
-$password = ""; 
+// $host = "54.179.134.14"; 
+// $username = ""; 
+// $password = ""; 
 
 return array(
 
@@ -71,7 +71,7 @@ return array(
 			'database' => 'fitadmin', 
 		    'username' => $username, 
 		    'password' => $password, 
-			'options' => $options
+			// 'options' => $options
 			// 'options' => [
 			// 		'db' => 'admin' // sets the authentication database required by mongo 3
 			// 	]
@@ -84,7 +84,7 @@ return array(
 			'database' => 'fitapi', 
 			'username' => $username, 
 			'password' => $password, 
-			'options' => $options
+			// 'options' => $options
 			// 'options' => [
 			// 	'db' => 'admin' // sets the authentication database required by mongo 3
 			// ]
@@ -97,7 +97,7 @@ return array(
 			'database' => 'fitcheckins', 
 			'username' => $username, 
 			'password' => $password, 
-			'options' => $options
+			// 'options' => $options
 			// 'options' => [
 			// 	'db' => 'admin' // sets the authentication database required by mongo 3
 			// ]
