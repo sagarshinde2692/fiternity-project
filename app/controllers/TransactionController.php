c7ff15e3b38d1012ebd38b470cb7e10999aac586
ankitamamnia <ankitamamnia@fitternity.com>
2019-08-28 11:29:23 +0530

    removed quantity, payment_details and payment_mode

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -1530,14 +1530,11 @@ class TransactionController extends \BaseController {
             }
             $payment_details = [];
 
-            // $onepassHoldCustomer = $this->utilities->onepassHoldCustomer();
-            // if(!(!empty($onepassHoldCustomer) && $onepassHoldCustomer && !empty($data['type']) && $data['type'] == 'workout-session' && $data['amount_customer'] < 1001)){
-                foreach ($payment_mode_type_array as $payment_mode_type) {
+            foreach ($payment_mode_type_array as $payment_mode_type) {
 
-                    $payment_details[$payment_mode_type] = $this->getPaymentDetails($order->toArray(),$payment_mode_type);
+                $payment_details[$payment_mode_type] = $this->getPaymentDetails($order->toArray(),$payment_mode_type);
     
-                }
-            // }
+            }
             
             $resp['data']['payment_details'] = $payment_details;
 
@@ -1693,6 +1690,13 @@ class TransactionController extends \BaseController {
         // if(!empty($data['studio_extended_validity']) && $data['studio_extended_validity']) {
         //     $this->utilities->scheduleStudioBookings(null, $order_id);
         // }
+
+        $onepassHoldCustomer = $this->utilities->onepassHoldCustomer();
+        if(!empty($onepassHoldCustomer) && $onepassHoldCustomer && $data['amount_customer'] < 1001 && !empty($data['type']) && $data['type'] == 'workout-session'){
+            unset($resp['data']['quantity_details']);
+            unset($resp['data']['payment_details']);
+            unset($resp['data']['payment_modes']);
+        }
         Log::info("capture response");
         Log::info($resp);
         return $resp;
