f2e2234dad3e86eabe5c3e0a2526db7b804d0b73
Akhil Kulkarni <akhilkulkarni@fitternity.com>
2019-09-03 17:55:34 +0530

    onepass pass token data fix

--- app/helpers.php
+++ app/helpers.php
@@ -4497,11 +4497,11 @@ if (!function_exists('setNewToken')) {
         Log::info(gettype($customer_data));
         Log::info('gettype($customer_data)');
         $pass_data = [];
-        if(!empty($pass) && empty($customer_data['pass'])){
+        if(!empty($pass)){
             $pass_data = ['pass'=>1, 'pass_start_date' => (!empty($pass['start_date']))?strtotime($pass['start_date']):null, 'pass_expiry_date' => (!empty($pass['end_date']))?strtotime($pass['end_date']):null, 'pass_type' => $pass['pass']['pass_type'], 'pass_sessions_total'=>$pass['onepass_sessions_total'], 'pass_sessions_used'=>$pass['onepass_sessions_used']];
             $customer_data = array_merge($customer_data, $pass_data);
             $update_header = true;
-        }else if(empty($pass) && !empty($customer_data['pass'])){
+        }else if(empty($pass)){
             unset($customer_data['pass']);
             unset($customer_data['pass_start_date']);
             unset($customer_data['pass_expiry_date']);
