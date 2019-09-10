6daa723ebe3d4b64628a01e2648ea9027fd3349f
fitdhruv <dhruvsarawagi@fitternity.com>
2019-09-10 13:51:20 +0530

    pss text

--- app/config/pass.php
+++ app/config/pass.php
@@ -94,7 +94,7 @@ return [
                     'ratecards' => []
                 ],
                 'remarks' => [
-                    'header' => "In addition to owning the coolest fitness membership, OnePass users get exclusive rewards, vouchers and more ! \nLimited Period Offer, Only Few Passes For Grabs.", // need content
+                    'header' => "In addition to owning the coolest fitness membership, OnePass users get exclusive rewards, vouchers and more ! \nEarly bird offer ends on 15th September. Buy your OnePass before prices go up!", // need content
                     'text' => 'Terms and Conditions',
                     "title" =>'Terms and Conditions',
                     'url' => $apiUrl.'/passtermscondition?type=unlimited'
@@ -128,7 +128,7 @@ return [
                     'ratecards' => []
                 ],
                 'remarks' => [
-                    'header' => "In addition to owning the coolest fitness membership, OnePass users get exclusive rewards, vouchers and more ! \nLimited Period Offer, Only Few Passes For Grabs.", // need content
+                    'header' => "In addition to owning the coolest fitness membership, OnePass users get exclusive rewards, vouchers and more ! \nEarly bird offer ends on 15th September. Buy your OnePass before prices go up!", // need content
                     'text' => 'Terms and Conditions',
                     "title" =>'Terms and Conditions',
                     'url' => $apiUrl.'/passtermscondition?type=subscribe'
@@ -426,7 +426,7 @@ return [
             ],
             "footer" => [
                 "index" => 0,
-                "text" => "Try the complementary pass @2500 & get 50% instant cashback",
+                "text" => "Early bird offer ends on 15th September. Buy your OnePass before prices go up!",
                 "button_text" => "Know More"
             ]
         ],
