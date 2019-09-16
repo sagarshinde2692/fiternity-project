d31be7bd5a299a6485f977f392451f1a8f893c98
kailashbajya <kailashbajya@fitternity.com>
2019-09-16 16:30:03 +0530

    added pass city id and pass city name in token

--- app/helpers.php
+++ app/helpers.php
@@ -2689,6 +2689,8 @@ if (!function_exists('get_elastic_service_sale_ratecards')) {
                             $data['pass_type']=$passOrder['pass']['pass_type'];
                             $data['pass_sessions_total']=$passOrder['onepass_sessions_total'];
                             $data['pass_sessions_used']=(!!$passOrder['onepass_sessions_used'])?$passOrder['onepass_sessions_used']:0;
+                            $data['pass_city_id'] = !empty($passOrder['pass_city_id']) ? $passOrder['pass_city_id'] : null;
+                            $data['pass_city_name'] = !empty($passOrder['pass_city_name']) ? $passOrder['pass_city_name'] : null;
                         }
                     }
 
@@ -4503,7 +4505,7 @@ if (!function_exists('setNewToken')) {
         Log::info('gettype($customer_data)');
         $pass_data = [];
         if(!empty($pass)){
-            $pass_data = ['pass'=>1, 'pass_start_date' => (!empty($pass['start_date']))?strtotime($pass['start_date']):null, 'pass_expiry_date' => (!empty($pass['end_date']))?strtotime($pass['end_date']):null, 'pass_type' => $pass['pass']['pass_type'], 'pass_sessions_total'=>$pass['onepass_sessions_total'], 'pass_sessions_used'=>$pass['onepass_sessions_used']];
+            $pass_data = ['pass'=>1, 'pass_start_date' => (!empty($pass['start_date']))?strtotime($pass['start_date']):null, 'pass_expiry_date' => (!empty($pass['end_date']))?strtotime($pass['end_date']):null, 'pass_type' => $pass['pass']['pass_type'], 'pass_sessions_total'=>$pass['onepass_sessions_total'], 'pass_sessions_used'=>$pass['onepass_sessions_used'], 'pass_city_id' => (!empty($pass['pass_city_id']) ? $pass['pass_city_id'] : null), 'pass_city_name' => (!empty($pass['pass_city_name']) ? $pass['pass_city_name'] : null)];
             $customer_data = array_merge($customer_data, $pass_data);
             $update_header = true;
         }else if(empty($pass)){
@@ -4513,6 +4515,8 @@ if (!function_exists('setNewToken')) {
             unset($customer_data['pass_type']);
             unset($customer_data['pass_sessions_total']);
             unset($customer_data['pass_sessions_used']);
+            unset($customer_data['pass_city_id']);
+            unset($customer_data['pass_city_name']);
             $update_header = true;
         }
         if(!empty($update_header) || $rel_banner_shown){
