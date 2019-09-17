3b9ed5d40836fe0b8315db2923fb59dfa36d8ef5
kailashbajya <kailashbajya@fitternity.com>
2019-09-17 12:28:03 +0530

    removed duplicate tnc

--- app/config/pass.php
+++ app/config/pass.php
@@ -173,8 +173,6 @@ return [
                 <li>In case the user is found misusing any of the OnePass services, he/she will be penalized at the discretion of Fitternity.</li>
                 <li>In case of any injury or accident at the Fitness Center, any claims on Fitternity would not be applicable whatsoever.</li>
                 <li>In case of any dispute, the final decision made by Fitternity would be final.</li>
-                <li>F45 Training in Juhu  - 8 sessions per month.</li>
-                <li>F45 Training in Bandra One BKC - 8 sessions per month.</li>
                 <li>In case of any assistance concerning OnePass write to us at onepass@fitternity.com or call your personal concierge at +917400062849</li>
             </ul><br/><br/>",
         ],
@@ -204,8 +202,6 @@ return [
                 <li>In case the user is found misusing any of the OnePass services, he/she will be penalized at the discretion of Fitternity.</li>
                 <li>In case of any injury or accident at the Fitness Center, any claims on Fitternity would not be applicable whatsoever.</li>
                 <li>In case of any dispute the final decision made by Fitternity would be final.</li>
-                <li>F45 Training in Juhu  - 8 sessions per month.</li>
-                <li>F45 Training in Bandra One BKC - 8 sessions per month.</li>
                 <li>In case of any assistance concerning OnePass write to us at onepass@fitternity.com or call your personal concierge at +917400062849.</li>
             </ul><br/><br/>"
         ],
@@ -238,8 +234,6 @@ return [
                 <li>In case the user is found misusing any of the OnePass services, he/she will be penalized at the discretion of Fitternity.</li>
                 <li>In case of any injury or accident at the Fitness Center, any claims on Fitternity would not be applicable whatsoever.</li>
                 <li>In case of any dispute, the final decision made by Fitternity would be final.</li>
-                <li>F45 Training in Juhu  - 8 sessions per month.</li>
-                <li>F45 Training in Bandra One BKC - 8 sessions per month.</li>
                 <li>In case of any assistance concerning OnePass write to us at onepass@fitternity.com or call your personal concierge at +917400062849</li>
             </ul><br/><br/>",
         ]
