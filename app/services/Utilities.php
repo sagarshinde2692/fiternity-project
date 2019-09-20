9fa770ee83207a3c6c3b90940d8b12731ca56514
kailashbajya <kailashbajya@fitternity.com>
2019-09-20 18:50:38 +0530

    adding notification to fitsquad checkout time

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -9573,10 +9573,10 @@ Class Utilities {
 				//session is not complitated
 				$return = $this->checkinCheckoutSuccessMsg($finder, $customer);
 				$return['header'] = "Session is not completed.";
-				$return['sub_header_2'] = "Seems you have not completed your workout at ".$finder['title'].". The check-out time window is 45 minutes to 2 hours from your check-in time.\n Please make sure you check-out in the same window in order to get a successful check-in to level up on your workout milestone.";
+				$return['sub_header_2'] = "Seems you have not completed your workout at ".$finder['title'].". The check-out time window is 45 minutes to 3 hours from your check-in time.\n Please make sure you check-out in the same window in order to get a successful check-in to level up on your workout milestone.";
 				return $return;
 			}
-			else if(($difference > 45 * 60) &&($difference <= 120 * 60))
+			else if(($difference > 45 * 60) &&($difference <= 180 * 60))
 			{
                 //checking out ----
                 if(!empty($source) && $source=='markcheckin'){
@@ -9584,15 +9584,15 @@ Class Utilities {
                 }
                 return null;
 			}
-			else if(($difference > 120 * 60) && ($difference < 180 * 60))
+			else if(($difference > 180 * 60) && ($difference < 240 * 60))
 			{
 				//times up not accaptable
 				$return  = $this->checkinCheckoutSuccessMsg($finder, $customer);
 				$return['header'] = "Times Up to checkout for the day.";
-				$return['sub_header_2'] = "Sorry you have lapsed the check-out time window for the day. (45 minutes to 2 hours from your check-in time) . \nThis check-in will not be marked into your profile.\n Continue with your workouts and achieve the milestones.";
+				$return['sub_header_2'] = "Sorry you have lapsed the check-out time window for the day. (45 minutes to 3 hours from your check-in time) . \nThis check-in will not be marked into your profile.\n Continue with your workouts and achieve the milestones.";
 				return $return;
 			}
-			else if($difference >= 180 * 60){
+			else if($difference >= 240 * 60){
                 return $this->checkinInitiate($finder_id, $finder, $customer_id, $customer, $data);
 			}
 		}
@@ -9765,7 +9765,7 @@ Class Utilities {
 					"status" => false
 				);
 			}
-			else if($difference < 180 * 60){
+			else if($difference < 240 * 60){
 				return  array(
 					'status' => true,
 					'logo' => Config::get('loyalty_constants.fitsquad_logo'),
@@ -9863,7 +9863,8 @@ Class Utilities {
 			// }
 			$resp = $this->checkinCheckoutSuccessMsg($finder, $customer);
 			$resp['header'] = 'CHECK- IN SUCCESSFUL';
-            $resp['sub_header_2'] = "Enjoy your workout at ".$finder['title']."\n Make sure you check-out post your workout by scanning the QR code again to get the successful check-in towards the goal of reaching your milestone. \n\n Please note - The check-in will not be provided if your check-out time is not mapped out. Don`t forget to scan the QR code again post your workout.";
+            // $resp['sub_header_2'] = "Enjoy your workout at ".$finder['title']."\n Make sure you check-out post your workout by scanning the QR code again to get the successful check-in towards the goal of reaching your milestone. \n\n Please note - The check-in will not be provided if your check-out time is not mapped out. Don`t forget to scan the QR code again post your workout.";
+            $resp['sub_header_2'] = "Enjoy your workout at ".$finder['title']." ".$finder['location_id']['name'].". Workout for at-least 45 minutes before check-out.\nMake sure you check-out by scanning the QR code again to get the successful check-in.";
             $resp['checkin'] = (!empty($addedCheckin['checkin'])? $addedCheckin['checkin']: null);
 			return $resp;
 		}else{	
