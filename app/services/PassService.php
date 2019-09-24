2a86d06481b1a5d2d6228df8fb99a5575bb254b7
kailashbajya <kailashbajya@fitternity.com>
2019-09-24 15:31:34 +0530

    updated check for brands  hybrid pass in allow

--- app/services/PassService.php
+++ app/services/PassService.php
@@ -693,7 +693,7 @@ class PassService {
                 }
                 else if($passOrder['pass']['pass_type']=='hybrid') {
                     // $duration = $passOrder['pass']['duration'];
-                    if(empty($finder) || in_array($finder['brand_id'], array_column($passOrder['pass']['brands'], '_id'))){
+                    if(empty($finder) || !in_array($finder['brand_id'], array_column($passOrder['pass']['brands'], '_id'))){
                         return [ 'allow_session' => false, 'order_id' => $passOrder['_id'], 'pass_type'=>$passType, "msg"=> "Not Applicable on ".$finder['title']];
                     }
 
