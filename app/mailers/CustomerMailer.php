4180f9b5e258e0ec2e240534a2647a709186295b
Akhil Kulkarni <akhilkulkarni@fitternity.com>
2019-09-26 20:31:03 +0530

    fix

--- app/mailers/CustomerMailer.php
+++ app/mailers/CustomerMailer.php
@@ -182,8 +182,8 @@ Class CustomerMailer extends Mailer {
 		}
 		
 		if(!empty($data['type']) && ($data['type']=='pass')){
-			if($data['pass_type'] =='hybrid'){
-				$data['pass_type'] = $data['pass']['branding'];
+			if($data['pass']['pass_type'] =='hybrid'){
+				$data['pass']['pass_type'] = $data['pass']['branding'];
 				if(empty($data['ratecard_flags']['onepass_attachment_type']) || in_array($data['ratecard_flags']['onepass_attachment_type'], ['complementary', 'membership_plus'])){
 					return;
 				}
