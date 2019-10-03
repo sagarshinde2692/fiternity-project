103f556867616546ad71b2b1ee4344f4d0ad6c5b
fitdhruv <dhruvsarawagi@fitternity.com>
2019-10-03 20:03:42 +0530

    re removed authentication on routes

--- app/routes.php
+++ app/routes.php
@@ -297,14 +297,14 @@ Route::group(array('before' => 'validatetoken'), function() {
     
     Route::post('passcapture', 'PassController@passCapture');
 
-    Route::get('booktrialdetail/{captureid}/{type?}', 'SchedulebooktrialsController@booktrialdetail');
-    
-    Route::get('orderdetail/{orderid}',  array('as' => 'orders.orderdetail','uses' => 'OrderController@getOrderDetail'));
-    
-    Route::get('/successmsg/{type}/{id}', 'HomeController@getSuccessMsg');
     
 });
 
+Route::get('booktrialdetail/{captureid}/{type?}', 'SchedulebooktrialsController@booktrialdetail');
+
+Route::get('orderdetail/{orderid}',  array('as' => 'orders.orderdetail','uses' => 'OrderController@getOrderDetail'));
+
+Route::get('/successmsg/{type}/{id}', 'HomeController@getSuccessMsg');
 
 Route::post('walletTransactionnew', array('uses' => 'OrderController@debitWalletTransaction'));
 /******************** CUSTOMERS SECTION END HERE ********************/
