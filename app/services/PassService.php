6eaea2912232cf35cb2f44674c8ca78201ab50ef
kailashbajya <kailashbajya@fitternity.com>
2019-10-07 13:20:55 +0530

    updated

--- app/services/PassService.php
+++ app/services/PassService.php
@@ -2122,7 +2122,7 @@ class PassService {
                 $upcoming['user_photo'] = $customer->onepass['photo']['url'];
             }
             $upcoming['time_diff'] = 0;
-            $upcoming['footer'] =Config('app.before_purchase_tab.footer');
+            $upcoming['footer'] = Config::get('app.before_purchase_tab.footer');
         }
 
         return $upcoming;
