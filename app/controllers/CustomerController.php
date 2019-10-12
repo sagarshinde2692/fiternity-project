1b3e03c34002945d8a04d1eece0610c87eb7dd27
kailashbajya <kailashbajya@fitternity.com>
2019-10-12 19:53:23 +0530

    added unlock notification to letus know

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -7604,35 +7604,38 @@ class CustomerController extends \BaseController {
 					];
 					Booktrial::where('_id', $data['_id'])->update(['kiosk_block_shown'=>true]);
 				}
-				if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31')){
-					$upcoming_booking = $this->passService->upcomingPassBooking(null, $data, $data['customer_id']);
-					Log::info('upcoming booking:::', [$upcoming_booking]);
-					if(!empty($upcoming_booking['footer']['unlock_url'])){
-						$response['unlocktext'] = $upcoming_booking['footer']['text'];
-						$response['button_text']['unlock'] = [
-							'text' => $upcoming_booking['footer']['unlock_text'],
-							'url' => $upcoming_booking['footer']['unlock_url']."?from=notification_before10min",
-						];
 
-						$response['sub_header'] = $upcoming_booking['footer']['unlock_text'];
-						$response['footer'] = "need to ask vipu/saili";
-					}
-					else {
-						$response['sub_header'] = $upcoming_booking['header'];
-						$response['footer'] = "need to ask vipu/saili";
-						unset($response['button_text']);
-						$response['block'] = false;
-					}
+				// if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31')){
+				// 	$upcoming_booking = $this->passService->upcomingPassBooking(null, $data, $data['customer_id']);
+				// 	Log::info('upcoming booking:::', [$upcoming_booking]);
+				// 	if(!empty($upcoming_booking['footer']['unlock_url'])){
+				// 		$response['unlocktext'] = $upcoming_booking['footer']['text'];
+				// 		$response['button_text']['unlock'] = [
+				// 			'text' => $upcoming_booking['footer']['unlock_text'],
+				// 			'url' => $upcoming_booking['footer']['unlock_url']."?from=notification_before10min",
+				// 		];
+
+				// 		$response['sub_header'] = $upcoming_booking['footer']['unlock_text'];
+				// 		$response['footer'] = "need to ask vipu/saili";
+				// 	}
+				// 	else {
+				// 		$response['sub_header'] = $upcoming_booking['header'];
+				// 		$response['footer'] = "need to ask vipu/saili";
+				// 		unset($response['button_text']);
+				// 		$response['block'] = false;
+				// 	}
 	
-					unset($response['activation_success']);
-					unset($response['button_text']['activate']);
-					unset($response['button_text']['activate']['cancel_text']);
-					unset($response['button_text']['activate']['cancel']);
-					unset($response['button_text']['activate']['cancel']);
-					unset($response['button_text']['qrcode']);
-					unset($response['button_text']['didnt_get']);
-					unset($response['subscription_code']);//$upcoming_booking['header'];
-				}
+				// 	unset($response['activation_success']);
+				// 	unset($response['button_text']['activate']);
+				// 	unset($response['button_text']['activate']['cancel_text']);
+				// 	unset($response['button_text']['activate']['cancel']);
+				// 	unset($response['button_text']['activate']['cancel']);
+				// 	unset($response['button_text']['qrcode']);
+				// 	unset($response['button_text']['didnt_get']);
+				// 	unset($response['subscription_code']);//$upcoming_booking['header'];
+				// }
+
+				$this->formatNotificationDataOfUnlock($response, $data, 'notification_before10min');
 				break;
 			case 'let_us_know':
 			case 'n+2':
@@ -7695,6 +7698,7 @@ class CustomerController extends \BaseController {
 				}
 				
 				$response['block'] = true;
+				$this->formatNotificationDataOfUnlock($response, $data, 'notification_after2hours');
 				break;
 			case 'n-3':
             case 'session_reminder':
@@ -11044,5 +11048,37 @@ class CustomerController extends \BaseController {
 
 		$data_prepa = array_merge($activated_session, $not_activated_session);
 		return $data_prepa;
+	}
+
+	public function formatNotificationDataOfUnlock(&$response, &$data, $from){
+		if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31')){
+			$upcoming_booking = $this->passService->upcomingPassBooking(null, $data, $data['customer_id']);
+			Log::info('upcoming booking:::', [$upcoming_booking]);
+			if(!empty($upcoming_booking['footer']['unlock_url'])){
+				$response['unlocktext'] = $upcoming_booking['footer']['text'];
+				$response['button_text']['unlock'] = [
+					'text' => $upcoming_booking['footer']['unlock_text'],
+					'url' => $upcoming_booking['footer']['unlock_url']."?from=".$from,
+				];
+
+				$response['sub_header'] = $upcoming_booking['footer']['unlock_text'];
+				$response['footer'] = "need to ask vipu/saili";
+			}
+			else {
+				$response['sub_header'] = $upcoming_booking['header'];
+				$response['footer'] = "need to ask vipu/saili";
+				unset($response['button_text']);
+				$response['block'] = false;
+			}
+
+			unset($response['activation_success']);
+			unset($response['button_text']['activate']);
+			unset($response['button_text']['activate']['cancel_text']);
+			unset($response['button_text']['activate']['cancel']);
+			unset($response['button_text']['activate']['cancel']);
+			unset($response['button_text']['qrcode']);
+			unset($response['button_text']['didnt_get']);
+			unset($response['subscription_code']);//$upcoming_booking['header'];
+		}
 	}
 }
