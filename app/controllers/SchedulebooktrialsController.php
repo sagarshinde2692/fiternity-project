48663f32b6072feb9779e1a27af8256dd05f85ea
kailashbajya <kailashbajya@fitternity.com>
2019-10-17 19:01:09 +0530

    added vendor code check at notificateion

--- app/controllers/SchedulebooktrialsController.php
+++ app/controllers/SchedulebooktrialsController.php
@@ -8020,7 +8020,8 @@ class SchedulebooktrialsController extends \BaseController {
                     $response['sub_header_2'] = "will be added into your steps counter post verifying your attendance from gym/studio.";    
                 }
 
-                if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31')){
+                //if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31'))
+                if(empty($booktrial['vendor_code'])){
                     // $response['sub_header_1'] = "Awesome !\n";
                     if(empty($booktrial['pass_order_id']) || !empty($booktrial['corporate_id'])) {
                         $response['sub_header_2'] .= "\nAwesome !\nWe are glad you enjoyed your workout.";
@@ -8081,7 +8082,8 @@ class SchedulebooktrialsController extends \BaseController {
                     $response['sub_header_2'] = "Sorry, cancellation is available only ".$cancel_preior_time." prior to your session time.\n\nKeep booking workouts to get closer to your steps milestone.";
                 }
 
-                if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31')){
+                //if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31'))
+                if(empty($booktrial['vendor_Code'])){
                     $response['sub_header_2'] = "Don't worry! Rebook the session and continue with your workout journey.";
                 }
 
@@ -8121,7 +8123,8 @@ class SchedulebooktrialsController extends \BaseController {
                     $response['sub_header_2'] = "We'll cancel you from this batch. Do you want to reschedule instead?";
                 }
 
-                if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31')){
+                //if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31'))
+                if(empty($booktrial['vendor_code'])){
                     $response['sub_header_2'] ="Don't worry! You can rebook the session for later and continue with your fitness journey.";
                 }
                 if(!empty($booktrial->pass_order_id)){
@@ -8175,7 +8178,8 @@ class SchedulebooktrialsController extends \BaseController {
                     $response['activate']['sub_header_2'] = 'Punch your subscription code on the kiosk/tab available at the center to activate your session';
                 }
 
-                if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31')){
+                //if(($this->device_type =='ios' && $this->app_version >= '5.2.4') || ($this->device_type =='android' && $this->app_version >= '5.31'))
+                if(empty($booktrial['vendor_code'])){
                     $response['activate'] = [
                         'sub_header_1' =>'UNLOCK YOUR SESSION',
                         'sub_header_2' =>'Navigate to upcoming session ticker on your homescreen to unlock this session at '.$booktrial['finder_name']
