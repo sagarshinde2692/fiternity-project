3e84bb7ce082043b4b2a81832072fac5203edc01
kailashbajya <kailashbajya@fitternity.com>
2019-10-17 16:05:06 +0530

    removed upcoming where upcoming_new is present

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -3979,6 +3979,7 @@ class CustomerController extends \BaseController {
 					}
 
 					if(!empty($this->device_type) && !empty($this->app_version) && ($this->device_type =='android' && $this->app_version >= '5.31') || ($this->device_type =='ios' && $this->app_version >= '5.2.4')){
+						$upcoming= [];
 						$unlocked = [];
 						foreach($upcoming_new as $x_new){
 
