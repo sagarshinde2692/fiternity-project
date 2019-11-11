78de1a0bebca4055d0f749753c1ff1feae367a6c
kailashbajya <kailashbajya@fitternity.com>
2019-11-11 13:51:57 +0530

    updated fitsquad image for fitternity grif

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -7884,7 +7884,8 @@ class FindersController extends \BaseController {
 					'text'=>"Checkout Rewards",
 					// "image"=>'https://b.fitn.in/global/cashback/rewards/fitternity-new-rewards-all-cities.jpg'
 					//'image' => 'https://b.fitn.in/global/Homepage-branding-2018/srp/Edited%20Fitsquad%20Grid%20%281%29.jpg'
-					'image'=> 'https://b.fitn.in/global/fitsquad-225.jpg'
+					// 'image'=> 'https://b.fitn.in/global/fitsquad-225.jpg'
+					"image"=> "https://b.fitn.in/global/firsquad_grid_new.png"
 				];
 
 				$data['checkout_summary'] = [
@@ -7896,7 +7897,8 @@ class FindersController extends \BaseController {
 						'text' => 'Checkout Rewards',
 						// 'image' => 'https://b.fitn.in/global/cashback/rewards/fitternity-new-rewards-all-cities.jpg'
 						//'image' => 'https://b.fitn.in/global/Homepage-branding-2018/srp/Edited%20Fitsquad%20Grid%20%281%29.jpg'
-						'image' => 'https://b.fitn.in/global/fitsquad-225.jpg'
+						// 'image' => 'https://b.fitn.in/global/fitsquad-225.jpg'
+						"image"=> "https://b.fitn.in/global/firsquad_grid_new.png"
 					],
 					'know_more' => true
 				];
