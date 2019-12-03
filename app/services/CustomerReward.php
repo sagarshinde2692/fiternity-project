13e97442c4abc2f0c030aa667661d824b46aa19a
kailashbajya <kailashbajya@fitternity.com>
2019-12-03 12:14:06 +0530

    removed rmz apply fitcash check for finder specific

--- app/services/CustomerReward.php
+++ app/services/CustomerReward.php
@@ -1209,7 +1209,9 @@ Class CustomerReward {
 
             $query = $utilities->getWalletQuery($request);
 
-            $current_wallet_balance = $query->sum('balance');/*
+            $current_wallet_balance = $query->sum('balance');
+            Log::info('current wallt balanvr :::::::', [$current_wallet_balance]);
+            /*
 
             if(isset($customer->demonetisation)){
 
--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -2723,15 +2723,8 @@ Class Utilities {
     
                     }
                 }
-
                 
-                if(!empty($request['order_type']) && ($request['order_type'] != "workout-session" || $request['order_type'] != "workout session") && !empty($request['city_id']) && $request['city_id'] != '3'){
-                    $query->where(function($query) use($finder_id) {$query->orWhere('valid_finder_id','exists',false)->orWhere('valid_finder_id',$finder_id)->orwhere('flags.use_for_self', 'exists', false)->orWhere('flags.use_for_self', false);});
-                }else if(!empty($request['order_type']) && ($request['order_type'] == "workout-session" || $request['order_type'] == "workout session") && !empty($request['city_id']) && $request['city_id'] == '3'){
-                    
-                }else{
-                    $query->where(function($query) use($finder_id) {$query->orWhere('valid_finder_id','exists',false)->orWhere('valid_finder_id',$finder_id);});
-                }
+                $query->where(function($query) use($finder_id) {$query->orWhere('valid_finder_id','exists',false)->orWhere('valid_finder_id',$finder_id);});
     
             }else{
                 
