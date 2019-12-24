d966280f7d4ceb740be62cf454aa5121b5ca38b6
kailashbajya <kailashbajya@fitternity.com>
2019-12-24 13:56:13 +0530

    updated query

--- public/pass_update_script.js
+++ public/pass_update_script.js
@@ -23,85 +23,100 @@
 //     }
 // )
 
-var x = db.passes.updateMany(
+// var x = db.passes.updateMany(
+//     {
+//         pass_type:{
+//             $in :["black","red"]
+//         }, 
+//         status: "1",
+//     }, 
+//     {
+//         $set: {
+//             "max_booking_count": 31,
+//             "vendor_restriction" : 
+//             [ 
+//                 {
+//                     "ids" : [ 
+//                         13660
+//                     ],
+//                     "start_date" : ISODate("2019-11-24T00:02:03.131Z"),
+//                     "type" : "finders",
+//                     "count" : 5,
+//                     "count_type" : "each"
+//                 }, 
+//                 {
+//                     "ids" : [ 
+//                         966
+//                     ],
+//                     "start_date" : ISODate("2019-11-24T00:02:03.131Z"),
+//                     "type" : "finders",
+//                     "count" : 8,
+//                     "count_type" : "each"
+//                 }, 
+//                 {
+//                     "ids" : [ 
+//                         11230
+//                     ],
+//                     "start_date" : ISODate("2019-11-24T00:02:03.131Z"),
+//                     "type" : "finders",
+//                     "count" : 10,
+//                     "count_type" : "each"
+//                 }, 
+//                 {
+//                     "ids" : [ 
+//                         1935, 
+//                         9304, 
+//                         9423, 
+//                         9481, 
+//                         9600, 
+//                         9932, 
+//                         9954, 
+//                         10674, 
+//                         10970, 
+//                         11021, 
+//                         11223, 
+//                         12208, 
+//                         12209, 
+//                         13094, 
+//                         13898, 
+//                         13968, 
+//                         14102, 
+//                         14107, 
+//                         14622, 
+//                         14626, 
+//                         14627, 
+//                         15431, 
+//                         15775, 
+//                         15980, 
+//                         16062, 
+//                         16251, 
+//                         16449, 
+//                         16450, 
+//                         16562, 
+//                         16636, 
+//                         16644, 
+//                         17470
+//                     ],
+//                     "type" : "finders",
+//                     "count" : 12,
+//                     "count_type" : "each"
+//                 }
+//             ]
+//         }
+//     }
+// )
+
+var z = db.passes.updateMany(
     {
         pass_type:{
             $in :["black","red"]
         }, 
-        status: "1",
-    }, 
+        status: "1"
+    },
     {
-        $set: {
-            "max_booking_count": 31,
-            "vendor_restriction" : 
-            [ 
-                {
-                    "ids" : [ 
-                        13660
-                    ],
-                    "start_date" : ISODate("2019-11-24T00:02:03.131Z"),
-                    "type" : "finders",
-                    "count" : 5,
-                    "count_type" : "each"
-                }, 
-                {
-                    "ids" : [ 
-                        966
-                    ],
-                    "start_date" : ISODate("2019-11-24T00:02:03.131Z"),
-                    "type" : "finders",
-                    "count" : 8,
-                    "count_type" : "each"
-                }, 
-                {
-                    "ids" : [ 
-                        11230
-                    ],
-                    "start_date" : ISODate("2019-11-24T00:02:03.131Z"),
-                    "type" : "finders",
-                    "count" : 10,
-                    "count_type" : "each"
-                }, 
-                {
-                    "ids" : [ 
-                        1935, 
-                        9304, 
-                        9423, 
-                        9481, 
-                        9600, 
-                        9932, 
-                        9954, 
-                        10674, 
-                        10970, 
-                        11021, 
-                        11223, 
-                        12208, 
-                        12209, 
-                        13094, 
-                        13898, 
-                        13968, 
-                        14102, 
-                        14107, 
-                        14622, 
-                        14626, 
-                        14627, 
-                        15431, 
-                        15775, 
-                        15980, 
-                        16062, 
-                        16251, 
-                        16449, 
-                        16450, 
-                        16562, 
-                        16636, 
-                        16644, 
-                        17470
-                    ],
-                    "type" : "finders",
-                    "count" : 12,
-                    "count_type" : "each"
-                }
-            ]
+        $unset : {
+            "max_booking_count" : '',
+            "vendor_restriction" : ''
         }
     }
 )
