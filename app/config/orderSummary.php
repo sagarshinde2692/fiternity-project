214367ab0e99b4318ed580054e64b20ef4884696
Laxansh <laxanshadesara@fitternity.com>
2019-12-28 01:34:23 +0530

    header reverted

--- app/config/orderSummary.php
+++ app/config/orderSummary.php
@@ -1,17 +1,17 @@
 <?php
 return array(
     'order_summary'=>[
-       // "header"=> "You Are Currently Booking ratecard_name service_name AT vendor_name",
+        "header"=> "You Are Currently Booking ratecard_name service_name AT vendor_name",
         "title"=>"Things to keep in mind",
         "remark_data" =>"ratecard_remark"     
     ],
     'slot_summary'=>[
-       // "header"=> "You Are Currently Booking service_name Session AT vendor_name",
+        "header"=> "You Are Currently Booking service_name Session AT vendor_name",
         "title"=>"Things to keep in mind",
         "remark_data" =>"ratecard_remark"     
     ],
     'service_summary'=>[
-       // "header"=> "You Are Currently Booking service_name Session AT vendor_name",
+        "header"=> "You Are Currently Booking service_name Session AT vendor_name",
         "title"=>"Things to keep in mind",
         "remark_data" =>"ratecard_remark"     
     ]
