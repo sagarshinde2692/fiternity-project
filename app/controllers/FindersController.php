ba948d64e56ca96434ecd5ffddf527083a6972f3
Akhil Kulkarni <akhil16.4.93@gmail.com>
2019-12-27 18:54:29 +0530

    membership_plus object changes

--- app/controllers/FindersController.php
+++ app/controllers/FindersController.php
@@ -7451,8 +7451,8 @@ class FindersController extends \BaseController {
 			$plusId = $plusRatecard['plus_id'];
 			$plusDuration = $plusRatecard['plus_duration'];
 			$retObj = [
-				'header' => 'By Purchasing This Membership Through Fitternity You Get Exclusive Accesss to '.(!empty($plusDuration))?ucwords($plusDuration):''.' <b>Fitternity Plus Membership</b>',
-				'image' => '',
+				'header' => 'By Purchasing This Membership Through Fitternity You Get Exclusive Accesss to '.((!empty($plusDuration))?ucwords($plusDuration):'').' <b>Fitternity Plus Membership</b>',
+				'image' => 'https://b.fitn.in/global/fitcompressed/onepass-image.png',
 				'description' => 'Fitternity Plus gives you access to exclusive fitness merchandise, great deals on workouts and much more!',
 				'know_more_text' => 'KNOW MORE',
 				'know_more_url' => 'https://www.fitternity.com/membership-plus/'.$plusId,
@@ -7461,7 +7461,7 @@ class FindersController extends \BaseController {
 				'address_required' => true
 			];
 			if($amt>4000) {
-				$retObj['tshirt_sizes'] = [ 'S','M','L','XL'];
+				$retObj['tshirt_sizes'] = [ 'S', 'M', 'L', 'XL' ];
 			}
 			return $retObj;
 		}
