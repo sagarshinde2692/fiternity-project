d7ec94b42637355621d492a1ca36337b32e0318b
kailashbajya <kailashbajya@fitternity.com>
2019-12-28 18:16:04 +0530

    updated

--- app/services/PassService.php
+++ app/services/PassService.php
@@ -2885,8 +2885,14 @@ class PassService {
 
         if($device=='android' && $app_version == '5.33'){
             foreach($coupons as $key=>&$value){
+                $value['code'] = strtoupper($value['code']);
                 $terms = !empty($value['terms']) ?  implode("<br>", $value['terms']) : '';
-                $value['terms'] = [$terms];
+                if(!empty($terms)){
+                    $value['terms'] = [$terms];
+                }
+                else{
+                    unset($value['terms']);
+                }
             }
         }
         $resp['options'] = $coupons;
