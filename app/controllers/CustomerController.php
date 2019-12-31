cf172a1b8026bb659103d36051256f249eb8b608
ankitamamnia <ankitamamnia@fitternity.com>
2019-12-31 20:13:58 +0530

    pps homepage line updated

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -4326,7 +4326,7 @@ class CustomerController extends \BaseController {
 			'header_sub_text' => 'WORKOUT WHEN YOU CAN, PAY WHEN YOU WORKOUT',
 			'subheader' => "Choose your fitness form, book a workout, pay for that session and go workout, it's that simple.",
             // 'knowmorelink' => 'know more',
-			'footer' => "Get 100% Instant Cashback on Workout Sessions",
+			'footer' => "50% Instant Cashback on Workout Sessions OR FLAT 20% Off",
 			'button_text' => 'EXPLORE'
 		];
 
@@ -4430,7 +4430,7 @@ class CustomerController extends \BaseController {
                 "logo"=>"https://b.fitn.in/global/pps/fexclusive1.png",
                 "header"=>"EXPERIENCE FITNESS LIKE NEVER BEFORE!",
                 "subheader"=>"Book sessions and only pay for days you workout",
-                "footer"=>"Get 100% Instant Cashback on Workout Sessions"
+                "footer"=>"50% Instant Cashback on Workout Sessions OR FLAT 20% Off"
             ];
         }
 		
