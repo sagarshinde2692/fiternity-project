809daf79f1fb5c6e184eb9428f6c517f314d50ca
kailashbajya <kailashbajya@fitternity.com>
2020-01-02 20:50:09 +0530

    updated coupons updated query

--- public/coupon_update.js
+++ public/coupon_update.js
@@ -2,7 +2,7 @@
 var coupons =  ["fitternityuw7rybqqevcred","fitternitywt27ycmgy7cred","fitternitymglhnxgfvkcred","fitternity2phahgl770cred","fitternity23u4bsgx9fcred","fitternity5vwrdjpuykcred","fitternitye1awbrcxzjcred","fitternityy2ns7ykowucred","fitternity31qzs4ihyhcred","fitternity2cid9u5ms4cred","fitternity6q6tids32ocred","fitternityx5pn4irn0qcred","fitternity432ngcilyacred","fitternityq41wyjaqnccred","fitternityeki48nn0ancred","fitternityqfqt375ls4cred","fitternitywi8xf6hqx4cred","fitternity3bolgx83xjcred","fitternityrnyhh2pmnhcred","fitternityqk0zhf6z63cred", "fitternity39esuvp3yncred","fitternitympbl7sowfbcred","fitternitye5we5dubd0cred","fitternityegat95oz8ncred","fitternitymvdyhkq5h5cred"];
 
 coupons = ["fitternityuw7rybqqevcred_afhjdshbsd"];
-var x = db.coupons.update({"code": {"$in": coupons}}, {$set: {
+var x = db.coupons.update({"code": {"$in": coupons}, total_used: 0}, {$set: {
     "and_conditions" : [ 
     {
         "key" : "pass.pass_type",
@@ -36,7 +36,7 @@ var x = db.coupons.update({"code": {"$in": coupons}}, {$set: {
 
 // var coupons_180 = ["fitternityhvbdagr5r4cred","fitternity66lgzvmouucred","fitternitybhqpf896ercred","fitternitybwnmax313vcred","fitternity691sq1ncqicred","fitternity72zyrf71llcred","fitternityswhfjsdntgcred","fitternityi0qksgmftccred","fitternityx0fxz6c67xcred","fitternitys0uafe2t1vcred"]
 
-// var y = db.coupons.update({"code": {"$in": coupons_180}}, {$set: {
+// var y = db.coupons.update({"code": {"$in": coupons_180}, total_used: 0}, {$set: {
 //     "and_conditions" : [ 
 //         {
 //             "key" : "pass.pass_type",
@@ -70,7 +70,7 @@ var x = db.coupons.update({"code": {"$in": coupons}}, {$set: {
 
 // var coupons_360 = ["fitternityajv04ohq4acred","fitternity31bizapbhwcred","fitternity99w4jcil5kcred","fitternityhg4cg81xz5cred","fitternity827jl6uaibcred"];
 
-// var y = db.coupons.update({"code": {"$in": coupons_360}}, {$set: {
+// var y = db.coupons.update({"code": {"$in": coupons_360}, total_used: 0}, {$set: {
 //     "and_conditions" : [ 
 //         {
 //             "key" : "pass.pass_type",
