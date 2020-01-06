d12d5c88b362fd63c660156c8ba616415d67a9a6
kailashbajya <kailashbajya@fitternity.com>
2020-01-06 18:54:59 +0530

    updated header lisitng city url

--- app/config/multifit.php
+++ app/config/multifit.php
@@ -1,9 +1,9 @@
 <?php
 return array(
     "vendors_slug"=> [
-        ['name'=>'Mysuru', 'slug'=>'multifitvendor/multifit---mysuru-mysuru', 'city_brand'=>false],
-        ['name'=>'Nagpur', 'slug'=>'multifitvendor/multifit-nagpur-dharampeth-nagpur', 'city_brand'=>false],
-        ['name'=>'Salem', 'slug'=>'multifitvendor/multifit---salem-salem', 'city_brand'=>false]
+        ['name'=>'Mysuru', 'slug'=>'multifit---mysuru-mysuru', 'city_brand'=>false],
+        ['name'=>'Nagpur', 'slug'=>'multifit-nagpur-dharampeth-nagpur', 'city_brand'=>false],
+        ['name'=>'Salem', 'slug'=>'multifit---salem-salem', 'city_brand'=>false]
     ],
     "without_brand_city"=> [
         'mysure'=>['slug'=>'multifit---mysuru-mysuru', 'city_brand'=>false],
