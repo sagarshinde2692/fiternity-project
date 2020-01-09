3987c8a73992216bd0bd1d3de5f1af63d1baf0b4
Sohail Shaikh <mohammedsohailshaikh@fitternity.com>
2020-01-09 19:18:13 +0530

    purchase summary removed from 1 year one pass

--- app/services/Utilities.php
+++ app/services/Utilities.php
@@ -11178,9 +11178,14 @@ Class Utilities {
                             $return_arr['msg_data'] = "Congratulations on your OnePass purchase. You will receive full 100% cashback as FitCash in your Fitternity account by 16th December 2019. Make the most of your FitCash to upgrade your OnePass. Kindly feel free to reach out to us on +917400062849 for queries";
                         }
         
-                        if(!empty($pass['duration']) && in_array($pass['duration'], [30, 90, 180, 360])){
+                        if(!empty($pass['duration']) && in_array($pass['duration'], [30, 90, 180])){
                             $return_arr['text'] = "Addnl FLAT 25% Off + 25% Cashback".getLineBreaker()."24 HOUR FLASH OFFER: FLAT 25% Off (No Restrictions) On 1 Year OnePass Membership".getLineBreaker()."Use Code: FIT2020. Limited Slots";
                             $return_arr['purchase_summary_value'] = "Addnl FLAT 25% Off + 25% Cashback On OnePass Membership | 24 HR FLASH OFFER: FLAT 25% Off On 1 Year OnePass Membership (No Restrictions) | Use Code: FIT2020. Limited Slots";
+                        }
+
+                        if(!empty($pass['duration']) && in_array($pass['duration'], [360])){
+                            $return_arr['text'] = "Addnl FLAT 25% Off + 25% Cashback".getLineBreaker()."24 HOUR FLASH OFFER: FLAT 25% Off (No Restrictions) On 1 Year OnePass Membership".getLineBreaker()."Use Code: FIT2020. Limited Slots";
+                            $return_arr['purchase_summary_value'] = "";
                         }
 
                     }
@@ -11228,9 +11233,14 @@ Class Utilities {
                             $return_arr['msg_data'] = "Congratulations on your OnePass purchase. You will receive full 100% cashback as FitCash in your Fitternity account by 16th December 2019. Make the most of your FitCash to upgrade your OnePass. Kindly feel free to reach out to us on +917400062849 for queries";
                         }
         
-                        if(!empty($pass['duration']) && in_array($pass['duration'], [30, 90, 180, 360])){
+                        if(!empty($pass['duration']) && in_array($pass['duration'], [30, 90, 180])){
                             $return_arr['text'] = "Addnl FLAT 25% Off + 25% Cashback".getLineBreaker()."24 HOUR FLASH OFFER: FLAT 25% Off (No Restrictions) On 1 Year OnePass Membership".getLineBreaker()."Use Code: FIT2020. Limited Slots";
                             $return_arr['purchase_summary_value'] = "Addnl FLAT 25% Off + 25% Cashback On OnePass Membership | 24 HR FLASH OFFER: FLAT 25% Off On 1 Year OnePass Membership (No Restrictions) | Use Code: FIT2020. Limited Slots";
+                        }
+
+                        if(!empty($pass['duration']) && in_array($pass['duration'], [360])){
+                            $return_arr['text'] = "Addnl FLAT 25% Off + 25% Cashback".getLineBreaker()."24 HOUR FLASH OFFER: FLAT 25% Off (No Restrictions) On 1 Year OnePass Membership".getLineBreaker()."Use Code: FIT2020. Limited Slots";
+                            $return_arr['purchase_summary_value'] = "";
                         }
 
                     }
@@ -11280,9 +11290,14 @@ Class Utilities {
                             $return_arr['msg_data'] = "Congratulations on your OnePass purchase. You will receive full 100% cashback as FitCash in your Fitternity account by 16th December 2019. Make the most of your FitCash to upgrade your OnePass. Kindly feel free to reach out to us on +917400062849 for queries";
                         }
         
-                        if(!empty($pass['duration']) && in_array($pass['duration'], [30, 90, 180, 360])){
+                        if(!empty($pass['duration']) && in_array($pass['duration'], [30, 90, 180])){
                             $return_arr['text'] = "Addnl FLAT 25% Off + 25% Cashback".getLineBreaker()."24 HOUR FLASH OFFER: FLAT 25% Off (No Restrictions) On 1 Year OnePass Membership".getLineBreaker()."Use Code: FIT2020. Limited Slots";
                             $return_arr['purchase_summary_value'] = "Addnl FLAT 25% Off + 25% Cashback On OnePass Membership | 24 HR FLASH OFFER: FLAT 25% Off On 1 Year OnePass Membership (No Restrictions) | Use Code: FIT2020. Limited Slots";
+                        }
+
+                        if(!empty($pass['duration']) && in_array($pass['duration'], [360])){
+                            $return_arr['text'] = "Addnl FLAT 25% Off + 25% Cashback".getLineBreaker()."24 HOUR FLASH OFFER: FLAT 25% Off (No Restrictions) On 1 Year OnePass Membership".getLineBreaker()."Use Code: FIT2020. Limited Slots";
+                            $return_arr['purchase_summary_value'] = "";
                         }
 
                     }
